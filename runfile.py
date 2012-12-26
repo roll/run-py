eae2ba9ec19a501ead03363b4f20d91da13756a3
roll <roll@respect31.com>
2012-12-26 15:34:33 +0400

    added runclasses check

--- runfile.py
+++ runfile.py
@@ -1,8 +1,13 @@
-from runclasses.package import PackageRunclass
 from package import package
 
-class Runclass(PackageRunclass):
+try:
+    from runclasses.package import PackageRunclass
+    base = PackageRunclass
+except ImportError:
+    base = object
     
+class Runclass(base):
+        
     #Protected
-    
+        
     _package = package
