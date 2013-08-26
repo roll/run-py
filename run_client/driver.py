69016c0950ab460bb47093186fd8791c8100d482
roll <roll@respect31.com>
2013-08-26 10:11:50 +0400

    rm old code

--- run_client/driver.py
+++
@@ -1,7 +0,0 @@
-class BaseDriver(object):
-    
-    def __init__(self, command):
-        self._command = command
-    
-    def process(self): 
-        pass
--- run_client/script.py
+++
@@ -1,6 +0,0 @@
-import sys
-from .program import Program
-
-def script():
-    program = Program(sys.argv)
-    return program.process()
