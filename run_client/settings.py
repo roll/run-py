5776cbb944c1d1c58acb8b3e55f0e374d9e58505
roll <roll@respect31.com>
2013-08-28 10:22:05 +0400

    upd settings

--- run_client/settings.py
+++ run_client/settings.py
@@ -6,57 +6,32 @@ class Settings(Settings):
     #Settings path
     PATH = os.path.expanduser('~/.run/settings.py')
     
-    #Parser settings
-    parser = {
-        'prog': 'run',
-        'add_help': False,
-    }
-    
-    #CLI options
-    options = {
-        'driver': {
-            'flags': ['-d', '--driver',],
-            'default': '',
-        },               
-        'ishelp': {
-            'flags': ['-h', '--help'],
-            'action': 'store_true',
-        },                    
-        'language': {
-            'flags': ['-l', '--language',],
-            'default': '',
-        },                          
-        'runfile': {
-            'flags': ['-f', '--runfile',],
-            'default': 'runfile',
-        },
-        'runclass': {
-            'flags': ['-c', '--runclass',],
-            'default': 'Runclass',
-        },                           
-    }
-    
-    #CLI arguments
-    arguments = {
-        'function': {
-            'nargs': '?',
-            'default': '',
-        },
+    #Command schema
+    command_schema = {
+        'config': {
+            'prog': 'run',
+            'add_help': False,
+        },                      
         'arguments': {
-            'nargs':'*',
-            'default': [],
-        },             
-    }
- 
-    #Language patterns
-    languages = {
-        'python': '\.py$',
-    }
-    
-    #Driver patterns
-    drivers = {
-        'internal': 'run.drivers.{language}.{language_capitalized}Driver',
-        'external': 'run_{language}.{language_capitalized}Driver',
+            'function': {
+                'nargs': '?',
+                'default': '',
+            },
+            'arguments': {
+                'nargs':'*',
+                'default': [],
+            },             
+        },
+        'options': {        
+            'file': {
+                'flags': ['-f', '--file',],
+                'default': 'runfile.py',
+            },
+            'ishelp': {
+                'flags': ['-h', '--help'],
+                'action': 'store_true',
+            },                      
+        },         
     }
     
     
