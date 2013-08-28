68678479926a7c31388e0e596b78328446f1cf0e
roll <roll@respect31.com>
2013-08-28 12:05:16 +0400

    mc

--- brieffile.py
+++ brieffile.py
@@ -1,6 +1,8 @@
 from brief import Brief, PackageContext, FileTemplate, FileTarget
 
 class Brief(Brief):
+    
+    #TODO: add name = 'run-tool'
        
     context = PackageContext()
     template = FileTemplate('package.tpl')
