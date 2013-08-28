e1d1527893c092d1e9296e68eb251f9a2a07c891
roll <roll@respect31.com>
2013-08-28 10:40:23 +0400

    added Request, Respond to API

--- run_client/__init__.py
+++ run_client/__init__.py
@@ -1,4 +1,6 @@
 from .program import Program, program
+from .request import Request
+from .respond import Respond
 from .responder import Responder 
 from .settings import Settings, settings
 from .version import Version, version
