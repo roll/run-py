9d1f8ac2218ecf208f0ffff8c19aa2b34d7f9cc8
roll <roll@respect31.com>
2013-08-28 10:43:37 +0400

    mc

--- run_client/__init__.py
+++ run_client/__init__.py
@@ -1,6 +1,6 @@
 from .program import Program, program
 from .request import Request
 from .respond import Respond
-from .server import Server 
+from .server import Server
 from .settings import Settings, settings
 from .version import Version, version
