f24456b30c088f0d20d942c3471c0c4ebb0dfc79
roll <roll@respect31.com>
2013-08-30 08:54:51 +0400

    added ipclight inher to client

--- run/client.py
+++ run/client.py
@@ -1,5 +1,15 @@
-class Client:
+from abc import ABCMeta, abstractmethod
+from ipclight import Client, SubprocessClient
+
+class Client(Client, metaclass=ABCMeta):
     
     #Public
     
     pass
+    
+    
+class SubprocessClient(Client):
+    
+    #Public
+    
+    pass  
