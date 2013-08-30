9aebf8f20e6d2dc0bf3c528c9cb51abdcfb7c70f
roll <roll@respect31.com>
2013-08-30 09:06:06 +0400

    fx

--- run/__init__.py
+++ run/__init__.py
@@ -1,7 +1,7 @@
 from .client import Client
 from .program import Program, program
 from .request import Request
-from .respond import Respond
+from .response import Response
 from .run import Run
 from .server import Server
 from .settings import Settings, settings
--- run/response.py
+++ run/response.py
@@ -1,4 +1,4 @@
-class Respond:
+class Response:
     
     #Public
     
