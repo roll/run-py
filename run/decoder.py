12d70f9049e95c5ec62b9c455826f4d39f16c5b7
roll <roll@respect31.com>
2013-08-31 07:45:02 +0400

    added stub methods to enc/dec

--- run/decoder.py
+++ run/decoder.py
@@ -8,6 +8,12 @@ class Decoder:
 
     #Protected
     
+    def _make_transport_message(self):
+        pass
+    
+    def _make_message(self):
+        pass
+    
     #TODO: use cached property
     @property
     def _transport(self):
--- run/encoder.py
+++ run/encoder.py
@@ -8,6 +8,12 @@ class Encoder:
 
     #Protected
     
+    def _make_transport_message(self):
+        pass
+    
+    def _make_data(self):
+        pass
+    
     #TODO: use cached property
     @property
     def _transport(self):
