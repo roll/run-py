ea711fc60688e260fea18c45405c1f0ee0766462
roll <roll@respect31.com>
2013-09-18 12:33:40 +0400

    added method

--- run/request.py
+++ run/request.py
@@ -4,10 +4,15 @@ class Request(CommonMessage):
     
     #Public
     
-    def __init__(self, arguments, options):
+    def __init__(self, method, arguments, options):
+        self._method = method
         self._arguments = arguments
         self._options = options
-        
+    
+    @property
+    def method(self):
+        return self.method  
+    
     @property
     def arguments(self):
         return self._arguments
