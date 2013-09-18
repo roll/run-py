cb769460316e1b16b0f3fd2fcd13a5c4c7886501
roll <roll@respect31.com>
2013-09-18 12:39:16 +0400

    added test

--- tests/test_response.py
+++ tests/test_response.py
@@ -6,4 +6,12 @@ class ResponseTest(unittest.TestCase):
     #Public
     
     def setUp(self):
-        self.response = Response()
+        self.result = 'result'
+        self.error = 'error'
+        self.response = Response(self.result, self.error)
+
+    def test_result(self):
+        self.assertEqual(self.response.result, self.result)
+        
+    def test_error(self):
+        self.assertEqual(self.response.error, self.error)
