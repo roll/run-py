5f26fcaa9ee79f9993ff7e8d4101a53bc3800944
roll <roll@respect31.com>
2013-09-18 11:51:46 +0400

    added arguments, options to request

--- run/request.py
+++ run/request.py
@@ -4,4 +4,14 @@ class Request(CommonMessage):
     
     #Public
     
-    pass
+    def __init__(self, arguments, options):
+        self._arguments = arguments
+        self._option = options
+        
+    @property
+    def arguments(self):
+        return self._arguments
+    
+    @property
+    def options(self):
+        return self._options    
