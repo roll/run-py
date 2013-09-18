cb6e5a17aaeb5d5e30a788d9979dd24290e9a5b8
roll <roll@respect31.com>
2013-09-18 12:37:28 +0400

    added result, error to response

--- run/response.py
+++ run/response.py
@@ -4,4 +4,14 @@ class Response(CommonMessage):
     
     #Public
     
-    pass
+    def __init__(self, result, error=None):
+        self._result = result
+        self._error = error
+    
+    @property    
+    def result(self):
+        return self._result
+
+    @property    
+    def error(self):
+        return self._error
