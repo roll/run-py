9fe5dfe8937061c8f58704b204b42a130b28b86e
roll <roll@respect31.com>
2013-09-20 11:56:51 +0400

    started test packer

--- tests/test_packer.py
+++ tests/test_packer.py
@@ -1,5 +1,6 @@
 import unittest
-from run import Packer
+import ipclight
+from run import Packer, Request
 
 class PackerTest(unittest.TestCase):
     
@@ -7,3 +8,15 @@ class PackerTest(unittest.TestCase):
     
     def setUp(self):
         self.packer = Packer()
+        self.protocol = 'run-json-1.0'
+        self.method = 'method'
+        self.arguments = ['arguments']
+        self.options = {'options': True}
+        self.message = Request(self.method, self.arguments, self.options)
+        
+    def test_pack(self):
+        transport_message = self.packer.pack(self.message, self.protocol)
+        self.assertIsInstance(transport_message, ipclight.Request)
+        self.assertEqual(transport_message.protocol, self.protocol)
+        #TODO: improve
+        self.assertTrue(transport_message.content)
