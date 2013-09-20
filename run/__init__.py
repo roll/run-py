0ca60d0941aaffae4fcde2fb9c69628bf79a7517
roll <roll@respect31.com>
2013-09-20 11:40:50 +0400

    added packer-unpacker to API

--- run/__init__.py
+++ run/__init__.py
@@ -2,10 +2,12 @@ from .client import Client
 from .decoder import Decoder
 from .encoder import Encoder
 from .message import Message
+from .packer import Packer
 from .program import Program, program
 from .request import Request
 from .response import Response
 from .run import Run
 from .server import Server
 from .settings import settings
+from .unpacker import Unpacker
 from .version import version
