60c2cd6f62f16d063d639da75bf7ac33bf361234
roll <roll@respect31.com>
2013-09-21 12:38:45 +0400

    added __init__ to server

--- run/server.py
+++ run/server.py
@@ -4,6 +4,9 @@ class Server(metaclass=ABCMeta):
     
     #Public
     
+    def __init__(self, run):
+        self._run = run
+    
     @abstractmethod
     def serve(self):
         pass #pragma: no cover
