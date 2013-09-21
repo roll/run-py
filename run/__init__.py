d340f581a66978e08cbae0b7c542a13c3a199b01
roll <roll@respect31.com>
2013-09-21 09:24:40 +0400

    added errors to API

--- run/__init__.py
+++ run/__init__.py
@@ -1,13 +1,13 @@
 from .client import Client
-from .decoder import Decoder
-from .encoder import Encoder
+from .decoder import Decoder, DecodeError
+from .encoder import Encoder, EncodeError
 from .message import Message
-from .packer import Packer
+from .packer import Packer, PackError
 from .program import Program, program
 from .request import Request
 from .response import Response
 from .run import Run
 from .server import Server
 from .settings import settings
-from .unpacker import Unpacker
+from .unpacker import Unpacker, UnpackError
 from .version import version
