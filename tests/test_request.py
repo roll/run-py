a15cb55d951d2abc25a902a6272e631d969c04e6
roll <roll@respect31.com>
2013-09-21 08:54:15 +0400

    mc

--- tests/test_request.py
+++ tests/test_request.py
@@ -6,23 +6,20 @@ class RequestTest(unittest.TestCase):
     #Public
     
     def setUp(self):
-        self.method = 'method'
-        self.arguments = ['arguments']
-        self.options = {'options': True}
-        self.request = Request(self.method, self.arguments, self.options)
+        self.request = Request('method', ['args'], {'opts': True})
 
     def test_method(self):
-        self.assertEqual(self.request.method, self.method)
+        self.assertEqual(self.request.method, 'method')
         
     def test_arguments(self):
-        self.assertEqual(self.request.arguments, self.arguments)
+        self.assertEqual(self.request.arguments, ['args'])
     
     def test_options(self):
-        self.assertEqual(self.request.options, self.options)
+        self.assertEqual(self.request.options, {'opts': True})
 
     def test_content(self):
         self.assertEqual(self.request.content, {
-            'method': self.method,
-            'arguments': self.arguments,
-            'options': self.options,
+            'method': 'method',
+            'arguments': ['args'],
+            'options': {'opts': True},
         })                
