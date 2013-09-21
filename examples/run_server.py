a3195392646f91de0133b3be7683475848e36834
roll <roll@respect31.com>
2013-09-21 12:55:41 +0400

    mc

--- examples/run_server.py
+++ examples/run_server.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3.3
 import sys
-from run import SubprocessServer, Response
+from run import SubprocessServer, Run
+
+class Run(Run):
+    
+    #Public
+    
+    pass
+    
 
 class RunServer(SubprocessServer):
     
     #Public
     
-    def respond(self, request):
-        return Response(request.protocol, request.content)
+    def run(self):
+        return Run()
 
 
 if __name__ == '__main__':
