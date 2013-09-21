23b557ad17fff7b7b488a9c8304c2d7dbd390e13
roll <roll@respect31.com>
2013-09-21 12:42:25 +0400

    cleaned Run

--- run/run.py
+++ run/run.py
@@ -2,17 +2,4 @@ class Run:
     
     #Public
     
-    #TODO: add accidental redefinition proof 
-    def respond(self, request=None):
-        if request:
-            return self._internal_respond(request)
-        else:
-            self._external_respond()
-    
-    #Protected
-    
-    def _internal_respond(self, request):
-        pass
-    
-    def _external_respond(self):
-        pass
+    pass
