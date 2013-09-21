81e83039168264eb51b2511860caffab62ef8b9a
roll <roll@respect31.com>
2013-09-21 09:11:35 +0400

    added test_unpack draft

--- tests/test_unpacker.py
+++ tests/test_unpacker.py
@@ -1,5 +1,6 @@
+import json
 import unittest
-from run import Unpacker
+from run import Unpacker, Request
 
 class UnpackerTest(unittest.TestCase):
     
@@ -8,5 +9,14 @@ class UnpackerTest(unittest.TestCase):
     def setUp(self):
         self.unpacker = Unpacker()
         
-    def test_unpack(self):
-        pass
+    def test_unpack_request(self):
+        message = self.unpacker.unpack('request/run-json-1.0/'+json.dumps({
+            'method': 'method',
+            'arguments': ['arguments'],
+            'options': {'options': True},
+        }))
+        self.assertIsInstance(message, Request)
+        self.assertEqual(message.protocol, 'run-json-1.0')
+        self.assertEqual(message.method, 'method')
+        self.assertEqual(message.arguments, ['arguments'])
+        self.assertEqual(message.options, {'options': True})
