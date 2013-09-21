d2a056200fb4f1e1f45acddc0eaa59ddec9abf7f
roll <roll@respect31.com>
2013-09-21 10:35:48 +0400

    impl test_decode

--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -1,5 +1,5 @@
 import unittest
-from run import Decoder
+from run import Decoder, Request
 
 class DecoderTest(unittest.TestCase):
     
@@ -7,3 +7,9 @@ class DecoderTest(unittest.TestCase):
     
     def setUp(self):
         self.decoder = Decoder()
+        
+    def test_decode(self):
+        text_message = 'request/run-json-1.0/{"method": "method"}'
+        message = self.decoder.decode(text_message)
+        self.assertIsInstance(message, Request)
+        self.assertEqual(message.method, 'method')   
