33f9d03f9cc4dcd603dd9aa2540bd489ede2e7f6
roll <roll@respect31.com>
2013-09-23 10:51:36 +0400

    upd settings to new lib31 command

--- run/settings.py
+++ run/settings.py
@@ -2,33 +2,36 @@ from lib31.package import Settings
 
 class Settings(Settings):
     
-    command_schema = {
-        'config': {
+    #Public
+    
+    default_protocol = 'run-json-1.0' 
+    
+    @property
+    def command_schema(self):
+        return {
             'prog': 'run',
-            'add_help': False,
-        },                      
-        'arguments': {
-            'method': {},
-            'parameters': {
-                'nargs':'*',
-                'default': [],
-            },             
-        },
-        'options': {
-            #TODO: make no required     
-            'server': {
-                'flags': ['-s', '--server',],
-                'required': True,
-            },   
-            #TODO: add default?
-            'protocol': {
-                'flags': ['-p', '--protocol',],
-                'required': False,
-            },                                     
-        },        
-    }
-    
-    default_protocol = 'run-json-1.0'    
-    
+            'add_help': False,                     
+            'arguments': [
+                {
+                 'name': 'method',
+                },
+                {
+                 'name': 'parameters',
+                 'nargs':'*',
+                 'default': [],
+                },             
+                {
+                 'dest': 'server',
+                 'flags': ['-s', '--server',], #TODO: make no required
+                 'required': True,
+                },   
+                {
+                 'dest': 'protocol', 
+                 'flags': ['-p', '--protocol',],
+                 'default': self.default_protocol,               
+                },                                     
+            ],        
+        }
+   
     
 settings = Settings()
