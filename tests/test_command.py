fa83091f90985edbb30c8d2105456b9e428c28da
roll <roll@respect31.com>
2013-09-23 08:47:37 +0400

    upd test_command

--- tests/test_command.py
+++ tests/test_command.py
@@ -5,9 +5,18 @@ class CommandTest(unittest.TestCase):
     
     #Public
     
-    def test_all_elements(self):
+    def test_argv1(self):
         command = Command([
              'run', '-s', 'server', 'method', 'arg1,arg2,opt1=True,', 'opt2=2'])
+        self.assertEqual(command.server, 'server')
         self.assertEqual(command.method, 'method')
         self.assertEqual(command.arguments, ['arg1', 'arg2'])
         self.assertEqual(command.options, {'opt1': True, 'opt2': 2})
+    
+    def test_argv2(self):
+        command = Command([
+             'run', '-s', 'server', 'method', '3'])
+        self.assertEqual(command.server, 'server')
+        self.assertEqual(command.method, 'method')
+        self.assertEqual(command.arguments, [3])
+        self.assertEqual(command.options, {})        
