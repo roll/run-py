7c29801e419d44fe5ca88356b38a54ef6ba84371
roll <roll@respect31.com>
2013-09-23 08:31:25 +0400

    impr command test

--- tests/test_command.py
+++ tests/test_command.py
@@ -5,6 +5,15 @@ class CommandTest(unittest.TestCase):
     
     #Public
     
-    def test(self):
-        command = Command(['run', '-s', 'server', 'method'])             
-        self.assertEqual(command.method, 'method')
+    def setUp(self):
+        self.command = Command(
+            ['run', '-s', 'server', 'method', 'arg1,arg2,opt1=True'])
+    
+    def test_method(self):
+        self.assertEqual(self.command.method, 'method')
+    
+    def test_arguments(self):
+        self.assertEqual(self.command.arguments, ['arg1', 'arg2'])
+    
+    def test_options(self):
+        self.assertEqual(self.command.options, {'opt1': True})
