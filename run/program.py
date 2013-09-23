a25e91f27aaa66aa14a112f128966839d841029c
roll <roll@respect31.com>
2013-09-23 06:42:01 +0400

    mc

--- run/program.py
+++ run/program.py
@@ -10,13 +10,23 @@ class Program(Program):
         
     def __call__(self):
         client = SubprocessClient(self._command.server)
-        #TODO: reimplement
-        request = Request(self._command.protocol, self._command.content)
-        response = client.request(request)
+        response = client.request(self._request)
         print(response.content)
             
     #Protected
+        
+    @property
+    def _request(self):
+        #TODO: reimplement
+        method = self._command.method
+        arguments, options = self._parse_parameters(self._command.parameters)
+        request = Request(method, arguments, options)
+        return request
     
+    #TODO: implement
+    def _parse_parameters(self):
+        pass
+        
     @property
     def _command_schema(self):
         return settings.command_schema
