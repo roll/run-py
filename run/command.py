a4fb86520673695d8f91211622c779ff9d53ad87
roll <roll@respect31.com>
2013-09-23 07:12:24 +0400

    impl command

--- run/command.py
+++ run/command.py
@@ -9,9 +9,32 @@ class Command(Command):
     #TODO: use cachedproperty 
     @property
     def arguments(self):
-        pass
+        return self._parse_parameters[0]
     
     #TODO: use cachedproperty    
     @property    
     def options(self):
-        pass  
+        return self._parse_parameters[1]
+    
+    #Protected
+    
+    #TODO: use cachedproperty 
+    @property
+    def _parsed_parameters(self):
+        arguments = []
+        options = {}
+        for element in csv.reader([''.join(self.parameters)]):
+            parts = [self._parse_literal(item) for item in 
+                     next(csv.reader([element], delimiter='='))]
+            if len(parts) == 1:
+                arguments.append(parts[0])
+            elif len(parts) == 2:
+                options[parts[0]] = parts[1]
+        return (arguments, options)
+    
+    def _parse_literal(self, literal):
+        try:
+            value = ast.literal_eval(literal)
+        except ValueError:
+            return literal
+        return value
