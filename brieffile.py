30c1c6205af9d408836af49296185bca45fb6f53
roll <roll@respect31.com>
2013-09-26 10:44:56 +0400

    mc

--- brieffile.py
+++ brieffile.py
@@ -2,6 +2,8 @@ from brief import FileInput, FileOutput
 from briefbooks import PackageBrief
 
 class Brief(PackageBrief):
+    
+    #Public
        
     input = FileInput('package.in')
     output = FileOutput('package.py')
