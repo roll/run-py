dcf7e85584b33029a1486d57bd459bcc6e325bf2
roll <roll@respect31.com>
2013-09-30 11:39:57 +0400

    upd readme

--- package.py
+++ package.py
@@ -48,78 +48,47 @@ Example
 -------
 - create runfile.py in current working directory::
 
-    def hello(person, times=1):    
-        """prints 'Hello, {person}, {times} times!'"""
-        print('Hello, {person}, {times} times!'.
-              format(person=person,
-                     times=str(times)))
-            
-    def nothing():
-        """does nothing"""
-        pass
-            
-    OR        
-            
-    class Runclass(object):
-        
-        def hello(self, person, times=1): 
-            """prints 'Hello, {person}, {times} times!'"""
-            print('Hello, {person}, {times} times!'.
-                  format(person=person,
-                         times=str(times)))
-            
-        def nothing(self):
-            """does nothing"""
-            pass
+    from run import Run
+
+    class Run(Run):
+    
+        #Public
+    
+        def hello(self, person, times=1):
+            """Print 'Hello {person} {times} times!'"""
+            print('Hello {person} {times} times!'.format(person=person,
+                                                         times=str(times)))
             
 - get functions/methods list from command line::
 
     $ run
     hello
-    nothing
+    help
+    list
 
-- autocomplete function/method from command line::
+- autocomplete method from command line::
 
     $ run he<TAB>
     $ run hello
     
-- get function/method help from command line::
+- get method help from command line::
 
     $ run hello -h
     hello(person, times=1)
-    prints 'Hello, {person}, {times} times!'
+    Print 'Hello, {person}, {times} times!'
 
-- run function/method from command line::
+- run method from command line::
 
-    $ run hello world times=3
-    Hello, world, 3 times!
-    
-Usage
------
-Command line::
-
-    run [-h] [-d DRIVER] [-l LANGUAGE] [-f RUNFILE] [-c RUNCLASS] 
-        [function] [arguments [arguments ...]]
-
-    positional arguments:
-      function
-      arguments
-
-    optional arguments:
-      -h, --help    
-      -d DRIVER, --driver DRIVER
-      -l LANGUAGE, --language LANGUAGE
-      -f RUNFILE, --runfile RUNFILE      
-      -c RUNCLASS, --runclass RUNCLASS
-
-Extension
----------
-You can write driver for your favorite language. 
-It's all about run/inspect functions -- no script/command line routine need to be implemented.
-Run core automaticly finds language drivers in run_{language}.{Language}Driver form.
+    $ run hello world, times=3
+    Hello world 3 times!
 
 History
 -------
+0.7.0
+`````
+- rewritten using IPC.Light
+- moved to Python 3.3
+
 0.6.0
 `````
 - included python driver
