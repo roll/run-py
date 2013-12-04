805268ed65a6f621297f97ca7f36a54bb685f457
roll <roll@respect31.com>
2013-12-04 14:15:11 +0400

    rm default_protocol from settings

--- run/settings.py
+++ run/settings.py
@@ -5,46 +5,37 @@ class Settings(Settings):
     
     #Public
     
-    default_protocol = 'run-json-1.0' 
-    
-    @property
-    def command_schema(self):
-        return {
-            'prog': 'run',
-            'add_help': False,                     
-            'arguments': [
-                {
-                 'name': 'method',
-                 'nargs': '?',
-                 'default': None,
-                },
-                {
-                 'name': 'arguments',
-                 'nargs':'*',
-                 'default': [],
-                },             
-                {
-                 'dest': 'server',
-                 'flags': ['-s', '--server'],
-                 'default': 'runfile.py',
-                },   
-                {
-                 'dest': 'protocol', 
-                 'flags': ['-p', '--protocol'],
-                 'default': self.default_protocol,               
-                },
-                {
-                 'dest': 'help',
-                 'action': 'store_true',
-                 'flags': ['-h', '--help'],
-                },                            
-                {
-                 'action': 'version',
-                 'flags': ['-v', '--version'],
-                 'version': 'Run '+str(version),               
-                },                                                             
-            ],        
-        }
+    command_schema = {
+        'prog': 'run',
+        'add_help': False,                     
+        'arguments': [
+            {
+             'name': 'method',
+             'nargs': '?',
+             'default': None,
+            },
+            {
+             'name': 'arguments',
+             'nargs':'*',
+             'default': [],
+            },             
+            {
+             'dest': 'server',
+             'flags': ['-s', '--server'],
+             'default': 'runfile.py',
+            },   
+            {
+             'dest': 'help',
+             'action': 'store_true',
+             'flags': ['-h', '--help'],
+            },                            
+            {
+             'action': 'version',
+             'flags': ['-v', '--version'],
+             'version': 'Run '+str(version),               
+            },                                                             
+        ],        
+    }
    
     
 settings = Settings()
