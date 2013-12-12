ec0352432e4a0b0f2c043dc34cc4caa39dbd011b
roll <roll@respect31.com>
2013-12-12 22:42:22 +0400

    mf

--- tests/test_command.py
+++ tests/test_command.py
@@ -7,16 +7,16 @@ class CommandTest(unittest.TestCase):
     
     def test_argv1(self):
         command = Command([
-             'run', '-f', 'file', 'method', 'arg1,arg2,kwarg1=True,', 'kwarg2=2'])
+             'run', '-f', 'file', 'task', 'arg1,arg2,kwarg1=True,', 'kwarg2=2'])
         self.assertEqual(command.file, 'file')
-        self.assertEqual(command.method, 'method')
+        self.assertEqual(command.task, 'task')
         self.assertEqual(command.args, ['arg1', 'arg2'])
         self.assertEqual(command.kwargs, {'kwarg1': True, 'kwarg2': 2})
     
     def test_argv2(self):
         command = Command([
-             'run', '-f', 'file', 'method', '3'])
+             'run', '-f', 'file', 'task', '3'])
         self.assertEqual(command.file, 'file')
-        self.assertEqual(command.method, 'method')
+        self.assertEqual(command.task, 'task')
         self.assertEqual(command.args, [3])
         self.assertEqual(command.kwargs, {})        
