a7b2e9fe66362bb538399cfbec8aedcfc0568ea8
roll <roll@respect31.com>
2013-12-12 16:02:11 +0400

    added tasks to API

--- run/__init__.py
+++ run/__init__.py
@@ -2,5 +2,6 @@ from .command import Command
 from .program import Program, program
 from .run import Run
 from .settings import settings
+from .task import Task, RenderTask
 from .var import Var, ParsedVar
 from .version import version
