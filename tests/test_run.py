f3012b633788938958b35ddc8135d84245e5d3b8
roll <roll@respect31.com>
2013-12-12 22:41:46 +0400

    rm run test

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,5 +1,4 @@
 import unittest
-from run import Run, Task
 
 #Tests
 
@@ -8,28 +7,4 @@ class RunTest(unittest.TestCase):
     #Public
 
     def setUp(self):
-        self.sub = MockRun()
-        
-    def test__tasks(self):
-        self.assertEqual(list(self.sub._tasks), ['x', 'a', 'b', 'c'])
-        
-        
-#Fixtures
-        
-class MockTask(Task):
-    
-    def complete(self, values):
-        pass   
-    
-    
-class BaseMockRun(Run):
-  
-    x = MockTask()
-    c = MockTask()
-    
-    
-class MockRun(BaseMockRun):
-  
-    a = MockTask()
-    b = MockTask()  
-    c = BaseMockRun.c
+        pass
