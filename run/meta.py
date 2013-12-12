5fce4bff7fbbc6a00f0831cb8f679f3639888db6
roll <roll@respect31.com>
2013-12-12 18:24:53 +0400

    added draft code to meta

--- run/meta.py
+++ run/meta.py
@@ -1,8 +1,12 @@
+import types
+
 class RunMeta(type):
-    
-    #Public
-    
-    pass
+   
+    def __new__(cls, name, bases, attrs):
+        for attr_name, attr_value in attrs.iteritems():
+            if isinstance(attr_value, types.FunctionType):
+                attrs[attr_name] = cls.deco(attr_value)
+        return type.__new__(cls, name, bases, attrs)
 
 
 def require(*task_names):
