41c568a52d87688e4a18e68ec599bc7fc8271259
roll <roll@respect31.com>
2013-12-12 17:07:38 +0400

    added descriptor to task impl

--- run/task.py
+++ run/task.py
@@ -5,9 +5,22 @@ from jinja2 import Environment, FileSystemLoader
 class Task(metaclass=ABCMeta):
     
     #Public
+    
+    def __init__(self, *args, **kwargs):
+        self._require = kwargs.get('require', [])
+
+    def __get__(self, run, runclass=None):
+        self._run = run
+        return self
+        
+    def __call__(self, *args, **kwargs):
+        for task_name in self._require:
+            task = getattr(self._run, task_name)
+            task()
+        self.complete(*args, **kwargs)
 
     @abstractmethod
-    def complete(self, run):
+    def complete(self, *args, **kwargs):
         pass #pragma: no cover
   
 
@@ -17,10 +30,10 @@ class RenderTask(Task):
         self._source = source
         self._target = target
         
-    def complete(self, run):
+    def complete(self):
         dirname, filename = os.path.split(os.path.abspath(self._source))
         environment = Environment(loader=FileSystemLoader(dirname))
         template = environment.get_template(filename)
-        text = template.render(run)
+        text = template.render(self._run)
         with open(self._target, 'w') as file:
             file.write(text)      
