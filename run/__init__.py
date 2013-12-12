81ab30aa71973dabec46583a4232fde089f078c4
roll <roll@respect31.com>
2013-12-12 16:06:29 +0400

    rn sub meta to run meta

--- run/__init__.py
+++ run/__init__.py
@@ -1,6 +1,6 @@
 from .command import Command
 from .program import Program, program
-from .run import Run
+from .run import Run, RunMeta
 from .settings import settings
 from .task import Task, RenderTask
 from .var import Var, ParsedVar
