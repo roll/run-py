96bbb52f565d508e587e8a39e59151b9582a9c8c
roll <roll@respect31.com>
2013-12-12 16:25:22 +0400

    added run to task iface

--- run/task.py
+++ run/task.py
@@ -7,7 +7,7 @@ class Task(metaclass=ABCMeta):
     #Public
 
     @abstractmethod
-    def complete(self, values):
+    def complete(self, run):
         pass #pragma: no cover
   
 
@@ -17,10 +17,10 @@ class RenderTask(Task):
         self._source = source
         self._target = target
         
-    def complete(self, values):
+    def complete(self, run):
         dirname, filename = os.path.split(os.path.abspath(self._source))
         environment = Environment(loader=FileSystemLoader(dirname))
         template = environment.get_template(filename)
-        text = template.render(values)
+        text = template.render(run)
         with open(self._target, 'w') as file:
             file.write(text)      
