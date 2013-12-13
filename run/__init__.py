48acab0727cf36b874c1e0b934d7bf774b092b7b
roll <roll@respect31.com>
2013-12-13 11:55:56 +0400

    added property concept stub

--- run/__init__.py
+++ run/__init__.py
@@ -1,6 +1,7 @@
 from .command import Command
 from .module import Module, ModuleMeta, require
 from .program import Program, program
+from .property import Property
 from .run import Run
 from .settings import settings
 from .task import Task, RenderTask
---
+++ run/property.py
@@ -0,0 +1,5 @@
+class Property:
+    
+    #Public
+    
+    pass
