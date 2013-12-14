1a778965637f87d9dbde7675c648ee07c3984725
roll <roll@respect31.com>
2013-12-14 12:02:37 +0400

    mr

--- run/binding.py
+++ run/binding.py
@@ -17,56 +17,54 @@ class Binding:
     def field(self):
         return self._field
     
+    #TODO: implement
+    @property    
+    def field_name(self):
+        pass
+    
     @property    
     def module(self):
-        return self._module   
-    
-    @property    
-    def params(self):
-        return self._params   
-    
-    @property 
-    def require(self):
-        return self.params.get('require', [])
-    
-    @property 
-    def help(self):
-        return self.params.get('help', None)    
-                  
+        return self._module
+            
+    #TODO: implement
     @property
-    def run(self):
-        run = self.owner
-        if isinstance(run, Run):
-            return run
-        else:
-            raise RuntimeError('Field is not bound')
-    
+    def module_run(self):
+        pass
+        
     @property
-    def fields(self):
+    def module_fields(self):
         fields = {}
-        for cls in self.run.__class__.mro():
+        for cls in self.module.__class__.mro():
             for name, attr in cls.__dict__.items():
                 if isinstance(attr, Field):
                     fields[name] = attr
         return fields
     
     @property
-    def modules(self):
+    def module_modules(self):
         return [name for name, prop 
                 in self.fields.items() 
                 if isinstance(prop, Module)]
     @property
-    def tasks(self):
+    def module_tasks(self):
         return [name for name, prop 
                 in self.fields.items() 
                 if isinstance(prop, Task)]
     @property
-    def vars(self):
+    def module_vars(self):
         return [name for name, prop 
                 in self.fields.items() 
                 if isinstance(prop, Var)]
     
+    @property 
+    def require(self):
+        return self._params.get('require', [])
+    
+    @property 
+    def help(self):
+        return self._params.get('help', None)
+    
     def resolve(self):
         for task_name in self.require:
-            task = getattr(self.run, task_name)
+            task = getattr(self.module, task_name)
             task()                                         
--- run/field.py
+++ run/field.py
@@ -1,23 +1,22 @@
 import inspect
 import importlib
+from lib31.python import cachedproperty
 
 class Field:
     
     #Public
     
     def __init__(self, *args, **kwargs):
-        self.__binding = None
+        self.__module = None
         self.__params = kwargs
     
     def __get__(self, module, module_class=None):
-        if not self.__binding:
-            pacakge = inspect.getmodule(self).__package__
-            module = importlib.import_module('.binding', pacakge)
-            self.__binding = module.Binding(self, module, self.__params)
-        if self.__binding.module != module:
+        if not self.__module:
+            self.__module = module
+        if self._binding.module != module:
             raise RuntimeError(
                 'Field "{0}" is already bound to module "{1}"'.
-                format(self, self._manager.module)) 
+                format(self, self._binding.module))
         return self
     
     def help(self):
@@ -25,9 +24,12 @@ class Field:
     
     #Protected
 
+    @cachedproperty
     def _binding(self):
-        if self.__binding:
-            return self.__binding
+        if self.__module:
+            package = inspect.getmodule(self).__package__
+            module = importlib.import_module('.binding', package)
+            return module.Binding(self, self.__module, self.__params)
         else:
             raise RuntimeError(
                 'Field "{0}" is not bound to any module'.format(self))
