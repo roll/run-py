aabf001e854eddabd3750aca7157e575f9b1434a
roll <roll@respect31.com>
2013-12-14 13:07:14 +0400

    started binding removing

--- run/field.py
+++ run/field.py
@@ -33,3 +33,54 @@ class Field:
         else:
             raise RuntimeError(
                 'Field "{0}" is not bound to any module'.format(self))
+    
+    #TODO: implement
+    @property    
+    def field_name(self):
+        pass
+    
+    @property    
+    def field_params(self):
+        return self._field_params
+    
+    @property 
+    def field_require(self):
+        return self.field_params.get('require', [])
+    
+    @property 
+    def field_help(self):
+        return self.field_params.get('help', None)
+            
+    @property    
+    def module(self):
+        return self._module
+        
+    @property
+    def module_fields(self):
+        fields = {}
+        for cls in self.module.__class__.mro():
+            for name, attr in cls.__dict__.items():
+                if isinstance(attr, Field):
+                    fields[name] = attr
+        return fields
+    
+    @property
+    def module_modules(self):
+        return [name for name, prop 
+                in self.fields.items() 
+                if isinstance(prop, Module)]
+    @property
+    def module_tasks(self):
+        return [name for name, prop 
+                in self.fields.items() 
+                if isinstance(prop, Task)]
+    @property
+    def __module_vars(self):
+        return [name for name, prop 
+                in self.fields.items() 
+                if isinstance(prop, Var)]
+    
+    def __resolve(self):
+        for task_name in self.field_require:
+            task = getattr(self.module, task_name)
+            task()                            
