319dc3f5a1a704ec91981a204a98344910ef2252
roll <roll@respect31.com>
2013-12-14 11:41:03 +0400

    rn owner to module

--- run/field.py
+++ run/field.py
@@ -9,15 +9,15 @@ class Field:
         self.__binding = None
         self.__params = kwargs
     
-    def __get__(self, owner, owner_class=None):
+    def __get__(self, module, module_class=None):
         if not self.__binding:
             pacakge = inspect.getmodule(self).__package__
             module = importlib.import_module('.binding', pacakge)
-            self.__binding = module.Binding(self, owner, self.__params)
-        if self.__binding.owner != owner:
+            self.__binding = module.Binding(self, module, self.__params)
+        if self.__binding.module != module:
             raise RuntimeError(
-                'Field "{0}" is already bound to owner "{1}"'.
-                format(self, self._manager.owner)) 
+                'Field "{0}" is already bound to module "{1}"'.
+                format(self, self._manager.module)) 
         return self
     
     def help(self):
@@ -30,4 +30,4 @@ class Field:
             return self.__binding
         else:
             raise RuntimeError(
-                'Field "{0}" is not bound to any owner'.format(self))
+                'Field "{0}" is not bound to any module'.format(self))
