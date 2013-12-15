37f143f897eae7976232d0f86b387ed78b499f62
roll <roll@respect31.com>
2013-12-15 21:51:53 +0400

    minor

--- run/attribute.py
+++ run/attribute.py
@@ -36,6 +36,7 @@ class DependentAttributeMixin(AttributeMixin):
         self.__require = kwargs.pop('require', [])
         super().__init__(*args, **kwargs)
     
+    #TODO: make it happened just one time
     def resolve(self):
         for task_name in self.__require:
             task = getattr(self.namespace, task_name)
