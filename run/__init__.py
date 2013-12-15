f685d1ea8175a37ea3662481cb8dc5923bc87508
roll <roll@respect31.com>
2013-12-15 18:12:12 +0400

    extended API

--- run/__init__.py
+++ run/__init__.py
@@ -1,11 +1,11 @@
 from .attribute import AttributeMixin, DependentAttributeMixin
 from .command import Command
 from .require import require
-from .module import Module
-from .namespace import NamespaceMeta, NamespaceMixin
+from .module import Module, RunModule
+from .namespace import NamespaceMeta, NamespaceMixin, NamespaceAttributes
 from .program import Program, program
 from .run import Run
-from .settings import settings
-from .task import Task
-from .var import Var
-from .version import version
+from .settings import Settings, settings
+from .task import Task, MethodTask
+from .var import Var, ValueVar, PropertyVar
+from .version import Version, version
