20340aa35ff2d43471f8f4107f337bf9e377d333
roll <roll@respect31.com>
2013-12-16 08:33:11 +0400

    impl UnitName test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -6,4 +6,13 @@ class UnitNameTest(unittest.TestCase):
     #Public
 
     def setUp(self):
-        self.unitname = UnitName
+        self.unitname = UnitName('namespace', 'attribute')
+        
+    def test_name(self):
+        self.assertEqual(self.unitname, 'namespace.attribute')
+        
+    def test_namespace(self):
+        self.assertEqual(self.unitname.namespace, 'namespace')
+        
+    def test_attribute(self):
+        self.assertEqual(self.unitname.attribute, 'attribute')                
