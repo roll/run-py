c16795352b5bd9cbfc86a3fceaa2c59104dab9d8
roll <roll@respect31.com>
2013-12-16 22:30:07 +0400

    added __copy__ to Attribute

--- run/attribute.py
+++ run/attribute.py
@@ -5,12 +5,22 @@ class Attribute(metaclass=ABCMeta):
     
     #Public
     
+    def __new__(cls, *args, **kwargs):
+        obj = super(Attribute, cls).__new__(cls)
+        obj.__cls = cls
+        obj.__args = args
+        obj.__kwargs = kwargs
+        return obj
+    
     @abstractmethod
     def __get__(self, module, module_class):
         pass #pragma: no cover
     
     def __set__(self, module, value):
         raise RuntimeError('Can\'t set attribute')
+    
+    def __copy__(self):
+        return self.__cls(*self.__args, **self.__kwargs)
 
     #TODO: use NullModule?
     @property
