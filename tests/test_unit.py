4ad263efc336a74245d97cf3b55092878f7c5568
roll <roll@respect31.com>
2013-12-16 08:35:38 +0400

    impl UnitHelp test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,5 +1,5 @@
 import unittest
-from run import UnitName
+from run import UnitName, UnitHelp
 
 class UnitNameTest(unittest.TestCase):
 
@@ -15,4 +15,21 @@ class UnitNameTest(unittest.TestCase):
         self.assertEqual(self.unitname.namespace, 'namespace')
         
     def test_attribute(self):
-        self.assertEqual(self.unitname.attribute, 'attribute')                
+        self.assertEqual(self.unitname.attribute, 'attribute')
+        
+        
+class UnitNameHelp(unittest.TestCase):
+
+    #Public
+
+    def setUp(self):
+        self.unithelp = UnitHelp('signature', 'docstring')
+        
+    def test_help(self):
+        self.assertEqual(self.unithelp, 'signature\ndocstring')
+        
+    def test_signature(self):
+        self.assertEqual(self.unithelp.signature, 'signature')
+        
+    def test_docstring(self):
+        self.assertEqual(self.unithelp.docstring, 'docstring')                        
