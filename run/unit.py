68d78b2aee6af2ecb60c1b923ca31bb99718fa55
roll <roll@respect31.com>
2013-12-16 08:18:38 +0400

    implemented UnitHelp

--- run/unit.py
+++ run/unit.py
@@ -20,8 +20,8 @@ class UnitName(str):
     #Public
     
     def __new__(cls, namespace, attribute):
-        qualname = '.'.join(namespace, attribute)
-        return super().__new__(cls, qualname)
+        name = '.'.join(namespace, attribute)
+        return super().__new__(cls, name)
     
     def __init__(self, namespace, attribute):
         self._namespace = namespace
@@ -40,4 +40,18 @@ class UnitHelp(str):
     
     #Public
     
-    pass    
+    def __new__(cls, signature, docstring):
+        hlp = '\n'.join(signature, docstring)
+        return super().__new__(cls, hlp)
+    
+    def __init__(self, signature, docstring):
+        self._signature = signature
+        self._docstring = docstring
+    
+    @property    
+    def signature(self):
+        return self._signature
+    
+    @property
+    def docstring(self):
+        return self._docstring      
