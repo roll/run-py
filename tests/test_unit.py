59f4e9ce0cecfe17f70b66e6820b042090188bf5
roll <roll@respect31.com>
2013-12-16 08:49:29 +0400

    added partial to Unit*

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -4,32 +4,32 @@ from run import UnitName, UnitHelp
 class UnitNameTest(unittest.TestCase):
 
     #Public
-
-    def setUp(self):
-        self.unitname = UnitName('namespace', 'attribute')
         
-    def test_name(self):
-        self.assertEqual(self.unitname, 'namespace.attribute')
+    def test_full(self):
+        unitname = UnitName('namespace', 'attribute')
+        self.assertEqual(unitname, 'namespace.attribute')
+        self.assertEqual(unitname.namespace, 'namespace')
+        self.assertEqual(unitname.attribute, 'attribute')
         
-    def test_namespace(self):
-        self.assertEqual(self.unitname.namespace, 'namespace')
-        
-    def test_attribute(self):
-        self.assertEqual(self.unitname.attribute, 'attribute')
+    def test_partial(self):
+        unitname = UnitName('', 'attribute')
+        self.assertEqual(unitname, 'attribute')
+        self.assertEqual(unitname.namespace, '')
+        self.assertEqual(unitname.attribute, 'attribute')        
         
         
 class UnitNameHelp(unittest.TestCase):
 
     #Public
 
-    def setUp(self):
-        self.unithelp = UnitHelp('signature', 'docstring')
+    def test_full(self):
+        unithelp = UnitHelp('signature', 'docstring')
+        self.assertEqual(unithelp, 'signature\ndocstring')
+        self.assertEqual(unithelp.signature, 'signature')
+        self.assertEqual(unithelp.docstring, 'docstring')
         
-    def test_help(self):
-        self.assertEqual(self.unithelp, 'signature\ndocstring')
-        
-    def test_signature(self):
-        self.assertEqual(self.unithelp.signature, 'signature')
-        
-    def test_docstring(self):
-        self.assertEqual(self.unithelp.docstring, 'docstring')                        
+    def test_partial(self):
+        unithelp = UnitHelp('signature', '')
+        self.assertEqual(unithelp, 'signature')
+        self.assertEqual(unithelp.signature, 'signature')
+        self.assertEqual(unithelp.docstring, '')                                
