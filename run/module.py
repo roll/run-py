7394848917c6667a89828cdcd162517db8509157
roll <roll@respect31.com>
2013-12-18 07:54:18 +0400

    Revert "rn *Builded to *Attributes"
    This reverts commit 21b6193a59001ac4783012ea932381845ca2e611.

--- run/module.py
+++ run/module.py
@@ -69,7 +69,7 @@ class ModuleBuilder(AttributeBuilder):
         return ModuleMeta(name, bases, dct)
     
     def _make_name(self):
-        return self._class.__name__+'Attributes'
+        return self._class.__name__+'Builded'
     
     def _make_bases(self):
         return (self._class,)
