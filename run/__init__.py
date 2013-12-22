f7f7ceb7382f9d1fbd9ca3a3f34be38e12cf36ea
roll <roll@respect31.com>
2013-12-22 14:08:06 +0400

    update API

--- run/__init__.py
+++ run/__init__.py
@@ -1,12 +1,14 @@
-from .attribute import Attribute, AttributeBuilder, AttributeMetadata 
+from .attribute import (AttributeBuilder, AttributeBuilderUpdate,
+                        AttributeBuilderSet, AttributeBuilderCall,
+                        AttributeMeta, Attribute, AttributeMetadata) 
 from .command import Command
-from .dependent import (DependentAttribute, DependentAttributeBuilder, 
-                        DependentAttributeTask, DependentAttributeDecorator,
-                        require, trigger)
+from .dependent import (DependentAttributeBuilder, DependentAttribute,
+                        DependentAttributeMeta, DependentAttributeTask, 
+                        DependentAttributeDecorator, require, trigger)
 from .logger import Logger
-from .module import ModuleMeta, Module, ModuleBuilder, ModuleAttributes
+from .module import ModuleBuilder, ModuleMeta, Module, ModuleAttributes
 from .program import Program, program
-from .run import Run
+from .run import RunMeta, Run
 from .settings import Settings, settings
 from .task import Task, MethodTask
 from .var import Var, ValueVar, PropertyVar
