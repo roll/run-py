4966678db439c6ba4e4555119343ad926bbdd889
roll <roll@respect31.com>
2013-12-22 15:06:23 +0400

    min

--- tests/test_program.py
+++ tests/test_program.py
@@ -3,5 +3,5 @@ from run import Program
 
 def test():
     path = os.path.join(os.path.dirname(__file__), '..', 'runfile.py')
-    program = Program(['run', '-f', path])
+    program = Program(['run', 'render', '-f', path])
     program()
