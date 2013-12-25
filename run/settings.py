0a48e75d52ad374983bbbf3ddcbbc9e68310dccc
roll <roll@respect31.com>
2013-12-25 20:39:10 +0400

    added names, tages flags to console API

--- run/settings.py
+++ run/settings.py
@@ -5,58 +5,75 @@ class Settings(Settings):
     
     #Public
     
-    default_path = '.'
-    default_file = 'runfile.py'
     default_attribute = 'default'
     default_arguments = []
+    default_file = 'runfile.py'
+    default_names = []
+    default_path = '.'
+    default_tags = []
+    
     default_main_module_name = '__main__'
     
-    command_schema = {
-        'prog': 'run',
-        'add_help': False,            
-        'arguments': [
-            {
-             'name': 'attribute',
-             'nargs': '?',
-             'default': None,
-            },
-            {
-             'name': 'arguments',
-             'nargs':'*',
-             'default': default_arguments,
-            },       
-            {
-             'dest': 'file',
-             'flags': ['-f', '--file'],
-             'default': default_file,
-            },   
-            {
-             'dest': 'help',
-             'action': 'store_true',
-             'flags': ['-h', '--help'],
-            },
-            {
-             'dest': 'meta',
-             'action': 'store_true',
-             'flags': ['-m', '--meta'],
-            },
-            {
-             'dest': 'path',
-             'flags': ['-p', '--path'],
-             'default': default_path,
-            },                      
-            {
-             'dest': 'recursively',
-             'action': 'store_true',
-             'flags': ['-r', '--recursively'],
-            },                                                   
-            {
-             'action': 'version',
-             'flags': ['-V', '--version'],
-             'version': 'Run '+str(version),               
-            },                                                             
-        ],        
-    }
+    @property
+    def command_schema(self):
+        return {
+            'prog': 'run',
+            'add_help': False,            
+            'arguments': [
+                {
+                 'name': 'attribute',
+                 'nargs': '?',
+                 'default': None,
+                },
+                {
+                 'name': 'arguments',
+                 'nargs':'*',
+                 'default': self.default_arguments,
+                },       
+                {
+                 'dest': 'file',
+                 'flags': ['-f', '--file'],
+                 'default': self.default_file,
+                },   
+                {
+                 'dest': 'help',
+                 'action': 'store_true',
+                 'flags': ['-h', '--help'],
+                },
+                {
+                 'dest': 'meta',
+                 'action': 'store_true',
+                 'flags': ['-m', '--meta'],
+                },
+                {
+                 'dest': 'names',
+                 'nargs':'*',
+                 'flags': ['-n', '--names'],
+                 'default': self.default_names,
+                },
+                {
+                 'dest': 'path',
+                 'flags': ['-p', '--path'],
+                 'default': self.default_path,
+                },                      
+                {
+                 'dest': 'recursively',
+                 'action': 'store_true',
+                 'flags': ['-r', '--recursively'],
+                },
+                {
+                 'dest': 'tags',
+                 'nargs':'*',
+                 'flags': ['-t', '--tags'],
+                 'default': self.default_tags,
+                },                                                     
+                {
+                 'action': 'version',
+                 'flags': ['-V', '--version'],
+                 'version': 'Run '+str(version),               
+                },                                                             
+            ],        
+        }
     
     
 settings = Settings()
