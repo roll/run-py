6f3c9d47da216c9217e6cde549685412340fffda
roll <roll@respect31.com>
2013-12-25 12:45:34 +0400

    added tests to finder

--- run/finder.py
+++ run/finder.py
@@ -3,49 +3,51 @@ import re
 import sys
 import inspect
 import importlib
-from .module import Module
-from .settings import settings
 
 class Finder:
 
     #Public
 
-    def find(self, path, **constraints):
-        files = self._find_files(path, names=settings.default_runfile)
-        modules = self._find_classes(files, bases=[Module])
-        filtered_modules = self._filter_classes(modules, constraints)
-        return filtered_modules
+    @classmethod
+    def find(cls, base_dir, file_pattern, **object_filters):
+        files = cls._find_files(base_dir, pattern=file_pattern)
+        modules = cls._import_modules(files)
+        objects = cls._find_classes(modules)
+        filtered_objects = cls._filter_objects(objects, **object_filters)
+        return filtered_objects
             
-    #Protected        
+    #Protected
     
-    def _find_files(self, path, names=[]):
-        """Find files recursively in path
-           where not names or filename in names"""      
+    @staticmethod
+    def _find_files(base_dir, file_pattern=''):
         files = []
-        for dirpath, _, filenames in os.walk(path):
-            for filename in filenames:
-                if filename in names:
-                    files.append(os.path.join(dirpath, filename))
+        for dir_path, _, file_names in os.walk(base_dir):
+            for file_name in file_names:
+                if re.match(file_pattern, file_name):
+                    files.append(os.path.join(dir_path, file_name))
         return files
-        
-    def _find_classes(self, files, bases=[]):
-        """Find and import classes of files
-           where not bases or issubclass(class, tuple(bases))"""
-        classes = []
+    
+    @staticmethod    
+    def _import_modules(files):
+        modules = []
         for file in files:
-            dirname, filename = os.path.split(os.path.abspath(file))
-            modulename = re.sub('\.pyc?', '', filename)
-            sys.path.insert(dirname)
-            module = importlib.import_module(modulename)
+            dir_name, file_name = os.path.split(os.path.abspath(file))
+            module_name = re.sub('\.pyc?', '', file_name)
+            sys.path.insert(0, dir_name)
+            module = importlib.import_module(module_name)
+            modules.append(module)
+        return modules
+        
+    @staticmethod        
+    def _get_objects(modules):
+        objects = []
+        for module in modules:
             for name in dir(module):
                 attr = getattr(module, name)
-                if (isinstance(attr, type) and
-                    inspect.getmodule(attr) == module and
-                    (not bases or issubclass(attr, tuple(bases))) and
-                    not inspect.isabstract(attr)):
-                    classes.append[attr]
-        return classes
+                if inspect.getmodule(attr) == module:
+                    objects.append(attr)
+        return objects
     
-    def _filter_classes(self, classes, constraints):
-        "Filter classes using constraints"
-        pass
+    @staticmethod    
+    def _filter_objects(objects, **object_filters):
+        return objects
---
+++ tests/component/finder/fixtures/runfile.py
@@ -0,0 +1,7 @@
+from run import Module
+
+class Module1(Module):
+    
+    #Public
+    
+    pass
