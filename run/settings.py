c5f168be4dfcec46078a93fe2d5acf85bd39cef1
roll <roll@respect31.com>
2013-12-26 11:16:14 +0400

    added dummy help message to console API

--- run/settings.py
+++ run/settings.py
@@ -18,79 +18,98 @@ class Settings(Settings):
     def command_schema(self):
         return {
             'prog': 'run',
-            'add_help': True,            
+            'add_help': False,            
             'arguments': [
                 {
                  'name': 'attribute',
                  'nargs': '?',
                  'default': None,
+                 'help': 'Attribute to process.',
                 },
                 {
                  'name': 'arguments',
                  'nargs':'*',
                  'default': self.default_arguments,
+                 'help': 'Arguments for attribute.',
                 },    
                 {
                  'dest': 'debug',
                  'action': 'store_true',
                  'flags': ['-d', '--debug'],
+                 'help': 'Enable debug mode.',
                 }, 
                 {
                  'dest': 'existent',
                  'action': 'store_true',
                  'flags': ['-e', '--existent'],
-                },                              
+                 'help': 'Process only existen attributes.',
+                }, 
+                {
+                 'action': 'help',
+                 'flags': ['-h', '--help'],
+                 'help': 'Display this help message.',                         
+                },                                      
                 {
                  'dest': 'info',
                  'action': 'store_true',
                  'flags': ['-i', '--info'],
+                 'help': 'Display attribute information.',                 
                 },                          
                 {
                  'dest': 'file',
                  'flags': ['-f', '--file'],
                  'default': self.default_file,
+                 'help': 'Runfile filename pattern.',                 
                 },
                 {
                  'dest': 'list',
                  'action': 'store_true',
                  'flags': ['-l', '--list'],
+                 'help': 'Display attribute attributes.',                 
                 },                          
                 {
                  'dest': 'meta',
                  'action': 'store_true',
                  'flags': ['-m', '--meta'],
+                 'help': 'Display attribute meta.',                 
                 },
                 {
                  'dest': 'names',
                  'nargs':'*',
                  'flags': ['-n', '--names'],
                  'default': self.default_names,
+                 'help': 'Main modules names to match.',
                 },
                 {
                  'dest': 'path',
                  'flags': ['-p', '--path'],
                  'default': self.default_path,
+                 'help': 'Base directory path.',
                 },                      
                 {
                  'dest': 'recursively',
                  'action': 'store_true',
                  'flags': ['-r', '--recursively'],
+                 'help': 'Enable finding runfiles recursively.',
                 },
                 {
                  'dest': 'tags',
                  'nargs':'*',
                  'flags': ['-t', '--tags'],
                  'default': self.default_tags,
+                 'help': 'Main modules tags to match.',                 
                 },   
                 {
                  'dest': 'verbose',
                  'action': 'store_true',
                  'flags': ['-v', '--verbose'],
+                 'help': 'Enable verbose mode.',                 
                 },                                                                                
                 {
                  'action': 'version',
                  'flags': ['-V', '--version'],
-                 'version': 'Run '+str(version),               
+                 'version': 'Run '+str(version),
+                 'help': 'Display the program version.',                          
                 },                                                             
             ],        
         }
