39a52def6dd6c7ebc762b3ee4269f8c347c87177
roll <roll@respect31.com>
2013-12-29 13:25:40 +0400

    rm signal storage in dispatcher

--- run/dispatcher.py
+++ run/dispatcher.py
@@ -4,13 +4,11 @@ class Dispatcher:
     
     def __init__(self):
         self._handlers = []
-        self._signals = []
     
     def add_handler(self, handler):
         self._handlers.append(handler)
     
     def add_signal(self, signal):
-        self._signals.append(signal)
         for handler in self._handlers:
             handler.handle(signal)
         
