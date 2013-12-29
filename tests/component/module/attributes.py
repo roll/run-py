a67470b51bc4589d3229990218cd3110604300fa
roll <roll@respect31.com>
2013-12-29 11:27:50 +0400

    added MAttributes test stub

---
+++ tests/component/module/attributes.py
@@ -0,0 +1,10 @@
+import unittest
+from run.module.attributes import ModuleAttributes
+
+class ModuleAttributesTest(unittest.TestCase):
+
+    #Public
+
+    def test(self):
+        attributes = ModuleAttributes()
+    
