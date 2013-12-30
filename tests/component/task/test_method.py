a1ea2c4264e02101e45b0795f4b09a4020ac7ec0
roll <roll@respect31.com>
2013-12-30 08:37:48 +0400

    impl MethodTask test

--- tests/component/task/test_method.py
+++ tests/component/task/test_method.py
@@ -1,9 +1,21 @@
 import unittest
 from run.task.method import MethodTask
 
+#Tests
+
 class MethodTaskTest(unittest.TestCase):
 
     #Public
 
     def test(self):
-        pass
+        task = MethodTask(mock_method, module=None)
+        self.assertEqual(task.meta_signature, '(module, value)')
+        self.assertEqual(task.meta_docstring, 'docstring')
+        self.assertEqual(task.complete('value'), 'value')
+    
+    
+#Fixtures
+
+def mock_method(module, value):
+    """docstring"""
+    return value
