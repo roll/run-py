afb63140c53af187b2f911cc65673902c14561d8
roll <roll@respect31.com>
2013-12-30 13:49:14 +0400

    Revert "min"
    This reverts commit 141e31744bd1cd49392daaf5348736970ad52b4a.

--- tests/component/var/test_value.py
+++ tests/component/var/test_value.py
@@ -5,6 +5,6 @@ class ValueVarTest(unittest.TestCase):
 
     #Public
 
-    def test_retrieve(self):
+    def test(self):
         var = ValueVar('value', module=None)
         self.assertEqual(var.retrieve(), 'value')
