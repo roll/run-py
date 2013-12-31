4c7c287ab0747579f8ad5dd7db319e6c95846926
roll <roll@respect31.com>
2013-12-31 10:02:57 +0400

    added DAB test stub

---
+++ tests/component/dependent/test_builder.py
@@ -0,0 +1,9 @@
+import unittest
+from run.dependent.builder import DependentAttributeBuilder
+
+class DependentAttributeBuilderTest(unittest.TestCase):
+
+    #Public
+
+    def test(self):
+        pass
