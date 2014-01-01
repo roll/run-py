4da3da960e231fc59b73ee617de82c274675e9de
roll <roll@respect31.com>
2014-01-01 07:48:59 +0400

    added MM test stub instead of A test

--- tests/component/dependent/test_dependent.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from run.dependent.dependent import DependentAttribute
-
-class DependentAttributeTest(unittest.TestCase):
-
-    #Public
-
-    def test(self):
-        pass
---
+++ tests/component/module/test_metaclass.py
@@ -0,0 +1,9 @@
+import unittest
+from run.module.metaclass import ModuleMetaclass
+
+class ModuleMetaclassTest(unittest.TestCase):
+
+    #Public
+
+    def test(self):
+        pass
