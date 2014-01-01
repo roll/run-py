c78144d79f743146384704f2991977d694f8da49
roll <roll@respect31.com>
2014-01-01 14:47:57 +0400

    min

--- tests/component/module/test_module.py
+++ tests/component/module/test_module.py
@@ -1,9 +1,20 @@
 import unittest
 from run.module.module import Module
 
+#Tests
+
 class ModuleTest(unittest.TestCase):
 
     #Public
 
     def test(self):
         pass
+    
+    
+#Fixtures
+
+class MockModule(Module):
+
+    #Public
+
+    pass
