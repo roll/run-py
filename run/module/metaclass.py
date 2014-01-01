604e8dad87995625fbf0b5e3834c3700eee7c51c
roll <roll@respect31.com>
2014-01-01 08:18:38 +0400

    refactored MM

--- run/module/metaclass.py
+++ run/module/metaclass.py
@@ -10,19 +10,30 @@ class ModuleMetaclass(AttributeMetaclass):
      
     def __new__(cls, name, bases, dct):
         for key, attr in dct.items():
-            if (not key.startswith('_') and
-                not key.startswith('meta_') and
-                not isinstance(attr, type) and
-                not isinstance(attr, Attribute) and
-                not isinstance(attr, AttributeBuilder)):
-                if callable(attr):
-                    dct[key] = MethodTask(attr)
-                elif inspect.isdatadescriptor(attr):
-                    dct[key] = PropertyVar(attr)
-                else:
-                    dct[key] = ValueVar(attr)
+            if key.startswith('_'):
+                continue
+            elif key.startswith('meta_'):
+                continue
+            elif isinstance(attr, type):
+                continue
+            elif isinstance(attr, cls._attribute_class):
+                continue
+            elif isinstance(attr, cls._attribute_builder_class):
+                continue
+            elif callable(attr):
+                dct[key] = cls._method_task_class(attr)
+            elif inspect.isdatadescriptor(attr):
+                dct[key] = cls._property_var_class(attr)
+            else:
+                dct[key] = cls._value_var_class(attr)
         return super().__new__(cls, name, bases, dct)
     
     #Protected
     
     _builder_class = ModuleBuilder
+    
+    _attribute_class = Attribute
+    _attribute_builder_class = AttributeBuilder
+    _method_task_class = MethodTask
+    _property_var_class = PropertyVar
+    _value_var_class = ValueVar
