580410bde34ed4edee393dc320928817cade3914
roll <roll@respect31.com>
2014-01-03 13:42:47 +0400

    impl StackTest

--- run/stack.py
+++ run/stack.py
@@ -9,15 +9,18 @@ class Stack(list):
     @property
     def formatted(self):
         names = []
-        previous = self[0]
-        names.append(previous.meta_qualname)
-        for attribute in self[1:]:
-            current = attribute
-            if current.meta_module == previous.meta_module:
-                names.append(current.meta_name)
-            else:
-                names.append(current.meta_qualname) 
-            previous = current
+        if len(self) == 1:
+            names = self
+        if len(self) >= 2:
+            previous = self[0]
+            names.append(previous.meta_qualname)
+            for attribute in self[1:]:
+                current = attribute
+                if current.meta_module == previous.meta_module:
+                    names.append(current.meta_name)
+                else:
+                    names.append(current.meta_qualname) 
+                previous = current
         formatted = '/'.join(names)
         return formatted
             
--- tests/component/test_stack.py
+++ tests/component/test_stack.py
@@ -1,9 +1,55 @@
 import unittest
 from run.stack import Stack
 
+#Tests
+
 class StackTest(unittest.TestCase):
 
     #Public
 
-    def test(self):
-        pass
+    def setUp(self):
+        self.stack = Stack()
+    
+    def test_push(self):
+        self.stack.push('attribute')
+        self.assertEqual(self.stack, ['attribute'])
+
+    def test_pop(self):
+        self.stack.push('attribute')
+        self.assertEqual(self.stack.pop(), 'attribute')
+        self.assertFalse(self.stack)
+        
+    def test_formatted_0_attributes(self):
+        self.assertEqual(self.stack.formatted, '')
+        
+    def test_formatted_1_attributes(self):
+        self.stack.push('attribute')
+        self.assertEqual(self.stack.formatted, 'attribute')
+        
+    def test_formatted_attributes_with_same_modules(self):
+        self.stack.push(MockAttribute1())
+        self.stack.push(MockAttribute1())
+        self.assertEqual(self.stack.formatted, 
+                         'module1.attribute1/attribute1')
+        
+    def test_formatted_attributes_with_different_modules(self):
+        self.stack.push(MockAttribute1())
+        self.stack.push(MockAttribute2())
+        self.assertEqual(self.stack.formatted, 
+                         'module1.attribute1/module2.attribute2')                 
+        
+        
+#Fixtures
+
+class MockAttribute1:
+    
+    meta_module = 'module1'
+    meta_name = 'attribute1'
+    meta_qualname = 'module1.attribute1'
+    
+    
+class MockAttribute2:
+    
+    meta_module = 'module2'
+    meta_name = 'attribute2'
+    meta_qualname = 'module2.attribute2'    
