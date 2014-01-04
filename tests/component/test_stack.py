76cd52a6487eb4e7c4bbc2de0dccfbcb30f5c084
roll <roll@respect31.com>
2014-01-04 13:49:48 +0400

    refactored StackTest

--- tests/component/test_stack.py
+++ tests/component/test_stack.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import Mock
 from run.stack import Stack
 
 #Tests
@@ -9,6 +10,14 @@ class StackTest(unittest.TestCase):
 
     def setUp(self):
         self.stack = Stack()
+        self.attribute1 = Mock(
+            meta_module='module1',
+            meta_name='attribute1',
+            meta_qualname='module1.attribute1')
+        self.attribute2 = Mock(
+            meta_module='module2',
+            meta_name='attribute2',
+            meta_qualname='module2.attribute2')        
     
     def test_push(self):
         self.stack.push('attribute')
@@ -23,33 +32,17 @@ class StackTest(unittest.TestCase):
         self.assertEqual(str(self.stack), '')
         
     def test___str___1_attributes(self):
-        self.stack.push(MockAttribute1())
+        self.stack.push(self.attribute1)
         self.assertEqual(str(self.stack), 'module1.attribute1')
         
     def test___str___attributes_with_same_modules(self):
-        self.stack.push(MockAttribute1())
-        self.stack.push(MockAttribute1())
+        self.stack.push(self.attribute1)
+        self.stack.push(self.attribute1)
         self.assertEqual(str(self.stack), 
                          'module1.attribute1/attribute1')
         
     def test___str___attributes_with_different_modules(self):
-        self.stack.push(MockAttribute1())
-        self.stack.push(MockAttribute2())
+        self.stack.push(self.attribute1)
+        self.stack.push(self.attribute2)
         self.assertEqual(str(self.stack), 
-                         'module1.attribute1/module2.attribute2')                 
-        
-        
-#Fixtures
-
-class MockAttribute1:
-    
-    meta_module = 'module1'
-    meta_name = 'attribute1'
-    meta_qualname = 'module1.attribute1'
-    
-    
-class MockAttribute2:
-    
-    meta_module = 'module2'
-    meta_name = 'attribute2'
-    meta_qualname = 'module2.attribute2'    
+                         'module1.attribute1/module2.attribute2')                    
