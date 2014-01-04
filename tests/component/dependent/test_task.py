1b2404a2896116e60e0262e715bd92619bd9b306
roll <roll@respect31.com>
2014-01-04 15:31:50 +0400

    refactored DependentAttributeTaskTest

--- tests/component/dependent/test_task.py
+++ tests/component/dependent/test_task.py
@@ -2,20 +2,17 @@ import unittest
 from unittest.mock import Mock
 from run.dependent.task import DependentAttributeTask
 
-#Tests
-
 class DependentAttributeTaskTest(unittest.TestCase):
 
     #Public
     
     def setUp(self):
         self.task = DependentAttributeTask('task')
+        self.attribute = Mock(meta_module=Mock(task=Mock()))
         
     def test___call__(self):
-        attr = MockAttribute()
-        attr.meta_module.task.reset_mock()
-        self.task(attr)
-        attr.meta_module.task.assert_called_with()
+        self.task(self.attribute)
+        self.attribute.meta_module.task.assert_called_with()
 
     def test_name(self):
         self.assertEqual(self.task.name, 'task')
@@ -38,24 +35,15 @@ class DependentAttributeTaskTest_with_args_and_kwargs(unittest.TestCase):
         self.args = ('arg',)
         self.kwargs = {'kwarg': 'kwarg'}
         self.task = DependentAttributeTask(('task', self.args, self.kwargs))
+        self.attribute = Mock(meta_module=Mock(task=Mock()))
         
     def test___call__(self):
-        attr = MockAttribute()
-        attr.meta_module.task.reset_mock()
-        self.task(attr)
-        attr.meta_module.task.assert_called_with(*self.args, **self.kwargs)
+        self.task(self.attribute)
+        self.attribute.meta_module.task.assert_called_with(
+            *self.args, **self.kwargs)
         
     def test_args(self):
         self.assertEqual(self.task.args, ('arg',))
         
     def test_kwargs(self):
         self.assertEqual(self.task.kwargs, {'kwarg': 'kwarg'})       
-    
-    
-#Fixtures
-
-class MockAttribute:
-    
-    #Public
-    
-    meta_module = Mock(task=Mock())
