419fd912ad61555d372872f5610917b995e0e1b0
roll <roll@respect31.com>
2014-01-04 18:11:02 +0400

    refactored AttributeBuilderTest

--- tests/component/attribute/test_builder.py
+++ tests/component/attribute/test_builder.py
@@ -2,8 +2,6 @@ import unittest
 from unittest.mock import Mock, call
 from run.attribute.builder import AttributeBuilder
 
-#Tests
-
 class AttributeBuilderTest(unittest.TestCase):
 
     #Public
@@ -11,18 +9,21 @@ class AttributeBuilderTest(unittest.TestCase):
     def setUp(self):
         self.args = ('arg1', 'arg2')
         self.kwargs = {'kwarg1': 'kwarg1', 'kwarg2': 'kwarg2'}
+        self.mock_set = Mock(apply = Mock())
+        MockAttributeBuilder = self._make_mock_attribute_builder_class()
+        self.MockAttribute = self._make_mock_attribute_class()
         self.builder = MockAttributeBuilder(
-            MockAttribute, *self.args, **self.kwargs)
+            self.MockAttribute, *self.args, **self.kwargs)
 
     def test___call__(self):
         self.builder.attr2 = 'value2'
         obj1 = self.builder()
         obj2 = self.builder()
-        self.assertIsInstance(obj1, MockAttribute)
-        self.assertIsInstance(obj2, MockAttribute)
+        self.assertIsInstance(obj1, self.MockAttribute)
+        self.assertIsInstance(obj2, self.MockAttribute)
         obj1.__init__.assert_called_with('arg1', kwarg1='kwarg1')
         obj2.__init__.assert_called_with('arg1', kwarg1='kwarg1')
-        MockSet.apply.assert_has_calls([call(obj1), call(obj2)])
+        self.mock_set.apply.assert_has_calls([call(obj1), call(obj2)])
     
     def test___getattr__(self):
         self.assertEqual(self.builder.attr1, 'value1') 
@@ -32,32 +33,22 @@ class AttributeBuilderTest(unittest.TestCase):
            
     def test___setattr__(self):
         self.builder.attr2 = 'value2'
-        self.builder._set_class.assert_called_with('attr2', 'value2')            
-    
-
-#Fixtures
-
-class MockSet:
-    
-    #Public
-    
-    apply = Mock()
-    
-    
-class MockAttribute:
-
-    #Public
-    
-    __init__ = Mock(return_value=None)
-    attr1 = 'value1' 
-    
-    def __meta_init__(self, args, kwargs):
-        args.remove('arg2')
-        kwargs.pop('kwarg2')
-
-
-class MockAttributeBuilder(AttributeBuilder): 
-      
+        self.builder._set_class.assert_called_with('attr2', 'value2')
+        
     #Protected
     
-    _set_class = Mock(return_value=MockSet())
+    def _make_mock_attribute_class(self):
+        class MockAttribute:
+            #Public
+            __init__ = Mock(return_value=None)
+            attr1 = 'value1' 
+            def __meta_init__(self, args, kwargs):
+                args.remove('arg2')
+                kwargs.pop('kwarg2')
+        return MockAttribute
+    
+    def _make_mock_attribute_builder_class(self):             
+        class MockAttributeBuilder(AttributeBuilder): 
+            #Protected
+            _set_class = Mock(return_value=self.mock_set)
+        return MockAttributeBuilder
