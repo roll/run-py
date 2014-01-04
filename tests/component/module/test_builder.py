6d563cd49a1e2306bb5ff833765360f85bff9ea9
roll <roll@respect31.com>
2014-01-04 14:44:38 +0400

    refactored ModuleBuilderTest

--- tests/component/module/test_builder.py
+++ tests/component/module/test_builder.py
@@ -2,28 +2,31 @@ import unittest
 from unittest.mock import Mock
 from run.module.builder import ModuleBuilder
 
-#Tests
-
 class ModuleBuilderTest(unittest.TestCase):
 
     #Public
     
     def setUp(self):
-        self.builder = MockModuleBuilder(MockModule2)
+        MockModuleBuilder = self._make_mock_module_builder_class()
+        self.MockModule = self._make_mock_module_class()
+        self.builder = MockModuleBuilder(self.MockModule)
     
     def test__create_object(self):
         obj = self.builder._create_object()
         self.assertIsInstance(obj, self.builder._builded_class)
         
     def test__builded_class(self):
-        self.assertTrue(issubclass(self.builder._builded_class, MockModule2))
-        self.assertTrue(issubclass(self.builder._builded_class, MockModule1))
+        self.assertTrue(issubclass(self.builder._builded_class, 
+                                   self.MockModule))
         
     def test__builded_class_name(self):
-        self.assertEqual(self.builder._builded_class_name, 'MockModule2Builded')    
+        self.assertEqual(
+            self.builder._builded_class_name, 
+                         'MockModuleBuilded')    
      
     def test__builded_class_bases(self):
-        self.assertEqual(self.builder._builded_class_bases, (MockModule2,))
+        self.assertEqual(self.builder._builded_class_bases, 
+                         (self.MockModule,))
         
     def test__builded_class_dict(self):
         self.assertEqual(self.builder._builded_class_dict, {
@@ -33,27 +36,21 @@ class ModuleBuilderTest(unittest.TestCase):
             'attr2': 'value2',
         })
     
-    
-#Fixtures
-
-class MockModule1:
-    
-    #Public
-    
-    __meta_init__ = lambda *args, **kwargs: None
-    attr1 = Mock(return_value='value1')
-
-
-class MockModule2(MockModule1):
-    """docstring"""
-
-    #Public
-
-    attr2 = Mock(return_value='value2')
-
-
-class MockModuleBuilder(ModuleBuilder):
-
     #Protected
-
-    _attribute_builder_class = Mock
+    
+    def _make_mock_module_class(self):
+        class BaseMockModule:
+            #Public
+            __meta_init__ = lambda *args, **kwargs: None
+            attr1 = Mock(return_value='value1')
+        class MockModule(BaseMockModule):
+            """docstring"""
+            #Public
+            attr2 = Mock(return_value='value2')
+        return MockModule
+    
+    def _make_mock_module_builder_class(self):    
+        class MockModuleBuilder(ModuleBuilder):
+            #Protected
+            _attribute_builder_class = Mock
+        return MockModuleBuilder
