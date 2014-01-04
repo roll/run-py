e2951ceff5e25e08619dffa9cd5be38a5b418c31
roll <roll@respect31.com>
2014-01-04 15:26:31 +0400

    refactored LoadModuleTest

--- tests/component/library/test_load.py
+++ tests/component/library/test_load.py
@@ -2,51 +2,37 @@ import unittest
 from unittest.mock import Mock
 from run.library.load import LoadModule
 
-#Tests
-
 class LoadModuleTest(unittest.TestCase):
 
     #Public
+    
+    def setUp(self):
+        self.kwargs = {
+            'names': 'names', 
+            'tags': 'tags', 
+            'basedir': 'basedir', 
+            'file_pattern': 'file_pattern', 
+            'recursively': 'recursively'}
 
     def test___new__(self):
-        module = MockLoadModule(
-            names='names', tags='tags', basedir='basedir', 
-            file_pattern='file_pattern', recursively='recursively')
-        self.assertIsInstance(module, MockModule)
-        (MockLoadModule._loader_class.assert_called_with(
-            names='names', tags='tags'))
-        MockLoader.load.assert_called_with(
+        mock_module = Mock()
+        MockLoadModule = self._make_mock_load_module_class([mock_module])
+        module = MockLoadModule(**self.kwargs)
+        self.assertIsInstance(module, Mock)
+        MockLoadModule._loader_class.assert_called_with(
+            names='names', tags='tags')
+        MockLoadModule._loader_class.return_value.load.assert_called_with(
             'basedir', 'file_pattern', 'recursively')
         
     def test___new___no_modules(self):
-        self.assertRaises(ImportError, NoModulesMockLoadModule)
+        MockLoadModule = self._make_mock_load_module_class([])
+        self.assertRaises(ImportError, MockLoadModule, **self.kwargs)
     
-
-#Fixtures
-
-class MockModule:
-    
-    #Public
-    
-    __init__ = Mock(return_value=None)
-    
-
-class MockLoader:
-
-    #Public
-
-    load = Mock(return_value=[MockModule])
-
-
-class MockLoadModule(LoadModule):
-
     #Protected
 
-    _loader_class = Mock(return_value=MockLoader())
-    
-
-class NoModulesMockLoadModule(LoadModule):
-
-    #Protected
-
-    _loader_class = Mock(return_value=Mock(load=Mock(return_value=[])))
+    def _make_mock_load_module_class(self, modules):
+        class MockLoadModule(LoadModule):
+            #Protected
+            _loader_class = Mock(return_value=Mock(
+                load=Mock(return_value=modules)))
+        return MockLoadModule
