80e61aa74c26fb3afbff48ab6afd466cc75c54ec
roll <roll@respect31.com>
2014-01-04 13:05:03 +0400

    added dyn mocks to ClusterTest

--- tests/component/test_cluster.py
+++ tests/component/test_cluster.py
@@ -10,6 +10,7 @@ class ClusterTest(unittest.TestCase):
     #Public
     
     def setUp(self):
+        MockCluster = self._make_mock_cluster_class()
         self.cluster_constructor = partial(MockCluster,
             names='names', tags='tags', basedir='basedir', 
             file_pattern='file_pattern', recursively='recursively',
@@ -35,46 +36,44 @@ class ClusterTest(unittest.TestCase):
         cluster = self.cluster_constructor(existent=True)
         self.assertEqual(cluster.attr2, [1])           
         
-
-#Fixtures
-
-    
-class MockModule1:
-
-    #Public
-
-    __init__ = Mock(return_value=None)
-    attr1 = 1
-    attr2 = 1
-        
-    
-class MockModule2:
-
-    #Public
-
-    __init__ = Mock(return_value=None)
-    attr1 = 2
-    
-    
-class MockModule3:
-
-    #Public
-
-    __init__ = Mock(return_value=None)
-    attr1 = 3
-    
-    
-class MockLoader:
-    
-    #Public
-    
-    __init__ = Mock(return_value=None)
-    load = Mock(return_value=[module for module in 
-        [MockModule1, MockModule2, MockModule3]])
-    
-    
-class MockCluster(Cluster):
-
     #Protected
-
-    _loader_class = MockLoader
+    
+    def _make_mock_module1_class(self):
+        class MockModule1:
+            #Public
+            __init__ = Mock(return_value=None)
+            attr1 = 1
+            attr2 = 1
+        return MockModule1
+    
+    def _make_mock_module2_class(self):
+        class MockModule2:
+            #Public
+            __init__ = Mock(return_value=None)
+            attr1 = 2
+        return MockModule2
+   
+    def _make_mock_module3_class(self):
+        class MockModule3:
+            #Public
+            __init__ = Mock(return_value=None)
+            attr1 = 3
+        return MockModule3  
+    
+    def _make_mock_loader_class(self):
+        MockModule1 = self._make_mock_module1_class()
+        MockModule2 = self._make_mock_module2_class()
+        MockModule3 = self._make_mock_module3_class()
+        class MockLoader:
+            #Public
+            __init__ = Mock(return_value=None)
+            load = Mock(return_value=[module for module in 
+                [MockModule1, MockModule2, MockModule3]])
+        return MockLoader
+    
+    def _make_mock_cluster_class(self):
+        MockLoader = self._make_mock_loader_class()
+        class MockCluster(Cluster):
+            #Protected
+            _loader_class = MockLoader
+        return MockCluster
