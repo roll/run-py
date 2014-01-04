d69e8e0fc7d880464ac5efb28823374fbf91f1d7
roll <roll@respect31.com>
2014-01-04 15:46:19 +0400

    refactored AttributeMetaclassTest

--- tests/component/attribute/test_metaclass.py
+++ tests/component/attribute/test_metaclass.py
@@ -3,42 +3,43 @@ from abc import ABCMeta
 from unittest.mock import Mock, MagicMock
 from run.attribute.attribute import AttributeMetaclass
 
-#Tests
-
 class AttributeMetaclassTest(unittest.TestCase):
 
     #Public
 
     def setUp(self):
-        self.metaclass = MockAttributeMetaclass
+        self.MockAttributeMetaclass = self._make_mock_attribute_metaclass()
+        self.MockAttribute = self._make_mock_attribute_class()
 
     def test(self):
-        self.assertTrue(issubclass(self.metaclass, ABCMeta))
+        self.assertTrue(issubclass(self.MockAttributeMetaclass, ABCMeta))
 
     def test___call__(self):
         args = ('arg1',)
         kwargs = {'kwarg1': 'kwarg1'}
-        attribute = MockAtrtibute(*args, **kwargs)
+        attribute = self.MockAttribute(*args, **kwargs)
         self.assertIsInstance(attribute, Mock)
-        (self.metaclass._builder_class.
-            assert_called_with(MockAtrtibute, *args, **kwargs))
-        self.assertFalse(self.metaclass._builder_class.return_value.called)
+        (self.MockAttributeMetaclass._builder_class.
+            assert_called_with(self.MockAttribute, *args, **kwargs))
+        (self.assertFalse(self.MockAttributeMetaclass._builder_class.
+            return_value.called))
     
     def test___call___with_module(self):
-        attribute = MockAtrtibute(module='module')
+        attribute = self.MockAttribute(module='module')
         self.assertIsInstance(attribute, MagicMock)
-        (self.metaclass._builder_class.
-            assert_called_with(MockAtrtibute, module='module'))
-        self.metaclass._builder_class.return_value.assert_called_with()    
-    
-    
-#Fixtures
-
-class MockAttributeMetaclass(AttributeMetaclass):
-
+        (self.MockAttributeMetaclass._builder_class.
+            assert_called_with(self.MockAttribute, module='module'))
+        (self.MockAttributeMetaclass._builder_class.
+            return_value.assert_called_with())
+        
     #Protected
-
-    _builder_class = Mock(return_value=Mock(return_value=MagicMock()))
     
+    def _make_mock_attribute_metaclass(self):
+        class MockAttributeMetaclass(AttributeMetaclass):
+            #Protected
+            _builder_class = Mock(return_value=Mock(return_value=MagicMock()))
+        return MockAttributeMetaclass
     
-class MockAtrtibute(metaclass=MockAttributeMetaclass): pass
+    def _make_mock_attribute_class(self):
+            class MockAtrtibute(metaclass=self.MockAttributeMetaclass): pass
+            return MockAtrtibute
