2995970ff0259ea6b6fa90d4cad96eda3c92ca60
roll <roll@respect31.com>
2014-01-14 07:44:55 +0400

    implemented TaskVarTest

--- run/task/partial.py
+++ run/task/partial.py
@@ -4,16 +4,17 @@ class PartialTask(Task):
 
     #Public
 
-    def __init__(self, task_name, *args, **kwargs):
-        self._task_name = task_name
+    def __init__(self, task, *args, **kwargs):
+        self._task = task
         self._args = args
         self._kwargs = kwargs
     
     def complete(self, *args, **kwargs):
+        task = getattr(self.meta_module, self._task)
         eargs = self._args+args
         ekwargs = self._kwargs
         ekwargs.update(kwargs) 
-        return self._task(*eargs, **ekwargs)
+        return task(*eargs, **ekwargs)
         
     #TODO: implement for partial
     @property
@@ -24,9 +25,3 @@ class PartialTask(Task):
     @property    
     def meta_docstring(self):
         return self._task.meta_docstring
-    
-    #Protected
-    
-    @property
-    def _task(self):
-        return getattr(self.meta_module, self._task)
