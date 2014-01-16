8b6a996647acf40a6393c49ce27a1689b3353c13
roll <roll@respect31.com>
2014-01-16 08:52:12 +0400

    removed test_find

--- run/modules/find.py
+++ run/modules/find.py
@@ -6,16 +6,10 @@ class FindModule(Module):
     #Public
 
     def find_files(self, *args, **kwargs):
-        return self._find_files_function(*args, **kwargs)
+        return find_files(*args, **kwargs)
     
     def find_strings(self, *args, **kwargs):
-        return self._find_strings_function(*args, **kwargs)
+        return find_strings(*args, **kwargs)
     
     def find_objects(self, *args, **kwargs):
-        return self._find_objects_function(*args, **kwargs)
-    
-    #Protected
-    
-    _find_files_function = staticmethod(find_files)
-    _find_strings_function = staticmethod(find_strings)
-    _find_objects_function = staticmethod(find_objects)
+        return find_objects(*args, **kwargs)
