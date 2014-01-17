60aef49599be99860fc09334265ea5c5b3a9585a
roll <roll@respect31.com>
2014-01-17 14:07:49 +0400

    added __init__ with method to MethodTask

--- run/task/method.py
+++ run/task/method.py
@@ -4,5 +4,8 @@ class MethodTask(FunctionTask):
     
     #Public
     
+    def __init__(self, method):
+        super().__init__(method)
+        
     def complete(self, *args, **kwargs):
         return self._function(self.meta_module, *args, **kwargs)
