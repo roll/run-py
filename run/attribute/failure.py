89ce6b13904e735015be87638a5c26cc4815334c
roll <roll@respect31.com>
2014-01-24 14:13:50 +0400

    min

--- run/attribute/failure.py
+++ run/attribute/failure.py
@@ -1,8 +1,6 @@
 from ..failure import Failure
 
 #TODO: implement
-#TODO: change exceptions raising style in core to more safe
-#(no potential recursion, etc)?
 class AttributeFailure(Failure):
 
     #Public
