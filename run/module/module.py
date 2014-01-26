7d3eb36ec18670f9a0d5d683b4ec41c5cac56c71
roll <roll@respect31.com>
2014-01-26 13:23:32 +0400

    improved Module.list/info/meta

--- run/module/module.py
+++ run/module/module.py
@@ -9,28 +9,31 @@ class Module(BaseModule):
         
     def list(self, attribute=None):
         "Print attributes"
-        names = []
-        if attribute and attribute in self.meta_attributes:
+        if attribute:
             attribute = self.meta_attributes[attribute]
-            attributes = attribute.meta_attributes
         else:
-            attributes = self.meta_attributes
-        for attribute in attributes.values():
-            names.append(attribute.meta_qualname)
-        for name in sorted(names):
-            self._meta_print_function(name)
+            attribute = self
+        names = []
+        if isinstance(attribute, BaseModule):
+            for attribute in attribute.meta_attributes.values():
+                names.append(attribute.meta_qualname)
+            for name in sorted(names):
+                self._meta_print_function(name)
+        else:
+            raise TypeError('Attribute "{attribute}" is not a module.'.
+                            format(attribute=attribute))
 
     def info(self, attribute=None):
         "Print information"
-        if attribute and attribute in self.meta_attributes:
+        if attribute:
             attribute = self.meta_attributes[attribute]
-            self._meta_print_function(attribute.meta_info)
         else:
-            self._meta_print_function(self.meta_info)
+            attribute = self
+        self._meta_print_function(attribute.meta_info)
         
     def meta(self, attribute=None):
         "Print metadata"
-        if attribute and attribute in self.meta_attributes:
+        if attribute:
             attribute = self.meta_attributes[attribute]
         else:
             attribute = self
