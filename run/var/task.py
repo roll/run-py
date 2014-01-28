9142eb1af72010f2e7b11ae6fe2b3d5f01164f3a
roll <roll@respect31.com>
2014-01-28 10:35:25 +0400

    added is_builtin to TaskVar

--- run/var/task.py
+++ run/var/task.py
@@ -4,26 +4,37 @@ class TaskVar(Var):
     
     #Public
     
+    def __meta_init__(self, args, kwargs):
+        super().__meta_init__(args, kwargs)
+        self._is_builtin = kwargs.pop('is_builtin', False)
+        
     def __init__(self, task, *args, **kwargs):
-        self._task = task
+        self._task_name = task
         self._args = args
         self._kwargs = kwargs
  
     def retrieve(self):
-        return self._base_task(*self._args, **self._kwargs)
+        if self._is_builtin:
+            #Complete without resolving requirements, triggers
+            result = self._task.complete(*self._args, **self._kwargs)
+        else:
+            result = self._task(*self._args, **self._kwargs)
+        return result    
         
     @property
     def meta_signature(self):
-        return self._base_task.meta_signature
+        return self._task.meta_signature
 
     @property    
     def meta_docstring(self):
-        return self._base_task.meta_docstring
+        return self._task.meta_docstring
     
     #Protected
     
     @property
-    def _base_task(self):
-        task = self.meta_module.meta_attributes[self._task]
-        base_task = task.meta_builder(module=self.meta_module)
-        return base_task
+    def _task(self):
+        task = getattr(self.meta_module, self._task_name)
+        if self._is_builtin:
+            #Rebuild task with rebase on own module
+            task = task.meta_builder(module=self.meta_module)
+        return task
--- tests/component/var/test_task.py
+++ tests/component/var/test_task.py
@@ -7,21 +7,23 @@ class PartialVarTest(unittest.TestCase):
     #Public
 
     def setUp(self):
-        self.base_task = Mock(return_value='value')        
-        MockVar = self._make_mock_var_class(self.base_task)
+        self.builtin_task = Mock(return_value='value')        
+        MockVar = self._make_mock_var_class(self.builtin_task)
         self.args = ('arg1',)
         self.kwargs = {'kwarg1': 'kwarg1'}
         self.var = MockVar('task', *self.args, module=None, **self.kwargs)
         
     def test_retrieve(self):
         self.assertEqual(self.var.retrieve(), 'value')
-        self.base_task.assert_called_with('arg1', kwarg1='kwarg1')
+        self.var.meta_module.task.assert_called_with('arg1', kwarg1='kwarg1')
         
     #Protected
     
-    def _make_mock_var_class(self, base_task):
+    def _make_mock_var_class(self, builtin_task):
         class MockVar(TaskVar):
             #Public
-            meta_module = Mock(meta_attributes={
-                'task': Mock(meta_builder=Mock(return_value=base_task))})
+            meta_module = Mock(task=Mock(
+                return_value='value',
+                meta_builder=Mock(
+                    return_value=builtin_task)))
         return MockVar
