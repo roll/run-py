0d57d3014fa5cee5bbeca02352635c46a1396650
roll <roll@respect31.com>
2014-01-28 10:29:46 +0400

    added is_builtin to PartialTask

--- run/task/partial.py
+++ run/task/partial.py
@@ -4,28 +4,39 @@ from .task import Task
 class PartialTask(Task):
 
     #Public
+    
+    def __meta_init__(self, args, kwargs):
+        super().__meta_init__(args, kwargs)
+        self._is_builtin = kwargs.pop('is_builtin', False)
 
     def __init__(self, task, *args, **kwargs):
-        self._task = task
+        self._task_name = task
         self._args = args
         self._kwargs = kwargs
     
     def complete(self, *args, **kwargs):
         eargs = self._args+args
         ekwargs = copy(self._kwargs)
-        ekwargs.update(kwargs) 
-        return self._base_task(*eargs, **ekwargs)
+        ekwargs.update(kwargs)
+        if self._is_builtin:
+            #Complete without resolving requirements, triggers
+            result = self._task.complete(*eargs, **ekwargs)
+        else:
+            result = self._task(*eargs, **ekwargs)
+        return result
         
     @property
     def meta_signature(self):
         return self._base_task.meta_signature
 
-    @property    
+    @property
     def meta_docstring(self):
         return self._base_task.meta_docstring
     
     @property
-    def _base_task(self):
-        task = self.meta_module.meta_attributes[self._task]
-        base_task = task.meta_builder(module=self.meta_module)
-        return base_task  
+    def _task(self):
+        task = getattr(self.meta_module, self._task_name)
+        if self._is_builtin:
+            #Rebuild task with rebase on own module
+            task = task.meta_builder(module=self.meta_module)
+        return task
--- tests/component/task/test_partial.py
+++ tests/component/task/test_partial.py
@@ -7,28 +7,30 @@ class PartialTaskTest(unittest.TestCase):
     #Public
 
     def setUp(self):
-        self.base_task = Mock(return_value='value')
-        MockTask = self._make_mock_task_class(self.base_task)
+        self.builtin_task = Mock(return_value='builtin_value')
+        MockTask = self._make_mock_task_class(self.builtin_task)
         self.args = ('arg1',)
         self.kwargs = {'kwarg1': 'kwarg1'}
         self.task = MockTask('task', *self.args, module=None, **self.kwargs)
         
     def test_complete(self):
         self.assertEqual(self.task.complete(), 'value')
-        self.base_task.assert_called_with('arg1', kwarg1='kwarg1')
+        self.task.meta_module.task.assert_called_with('arg1', kwarg1='kwarg1')
 
     def test_complete_with_args_and_kwargs(self):
         args = ('arg2',)
         kwargs = {'kwarg2': 'kwarg2'}
         self.assertEqual(self.task.complete(*args, **kwargs), 'value')
-        self.base_task.assert_called_with(
+        self.task.meta_module.task.assert_called_with(
             'arg1', 'arg2', kwarg1='kwarg1', kwarg2='kwarg2')
         
     #Protected
     
-    def _make_mock_task_class(self, base_task):
+    def _make_mock_task_class(self, builtin_task):
         class MockTask(PartialTask):
             #Public
-            meta_module = Mock(meta_attributes={
-                'task': Mock(meta_builder=Mock(return_value=base_task))})
+            meta_module = Mock(task=Mock(
+                return_value='value',
+                meta_builder=Mock(
+                    return_value=builtin_task)))
         return MockTask
