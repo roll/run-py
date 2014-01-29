0ea57f02f18a4a52a7e7307f6b468a1110cf9c9a
roll <roll@respect31.com>
2014-01-29 13:57:12 +0400

    reimplemented MethodTask

--- run/task/method.py
+++ run/task/method.py
@@ -1,11 +1,16 @@
+from copy import copy
 from .function import FunctionTask
 
 class MethodTask(FunctionTask):
     
     #Public
     
-    def __init__(self, method):
-        super().__init__(method)
+    def __init__(self, method, *args, **kwargs):
+        super().__init__(method, *args, **kwargs)
         
     def invoke(self, *args, **kwargs):
-        return self._function(self.meta_module, *args, **kwargs)
+        eargs = copy(self._args)
+        eargs = eargs+args
+        ekwargs = copy(self._kwargs)
+        ekwargs.update(kwargs)
+        return self._function(self.meta_module, *eargs, **ekwargs)
