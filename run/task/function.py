2062cf7972f9dd63cb436828defd6dc4a456a2bc
roll <roll@respect31.com>
2014-01-29 13:46:06 +0400

    added initial args, kwargs to FunctionTask

--- run/task/function.py
+++ run/task/function.py
@@ -1,15 +1,22 @@
 import inspect
+from copy import copy
 from .task import Task
 
 class FunctionTask(Task):
     
     #Public
 
-    def __init__(self, function):
+    def __init__(self, function, *args, **kwargs):
         self._function = function
+        self._args = args
+        self._kwargs = kwargs
     
     def invoke(self, *args, **kwargs):
-        return self._function(*args, **kwargs)
+        eargs = copy(self._args)
+        eargs = eargs+args
+        ekwargs = copy(self._kwargs)
+        ekwargs.update(kwargs)
+        return self._function(*eargs, **ekwargs)
         
     @property
     def meta_signature(self):
