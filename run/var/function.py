40de9ee9502d0f597963f899735c557abd44ea51
roll <roll@respect31.com>
2014-01-29 13:48:53 +0400

    reimplemented FunctionVar as Var, FunctionTask

--- run/var/function.py
+++ run/var/function.py
@@ -1,18 +1,4 @@
-import inspect
+from ..task import FunctionTask
 from .var import Var
 
-class FunctionVar(Var):
-    
-    #Public
-
-    def __init__(self, function, *args, **kwargs):
-        self._function = function
-        self._args = args
-        self._kwargs = kwargs
-    
-    def invoke(self):
-        return self._function(*self._args, **self._kwargs)
-    
-    @property    
-    def meta_docstring(self):
-        return str(inspect.getdoc(self._function))
+class FunctionVar(Var, FunctionTask): pass
