d890ac60fd25b43a4d61156412ff43b6f75afc6a
roll <roll@respect31.com>
2014-01-29 13:58:05 +0400

    reimplemented MethodVar as Var,MethodTask

--- run/var/method.py
+++ run/var/method.py
@@ -1,11 +1,4 @@
-from .function import FunctionVar
+from ..task import MethodTask
+from .var import Var
 
-class MethodVar(FunctionVar):
-    
-    #Public
-    
-    def __init__(self, method, *args, **kwargs):
-        super().__init__(method, *args, **kwargs)   
-    
-    def invoke(self):
-        return self._function(self.meta_module, *self._args, **self._kwargs)
+class MethodVar(Var, MethodTask): pass
