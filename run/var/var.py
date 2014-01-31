e61bf9e16f5432f1185f82da4b44446a7f649c53
roll <roll@respect31.com>
2014-01-31 12:42:44 +0400

    added caching to Var

--- run/var/var.py
+++ run/var/var.py
@@ -1,12 +1,22 @@
 from abc import ABCMeta
+from box.functools import DEFAULT
 from ..task import Task
 from .signal import InitiatedVarSignal, ProcessedVarSignal
 
 class Var(Task, metaclass=ABCMeta):
     
     #Public
+    
+    def __meta_init__(self, args, kwargs):
+        super().__meta_init__(args, kwargs)
+        self._is_cache = kwargs.pop('is_cache', True)
+        self._cache = DEFAULT
 
     def __get__(self, module, module_class=None):
+        if self._is_cache:
+            if self._cache == DEFAULT:
+                self._cache = self()
+            return self._cache
         return self()
  
     def __set__(self, module, value):
