98aafec85b4d018b647e160d81f1de998e94c4af
roll <roll@respect31.com>
2014-01-31 07:38:50 +0400

    implemented AutoModule draft

--- run/module/auto.py
+++ run/module/auto.py
@@ -1,7 +1,29 @@
+from ..task import FunctionTask
 from .module import Module
 
 class AutoModule(Module):
 
     #Public
 
-    pass
+    def __init__(self, *objects):
+        self._objects = objects
+        for name, function in self._functions.items():
+            if not hasattr(type(self), name):
+                setattr(type(self), name, FunctionTask(function))
+                
+    #Protected
+    
+    @property
+    def _functions(self):
+        functions = {}
+        for obj in reversed(self._objects):
+            for name in dir(obj):
+                if name.startswith('_'):
+                    continue
+                value = getattr(obj, name)
+                if not callable(value):
+                    continue
+                if isinstance(value, type):
+                    continue
+                functions[name] = value
+        return functions                   
