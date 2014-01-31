1b64bdbcbb8898aa66a79e4ee6d983eb3b5b53ef
roll <roll@respect31.com>
2014-01-31 10:26:41 +0400

    rebased modules.find on AutoModule

--- run/modules/find.py
+++ run/modules/find.py
@@ -1,15 +1,8 @@
-from run import Module
-from box.findtools import find_files, find_strings, find_objects
+from run import AutoModule
+from box import findtools
 
-class FindModule(Module):
+class FindModule(AutoModule):
     
     #Public
 
-    def find_files(self, *args, **kwargs):
-        return find_files(*args, **kwargs)
-    
-    def find_strings(self, *args, **kwargs):
-        return find_strings(*args, **kwargs)
-    
-    def find_objects(self, *args, **kwargs):
-        return find_objects(*args, **kwargs)
+    _default_objects = [findtools]
