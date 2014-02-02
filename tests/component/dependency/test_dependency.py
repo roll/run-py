b0886a60e44a0793e7be723b03fff683877ecbda
roll <roll@respect31.com>
2014-02-02 13:43:47 +0400

    recovery DependencyTest

--- tests/component/dependency/test_dependency.py
+++ tests/component/dependency/test_dependency.py
@@ -7,74 +7,66 @@ class DependencyTest(unittest.TestCase):
     #Public
     
     def setUp(self):
-        MockConstraint = self._make_mock_constraint_class()
+        self.attribute = Mock(meta_module=Mock(
+            task1=Mock(), task2=Mock()))
+        MockDependency = self._make_mock_dependency_class()
         self.tasks = ['task1', 'task2']
-        self.constraint = MockConstraint(self.tasks)
-        
-    def setUp(self):
-        self.dependency = TaskDependency('task')
-        self.attribute = Mock(meta_module=Mock(task=Mock()))
+        self.dependency = MockDependency(self.tasks)
         
     def test___call__with_function_is_builder(self):
-        builder = self.constraint._attribute_class('method')
-        decorated = self.constraint(builder)
+        builder = self.dependency._method_task_class('method')
+        decorated = self.dependency(builder)
         self.assertIs(decorated, builder)
-        self.constraint._apply_dependency.assert_called_with(decorated)
+        self.dependency._add_dependency.assert_called_with(decorated)
         
     def test___call__with_function_is_raw_function(self):
         function = 'function'
-        decorated = self.constraint(function)
-        self.assertIsInstance(decorated, self.constraint._builder_class)
-        self.constraint._attribute_class.assert_called_with(function)
-        self.constraint._apply_dependency.assert_called_with(decorated)
+        decorated = self.dependency(function)
+        self.assertIsInstance(decorated, self.dependency._builder_class)
+        self.dependency._method_task_class.assert_called_with(function)
+        self.dependency._add_dependency.assert_called_with(decorated)
         
     def test_resolve(self):
         self.dependency.resolve(self.attribute)
-        self.attribute.meta_module.task.assert_called_with()
-
-    def test_name(self):
-        self.assertEqual(self.dependency.name, 'task')
-        
-    def test_args(self):
-        self.assertEqual(self.dependency.args, ())
-        
-    def test_kwargs(self):
-        self.assertEqual(self.dependency.kwargs, {})
+        self.attribute.meta_module.task1.assert_called_with()
+        self.attribute.meta_module.task2.assert_called_with()
         
     def test_is_resolved(self):
         self.assertEqual(self.dependency.is_resolved, False)          
         
     #Protected
     
-    def _make_mock_constraint_class(self):
-        class MockConstraint(TaskConstraint):
+    def _make_mock_dependency_class(self):
+        class MockDependency(Dependency):
+            #Public
+            is_resolved = False
             #Protected
             _builder_class = Mock
-            _attribute_class = Mock(return_value=Mock(
+            _method_task_class = Mock(return_value=Mock(
                 require=Mock(), 
                 trigger=Mock()))
-            _apply_dependency = Mock()
-        return MockConstraint
+            _add_dependency = Mock()
+        return MockDependency
         
         
-class DependencyTest_with_args_and_kwargs(unittest.TestCase):
-
-    #Public
-    
-    def setUp(self):
-        self.args = ('arg',)
-        self.kwargs = {'kwarg': 'kwarg'}
-        self.dependency = TaskDependency(
-            ('task', self.args, self.kwargs))
-        self.attribute = Mock(meta_module=Mock(task=Mock()))
-        
-    def test_resolve(self):
-        self.dependency.resolve(self.attribute)
-        self.attribute.meta_module.task.assert_called_with(
-            *self.args, **self.kwargs)
-        
-    def test_args(self):
-        self.assertEqual(self.dependency.args, ('arg',))
-        
-    def test_kwargs(self):
-        self.assertEqual(self.dependency.kwargs, {'kwarg': 'kwarg'})           
+# class DependencyTest_with_args_and_kwargs(unittest.TestCase):
+# 
+#     #Public
+#     
+#     def setUp(self):
+#         self.args = ('arg',)
+#         self.kwargs = {'kwarg': 'kwarg'}
+#         self.dependency = TaskDependency(
+#             ('task', self.args, self.kwargs))
+#         self.attribute = Mock(meta_module=Mock(task=Mock()))
+#         
+#     def test_resolve(self):
+#         self.dependency.resolve(self.attribute)
+#         self.attribute.meta_module.task.assert_called_with(
+#             *self.args, **self.kwargs)
+#         
+#     def test_args(self):
+#         self.assertEqual(self.dependency.args, ('arg',))
+#         
+#     def test_kwargs(self):
+#         self.assertEqual(self.dependency.kwargs, {'kwarg': 'kwarg'})           
