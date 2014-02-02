234d804bd27491f97a356400cb1b4b43d1b30beb
roll <roll@respect31.com>
2014-02-02 13:49:24 +0400

    recovered Dependency tests

--- tests/component/dependency/test_dependency.py
+++ tests/component/dependency/test_dependency.py
@@ -7,11 +7,14 @@ class DependencyTest(unittest.TestCase):
     #Public
     
     def setUp(self):
-        self.attribute = Mock(meta_module=Mock(
-            task1=Mock(), task2=Mock()))
         MockDependency = self._make_mock_dependency_class()
         self.tasks = ['task1', 'task2']
-        self.dependency = MockDependency(self.tasks)
+        self.args = ('arg',)
+        self.kwargs = {'kwarg': 'kwarg'}
+        self.dependency = MockDependency(
+            self.tasks, *self.args, **self.kwargs)
+        self.attribute = Mock(meta_module=Mock(
+            task1=Mock(), task2=Mock()))        
         
     def test___call__(self):
         function = 'function'
@@ -28,11 +31,10 @@ class DependencyTest(unittest.TestCase):
         
     def test_resolve(self):
         self.dependency.resolve(self.attribute)
-        self.attribute.meta_module.task1.assert_called_with()
-        self.attribute.meta_module.task2.assert_called_with()
-        
-    def test_is_resolved(self):
-        self.assertEqual(self.dependency.is_resolved, False)          
+        (self.attribute.meta_module.task1.
+            assert_called_with('arg', kwarg='kwarg'))
+        (self.attribute.meta_module.task2.
+            assert_called_with('arg', kwarg='kwarg'))        
         
     #Protected
     
@@ -46,27 +48,4 @@ class DependencyTest(unittest.TestCase):
                 require=Mock(), 
                 trigger=Mock()))
             _add_dependency = Mock()
-        return MockDependency
-        
-        
-# class DependencyTest_with_args_and_kwargs(unittest.TestCase):
-# 
-#     #Public
-#     
-#     def setUp(self):
-#         self.args = ('arg',)
-#         self.kwargs = {'kwarg': 'kwarg'}
-#         self.dependency = TaskDependency(
-#             ('task', self.args, self.kwargs))
-#         self.attribute = Mock(meta_module=Mock(task=Mock()))
-#         
-#     def test_resolve(self):
-#         self.dependency.resolve(self.attribute)
-#         self.attribute.meta_module.task.assert_called_with(
-#             *self.args, **self.kwargs)
-#         
-#     def test_args(self):
-#         self.assertEqual(self.dependency.args, ('arg',))
-#         
-#     def test_kwargs(self):
-#         self.assertEqual(self.dependency.kwargs, {'kwarg': 'kwarg'})           
+        return MockDependency          
