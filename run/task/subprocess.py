4ccf5e0478bddc8e14d7a243a4bbca3735af04c3
roll <roll@respect31.com>
2014-02-04 10:08:00 +0400

    implemented SubprocessTask

--- run/task/subprocess.py
+++ run/task/subprocess.py
@@ -1,7 +1,19 @@
+from subprocess import Popen, PIPE
 from .task import Task
 
 class SubprocessTask(Task):
 
     #Public
 
-    pass
+    def __init__(self, prefix=''):
+        self._prefix = prefix
+        
+    def invoke(self, command):
+        ecommand = self._prefix+command
+        process = Popen(ecommand, shell=True, 
+            stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        returncode = process.wait()
+        if returncode != 0:
+            raise RuntimeError(
+                'Command "{ecommand}" exited with "{returncode}"'.
+                format(ecommand=ecommand, returncode=returncode))
