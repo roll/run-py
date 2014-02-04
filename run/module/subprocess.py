d6208c4f283d91fba9943a998af1ed0a78362bcf
roll <roll@respect31.com>
2014-02-04 10:51:21 +0400

    implemented SubprocessModule

--- run/module/subprocess.py
+++ run/module/subprocess.py
@@ -1,7 +1,19 @@
 from .module import Module
+from ..task import SubprocessTask
 
 class SubprocessModule(Module):
 
     #Public
 
-    pass
+    def __init__(self, mapping={}, prefix='', separator=' '):
+        mapping = self._default_mapping
+        mapping.update(mapping)
+        for task_name, task_prefix in mapping.items():
+            if not hasattr(type(self), task_name):
+                eprefix = separator.join(filter([prefix, task_prefix]))
+                task = SubprocessTask(eprefix, separator, module=self)
+                setattr(type(self), task_name, task)        
+        
+    #Protected
+    
+    _default_mapping = {} 
