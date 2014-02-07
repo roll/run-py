d39c296f81426d0372b6e2705652b28a81d98aab
roll <roll@respect31.com>
2014-02-07 15:25:57 +0400

    added *_dependency forwarding to TaskBuilder

--- run/task/builder.py
+++ run/task/builder.py
@@ -4,11 +4,25 @@ class TaskBuilder(AttributeBuilder):
     
     #Public
     
+    def add_dependency(self, *args, **kwargs):
+        self._updates.append(self._call_class(
+            'add_dependency', *args, **kwargs))
+        
+    def ebable_dependency(self, *args, **kwargs):
+        self._updates.append(self._call_class(
+            'ebable_dependency', *args, **kwargs))
+        
+    def disable_dependency(self, *args, **kwargs):
+        self._updates.append(self._call_class(
+            'disable_dependency', *args, **kwargs))                
+        
     def require(self, *args, **kwargs):
-        self._updates.append(self._call_class('require', *args, **kwargs))
+        self._updates.append(self._call_class(
+            'require', *args, **kwargs))
         
     def trigger(self, *args, **kwargs):
-        self._updates.append(self._call_class('trigger', *args, **kwargs))
+        self._updates.append(self._call_class(
+            'trigger', *args, **kwargs))
         
     #Protected
     
