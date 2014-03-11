5934a91d661c0e8fa842318c1ed5e7eee91aefd9
roll <roll@respect31.com>
2014-03-11 10:14:17 +0400

    added task decorator draft

--- run/task/method.py
+++ run/task/method.py
@@ -9,3 +9,15 @@ class MethodTask(FunctionTask):
         
     def effective_invoke(self, *args, **kwargs):
         return self._function(self.meta_module, *args, **kwargs)
+    
+    
+class task:
+    
+    #Public
+    
+    def __init__(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+        
+    def __call__(self, method):
+        return MethodTask(method, *self._args, **self._kwargs)
