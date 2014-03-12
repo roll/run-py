482d0c291f45cf696848871e81488ef078d6cb47
roll <roll@respect31.com>
2014-03-12 09:34:05 +0400

    renamed is_merge to merge

--- run/task/nested.py
+++ run/task/nested.py
@@ -4,13 +4,13 @@ class NestedTask(PartialTask):
 
     #Public
         
-    def __init__(self, task, *args, is_merge=False, **kwargs):
+    def __init__(self, task, *args, merge=False, **kwargs):
         self._task_name = task
-        self._is_merge = is_merge
+        self._merge = merge
         super().__init__(*args, **kwargs)       
 
     def effective_invoke(self, *args, **kwargs):
-        if self._is_merge:
+        if self._merge:
             #Invoke without resolving requirements, triggers
             result = self._task.invoke(*args, **kwargs)
         else:
@@ -30,7 +30,7 @@ class NestedTask(PartialTask):
     @property
     def _task(self):
         task = getattr(self.meta_module, self._task_name)
-        if self._is_merge:
+        if self._merge:
             #Rebuild task with rebase on nested task module
             task = task.meta_builder(module=self.meta_module)
         return task    
