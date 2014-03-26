36babd83877a900d2781688df8d9dcc575e7e58d
roll <roll@respect31.com>
2014-03-26 14:54:01 +0400

    moved fork builder to public

--- run/attribute/builder.py
+++ run/attribute/builder.py
@@ -10,7 +10,16 @@ class AttributeBuilder:
         self._class = cls
         self._args = list(args)
         self._kwargs = kwargs
-        
+            
+    def fork(self, *args, **kwargs):
+        eargs = self._args+list(args)
+        ekwargs = copy(self._kwargs)
+        ekwargs.update(kwargs)
+        ekwargs.setdefault('module', self._module)
+        ekwargs.setdefault('updates', copy(self._updates))
+        builder = type(self)(self._class, *eargs, **ekwargs)
+        return builder
+     
     def build(self, *args, **kwargs):
         obj = self._create_object()
         self._init_object(obj, *args, **kwargs)
@@ -42,22 +51,13 @@ class AttributeBuilder:
         return object.__new__(self._class)
         
     def _init_object(self, obj, *args, **kwargs):
-        builder = self._fork_builder(*args, **kwargs)
+        builder = self.fork(*args, **kwargs)
         obj.__meta_build__(builder)
         if builder.module != True:
             obj.__meta_bind__(builder.module)
             obj.__meta_init__()
             obj.__meta_update__()
             obj.__meta_ready__()
-            
-    def _fork_builder(self, *args, **kwargs):
-        eargs = self._args+list(args)
-        ekwargs = copy(self._kwargs)
-        ekwargs.update(kwargs)
-        ekwargs.setdefault('module', self._module)
-        ekwargs.setdefault('updates', copy(self._updates))
-        builder = type(self)(self._class, *eargs, **ekwargs)
-        return builder
     
     
 def build(attribute, *args, **kwargs):
