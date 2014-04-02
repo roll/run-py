2d0f0b8195e881458e2d13b692637b1fdb11318e
roll <roll@respect31.com>
2014-04-02 13:10:52 +0400

    returned Attribute.__repr__

--- run/attribute/attribute.py
+++ run/attribute/attribute.py
@@ -31,12 +31,12 @@ class Attribute(metaclass=AttributeMetaclass):
         pass
     
     def __repr__(self):
-        #TODO: return
-#         if self._meta_ready:
-#             return '<{category} "{qualname}">'.format(
-#                 category=self.meta_type, 
-#                 qualname=self.meta_qualname)
-        return super().__repr__()
+        try:
+            return '<{category} "{qualname}">'.format(
+                category=self.meta_type, 
+                qualname=self.meta_qualname)
+        except Exception:
+            return super().__repr__()
     
     @property
     def meta_basedir(self):
