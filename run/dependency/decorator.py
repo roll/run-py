1408be4a3af7dec927287a6044d53576294f6398
roll <roll@respect31.com>
2014-04-03 14:25:37 +0400

    remove prototype arg availability to pass to DepDecorator

--- run/dependency/decorator.py
+++ run/dependency/decorator.py
@@ -5,19 +5,11 @@ class DependencyDecorator(metaclass=ABCMeta):
     #Public    
     
     def __call__(self, method):
-        prototype = method
-        if not isinstance(method, self._prototype_class):
-            prototype = self._method_task_class(method)
+        prototype = self._method_task_class(method)
         self._add_dependency(prototype)
         return prototype
     
-    #Protected    
-    
-    @property
-    def _prototype_class(self):
-        #Cycle dependency if static
-        from ..task import TaskPrototype    
-        return TaskPrototype  
+    #Protected
     
     @property
     def _method_task_class(self):
