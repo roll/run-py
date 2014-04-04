00f162559719e94fce98af493b833805698aed35
roll <roll@respect31.com>
2014-04-04 11:20:16 +0400

    used expand in ValueTask

--- run/task/value.py
+++ run/task/value.py
@@ -3,9 +3,6 @@ from .task import Task
 class ValueTask(Task):
     
     #Public
-    
-    def __init__(self, value):
-        self._value = value
  
-    def invoke(self):
-        return self._value
+    def invoke(self, value):
+        return value
