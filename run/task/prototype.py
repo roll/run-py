78e750c8f870903d307821b12c318d0e8f87ef9d
roll <roll@respect31.com>
2014-04-04 13:48:13 +0400

    fixed TaskPrototype

--- run/task/prototype.py
+++ run/task/prototype.py
@@ -7,22 +7,27 @@ class TaskPrototype(AttributePrototype):
     def depend(self, *args, **kwargs):
         update = self._call_class('depend', *args, **kwargs)
         self._updates.append(update)
+        return self
         
     def require(self, *args, **kwargs):
         update = self._call_class('require', *args, **kwargs)
-        self._updates.append(update)        
+        self._updates.append(update)
+        return self 
         
     def trigger(self, *args, **kwargs):
         update = self._call_class('trigger', *args, **kwargs)
-        self._updates.append(update)        
+        self._updates.append(update)
+        return self   
           
     def enable_dependency(self, *args, **kwargs):
         update = self._call_class('enable_dependency', *args, **kwargs)
-        self._updates.append(update)        
+        self._updates.append(update)
+        return self  
         
     def disable_dependency(self, *args, **kwargs):
         update = self._call_class('disable_dependency', *args, **kwargs)
-        self._updates.append(update)          
+        self._updates.append(update)
+        return self         
         
     #Protected
     
