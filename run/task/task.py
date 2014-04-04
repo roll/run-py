7f2d78f2be7e91720ffb66fd381e3ac697c38e01
roll <roll@respect31.com>
2014-04-04 08:58:31 +0400

    implemented module proxy

--- run/task/task.py
+++ run/task/task.py
@@ -129,4 +129,35 @@ class Task(Attribute, metaclass=TaskMetaclass):
     def _effective_kwargs(self, **kwargs):
         ekwargs = copy(self._kwargs)
         ekwargs.update(kwargs)
-        return ekwargs       
+        return ekwargs
+    
+
+class module:
+    
+    #Public
+    
+    def __init__(self):
+        self._get = False
+        self._attr = ''
+        self._call = False
+        self._args = ()
+        self._kwargs = {}
+        
+    def __getattr__(self, name):
+        self._get = True
+        self._attr = '.'.join(self._attr, name)
+        return self
+    
+    def __call__(self, *args, **kwargs):
+        self._call = True
+        self._args = args
+        self._kwargs = kwargs
+        return self
+        
+    def expand(self, module):
+        result = module
+        if self._get:
+            result = getattr(result, self._attr)
+        if self._call:
+            result = result(*self._args, **self._kwargs)
+        return result           
