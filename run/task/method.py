40e46662207e47f6ccccffa662e9361450382d27
roll <roll@respect31.com>
2014-04-22 11:14:01 +0200

    reimp MethodTask sig

--- run/task/method.py
+++ run/task/method.py
@@ -1,3 +1,4 @@
+import re
 from .function import FunctionTask
 
 class MethodTask(FunctionTask):
@@ -6,6 +7,11 @@ class MethodTask(FunctionTask):
     
     def __init__(self, method, *args, **kwargs):
         super().__init__(method, *args, **kwargs)
+        
+    @property
+    def meta_signature(self):
+        return self._meta_params.get('signature', 
+            re.sub('self,?', '', super().meta_signature))        
         
     def invoke(self, *args, **kwargs):
         return self._function(self.meta_module, *args, **kwargs)
