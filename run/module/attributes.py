57481a8b97652c7dc391108d801e6a3d45d6c4eb
roll <roll@respect31.com>
2014-04-24 11:12:24 +0200

    min

--- run/module/attributes.py
+++ run/module/attributes.py
@@ -14,6 +14,9 @@ class ModuleAttributes(dict):
         """Return attribute by given name.
         
         Supports nested names like "module.attribute".
+        
+        :raise AttributeError: if module has not this name attribute
+        :raise TypeError: if attribute is not instance of category
         """
         try:
             name, nested_name = name.split('.', 1)
@@ -25,15 +28,15 @@ class ModuleAttributes(dict):
             raise AttributeError(
             'Module "{module}" has no attribute "{name}".'.
             format(module=self._module, name=name)) from None
-        if nested_name:
-            #TODO: add is Module check? 
-            return attribute.meta_attributes.get_attribute(
-                nested_name, category=None, resolve=False)
         if category:
             if not isinstance(attribute, category):
                 raise TypeError(
                     'Attribute "{name}" is not a "{category}".'.
                     format(name=name, category=category))
+        if nested_name:
+            #TODO: add is Module check? 
+            return attribute.meta_attributes.get_attribute(
+                nested_name, category=category, resolve=resolve)                
         if resolve:
             return attribute.__get__(attribute.meta_module)
         return attribute  
