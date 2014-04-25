930b327697b8f98d288d5ad91d3adbf55c997864
roll <roll@respect31.com>
2014-04-25 19:50:27 +0200

    added test_find_list

--- tests/system/test_modules.py
+++ tests/system/test_modules.py
@@ -12,7 +12,26 @@ class ModulesTest(ExamplesTest):
         
     def test_auto_factorial(self):
         result = self._execute('auto.factorial 5')
-        self.assertEqual(result, '120\n')        
+        self.assertEqual(result, '120\n')
+        
+    def test_find_list(self):
+        result = self._execute('find.list')
+        self.assertEqual(
+            result,
+            'find.default\n'
+            'find.derived\n' 
+            'find.descriptor\n' 
+            'find.find\n' 
+            'find.function\n' 
+            'find.info\n' 
+            'find.input\n' 
+            'find.list\n' 
+            'find.meta\n' 
+            'find.method\n' 
+            'find.null\n'
+            'find.render\n' 
+            'find.subprocess\n' 
+            'find.value\n')               
      
     #Protected 
         
