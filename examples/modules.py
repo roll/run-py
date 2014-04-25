248cc6f9424a0a755afe310abeb5d4e99e786054
roll <roll@respect31.com>
2014-04-25 19:58:47 +0200

    added subprocess example

--- examples/modules.py
+++ examples/modules.py
@@ -1,6 +1,6 @@
 import os
 import math
-from run import Module, AutoModule, FindModule
+from run import Module, AutoModule, FindModule, SubprocessModule
 
 class MainModule(Module):
     
@@ -13,4 +13,11 @@ class MainModule(Module):
     find = FindModule(
         filename='tasks.py',
         basedir=os.path.dirname(__file__),
+    )
+    
+    subprocess = SubprocessModule(
+        mapping={
+            'hello': 'echo "Hello World!"',
+            'goodbye': 'echo "Goodbye World!"',
+        },
     )
