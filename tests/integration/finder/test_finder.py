dc80fa2c037eca95db7b9129db5a13cf025e4a5f
roll <roll@respect31.com>
2014-04-28 08:33:46 +0200

    min

--- tests/integration/finder/test_finder.py
+++ tests/integration/finder/test_finder.py
@@ -5,14 +5,11 @@ from run.finder import Finder
 class FinderTest(unittest.TestCase):
 
     #Public
-
-    def setUp(self):
-        self.basedir = self._get_fixtures_path()
          
     def test_find_recursively(self):
         finder = Finder()
         modules = list(finder.find(
-            'runfile.py', basedir=self.basedir, recursively=True))
+            'runfile.py', basedir=self._basedir, recursively=True))
         self.assertEqual(len(modules), 3)
         self.assertEqual(modules[0].__name__, 'Module1')
         self.assertEqual(modules[1].__name__, 'Module2')
@@ -21,18 +18,19 @@ class FinderTest(unittest.TestCase):
     def test_find_with_names(self):
         finder = Finder(names=['name1'])
         modules = list(finder.find(
-            'runfile.py', basedir=self.basedir, recursively=True))
+            'runfile.py', basedir=self._basedir, recursively=True))
         self.assertEqual(len(modules), 1)
         self.assertEqual(modules[0].__name__, 'Module1')
 
     def test_find_with_tags(self):
         finder = Finder(tags=['tag2'])
         modules = list(finder.find(
-            'runfile.py', basedir=self.basedir, recursively=True))
+            'runfile.py', basedir=self._basedir, recursively=True))
         self.assertEqual(len(modules), 1)
         self.assertEqual(modules[0].__name__, 'Module2')
         
     #Protected
     
-    def _get_fixtures_path(self, *args):
-        return os.path.join(os.path.dirname(__file__), 'fixtures', *args)  
+    @property
+    def _basedir(self):
+        return os.path.join(os.path.dirname(__file__), 'fixtures')  
