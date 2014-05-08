524428784395cf0a20864aa224c04e61e14efb41
roll <roll@respect31.com>
2014-05-08 12:44:26 +0200

    upgraded to box 0.19

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ package = {
         exclude=['tests*']
     ),
 	'include_package_data': True,
-    'install_requires': ['box>=0.18.1', 'jinja2'],  
+    'install_requires': ['box>=0.19', 'jinja2'],  
     'tests_require': ['nose'],
     'test_suite': 'nose.collector',
     
