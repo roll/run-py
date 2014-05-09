22a14280c246958d2542973c4164f77f42c6018d
roll <roll@respect31.com>
2014-05-09 19:43:52 +0200

    fixed test_find

--- tests/integration/run/find/fixtures/runfile.py
+++ tests/integration/run/find/fixtures/runfile.py
@@ -1,4 +1,3 @@
-from abc import abstractmethod
 from run import Module
 
 print('Hits runfile.py')
@@ -9,17 +8,6 @@ class Module1(Module):
     
     meta_name = 'name1'
     meta_tags = ['tag1']
-
-
-#Should be ignored by loader
-
-class AbstractModule(Module):
-    
-    #Public
-    
-    @abstractmethod
-    def method(self):
-        pass #pragma: no cover
  
     
 class Class: pass
