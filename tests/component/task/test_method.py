dfc0c4e1bdeaed1d7d66e32fbff33d51ada73974
roll <roll@respect31.com>
2014-05-11 12:56:05 +0200

    improved MethodTask test

--- tests/component/task/test_method.py
+++ tests/component/task/test_method.py
@@ -8,10 +8,16 @@ class MethodTaskTest(unittest.TestCase):
     
     def setUp(self):
         self.args = ('arg1',)
-        self.kwargs = {'kwarg1': 'kwarg1'}        
-        self.function = Mock(return_value='value')
-        self.task = MethodTask(self.function, meta_module='module')
+        self.kwargs = {'kwarg1': 'kwarg1'}
+        self.method = Mock()
+        self.task = MethodTask(self.method, meta_module=None)
         
-    def test_invoke(self):        
-        self.assertEqual(self.task.invoke(*self.args, **self.kwargs), 'value')
-        self.function.assert_called_with('module', *self.args, **self.kwargs)
+    def test___call__(self):
+        result = self.task(*self.args, **self.kwargs)        
+        self.assertEqual(result, self.method.return_value)
+        self.method.assert_called_with(
+            self.task.meta_module, *self.args, **self.kwargs)
+        
+    def test_meta_signature(self):
+        self.assertEqual(self.task.meta_signature, '(*args, **kwargs)')
+    
