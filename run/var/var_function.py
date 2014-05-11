5ef745261d352cdcb86fa485eb09eebf4cfd005d
roll <roll@respect31.com>
2014-05-11 10:07:01 +0200

    implemented var tests

--- run/var/var_function.py
+++ run/var/var_function.py
@@ -2,9 +2,23 @@ from ..task import task
 from .descriptor import DescriptorVar
 
 class var(task):
+    """Make var from method with default kwargs to invoke.
+      
+    - as function::
+      
+       var = var(method, **kwargs)      
+      
+    - as decorator::
+      
+        @var(**kwargs)
+        def method(self):
+            pass
+    """    
     
     #Protected
     
-    @staticmethod
-    def _make_task(method, **kwargs):
-        return DescriptorVar(property(method), **kwargs)
+    _task_class = DescriptorVar
+    
+    @classmethod
+    def _make_task(cls, method, **kwargs):
+        return cls._task_class(property(method), **kwargs)
--- tests/component/var/test_var_function.py
+++ tests/component/var/test_var_function.py
@@ -1,9 +1,26 @@
 import unittest
+from unittest.mock import Mock, patch
 from run.var.var_function import var
 
 class var_Test(unittest.TestCase):
 
     #Public
 
-    def test(self):
-        pass
+    def setUp(self):
+        self.method = 'method'
+        self.kwargs = {'kwarg1': 'kwarg1'}
+        self.property = Mock(return_value='property')
+        self.task_class = Mock(return_value='var')
+        patch('builtins.property', new=self.property).start()       
+        patch.object(var, '_task_class', new=self.task_class).start()
+        self.addCleanup(patch.stopall)
+
+    def test_as_function(self):
+        self.assertEqual(var(self.method, **self.kwargs), 'var')
+        self.property.assert_called_with(self.method)
+        self.task_class.assert_called_with('property', **self.kwargs)
+        
+    def test_as_decorator(self):
+        self.assertEqual(var(**self.kwargs)('method'), 'var')
+        self.property.assert_called_with(self.method)
+        self.task_class.assert_called_with('property', **self.kwargs) 
