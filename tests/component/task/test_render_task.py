f10a133a8dc0a100a1317716e2f9426ce061b3b1
roll <roll@respect31.com>
2014-05-11 12:24:45 +0200

    added RenderTask test

--- tests/component/task/test_render_task.py
+++ tests/component/task/test_render_task.py
@@ -1,9 +1,24 @@
 import unittest
+from functools import partial
+from unittest.mock import patch
 from run.task.render import RenderTask
 
 class RenderTaskTest(unittest.TestCase):
 
     #Public
 
-    def test(self):
-        pass
+    def setUp(self):
+        self.args = ('arg1',)
+        self.kwargs = {'kwarg1': 'kwarg1'}
+        self.ptask = partial(RenderTask, meta_module=None)
+    
+    @patch('box.jinja2.render_file')
+    def test___call__(self, render_file):
+        task = self.ptask()
+        result = task(*self.args, **self.kwargs)
+        self.assertEqual(result, render_file.return_value)
+        render_file.assert_called_with(
+            *self.args, context=task.meta_module, **self.kwargs)
+        
+    def test___call___with_unsopported_mode(self):
+        self.assertRaises(ValueError, self.ptask, mode='unsupported')        
