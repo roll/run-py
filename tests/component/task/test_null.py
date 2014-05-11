01a6bade478ce7129ff9fbffe8fd48b21d7b7786
roll <roll@respect31.com>
2014-05-11 12:42:03 +0200

    added NullTask test

---
+++ tests/component/task/test_null.py
@@ -0,0 +1,15 @@
+import unittest
+from run.task.null import NullTask
+
+class NullTaskTest(unittest.TestCase):
+
+    #Public
+    
+    def setUp(self):
+        self.task = NullTask(meta_module=None)
+
+    def test___call__(self):
+        self.assertEqual(self.task(), None)
+
+    def test_meta_docstring(self):
+        self.assertTrue(self.task.meta_docstring)
