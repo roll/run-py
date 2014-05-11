1a92ea671ddcfffe2c157190d9877702d1b89f87
roll <roll@respect31.com>
2014-05-11 13:45:50 +0200

    added skip test

---
+++ tests/component/module/test_skip.py
@@ -0,0 +1,12 @@
+import unittest
+from unittest.mock import Mock
+from run.module.skip import skip
+
+class skip_Test(unittest.TestCase):
+
+    #Public
+    
+    def test(self):
+        attribute = Mock()
+        self.assertEqual(skip(attribute), attribute)
+        self.assertEqual(attribute.__isskippedattribute__, True)
