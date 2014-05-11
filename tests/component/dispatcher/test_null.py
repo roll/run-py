b5e8037f157625fcea8deed59f044cc28e370540
roll <roll@respect31.com>
2014-05-11 13:18:23 +0200

    added NullDispatcher test

---
+++ tests/component/dispatcher/test_null.py
@@ -0,0 +1,12 @@
+import unittest
+from run.dispatcher.null import NullDispatcher
+
+class NullDispatcherTest(unittest.TestCase):
+
+    #Public
+    
+    def setUp(self):
+        self.dispatcher = NullDispatcher()
+
+    def test___bool__(self):
+        self.assertFalse(self.dispatcher)
