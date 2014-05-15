81ceff6541b4b588894dd06726e487462eb31cea
roll <roll@respect31.com>
2014-05-15 09:23:34 +0200

    fixed category error in nested get_attribute

--- run/module/attributes.py
+++ run/module/attributes.py
@@ -35,12 +35,6 @@ class ModuleAttributes(dict):
             raise AttributeError(
             'Module "{module}" has no attribute "{name}".'.
             format(module=self._module, name=name)) from None
-        if category:
-            category = import_object(category)
-            if not isinstance(attribute, category):
-                raise TypeError(
-                    'Attribute "{name}" is not a {category}.'.
-                    format(name=name, category=category))
         if nested_name:
             from .module import Module
             if not isinstance(attribute, Module):
@@ -49,6 +43,12 @@ class ModuleAttributes(dict):
                     format(attribute=attribute))
             return attribute.meta_attributes.get_attribute(
                 nested_name, category=category, resolve=resolve)
+        if category:
+            category = import_object(category)
+            if not isinstance(attribute, category):
+                raise TypeError(
+                    'Attribute "{name}" is not a {category}.'.
+                    format(name=name, category=category)) 
         if resolve:
             return attribute.__get__(attribute.meta_module)
         return attribute  
