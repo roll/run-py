34b3d96c938f480dcaa7df18cb27a5ad231a7fdb
roll <roll@respect31.com>
2014-05-17 19:25:50 +0400

    min

--- runfile.py
+++ runfile.py
@@ -18,7 +18,7 @@ class MainModule(ManageModule):
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers', 
         'License :: OSI Approved :: MIT License', 
-        'Programming Language :: Python :: 3.3', 
+        'Programming Language :: Python :: 3', 
         'Topic :: Software Development :: Libraries :: Python Modules', 
         'Topic :: System :: Systems Administration', 
     ]
