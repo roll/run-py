afbd48ff46e3ea0b56ca745bf42840631342b0f1
roll <roll@respect31.com>
2014-05-17 18:37:05 +0400

    min

--- runfile.py
+++ runfile.py
@@ -5,6 +5,7 @@ class MainModule(ManageModule):
     
     #Modules
         
+    #TODO: move to ManageModule when deferred load will be added to run    
     #TODO: use names instead basedir after run fix
     docs = FindModule(basedir='docs')
     tests = FindModule(basedir='tests') 
@@ -23,16 +24,10 @@ class MainModule(ManageModule):
     ]
     data_files = [('/etc/bash_completion.d', ['data/run.sh'])]
     description = 'Run is a program to run tasks from files.'
-    development_requires = [
-        'sphinx',
-        'sphinx_rtd_theme',
-    ]
+    development_requires = ['sphinx', 'sphinx_rtd_theme']
     entry_points = {'console_scripts': ['run = run:program']}
     github_user = 'respect31'
-    install_requires = [
-        'box>=0.20',
-        'jinja2',
-    ]
+    install_requires = ['box>=0.20', 'jinja2']
     license = 'MIT License'  
     maintainer = 'roll'
     maintainer_email = 'roll@respect31.com'
@@ -41,8 +36,5 @@ class MainModule(ManageModule):
     pypi_name = 'runpack'
     pypi_user = 'roll'
     pypi_password_secure = 'JaTeiyjnimmtwhbdfPMZZdtp+5S920vb0HobJWL1QQjHVAo5Hwt0kTWYG+zjDrpWUL+NanVNqhQA8xnvWKbI5cZ+n3PvS7KFbgn6XcTYfeEGyEdYUFi0sXaUsgcfke+9nyMBDLoRH2M7TGqpLY2dmXk5C0h0RMkkAPjxgZCan94='
-    tests_require = [
-        'packgram>=0.9',        
-        'nose',
-    ]
+    tests_require = ['packgram>=0.9', 'nose']
     test_suite = 'nose.collector'    
