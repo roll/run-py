783ff36f1d7b6b631e60e3be671d5d0546a41d04
roll <roll@respect31.com>
2014-06-13 10:13:44 +0400

    added fork example

--- run/attribute/fork.py
+++ run/attribute/fork.py
@@ -1,4 +1,14 @@
 def fork(prototype, *args, **kwargs):
     """Fork attribute prototype with optional args, kwargs altering.
+    
+    Usage example::
+    
+      class Module(Module):
+          
+          attr1 = SomeTask()
+          attr2 = fork(attr1, meta_basedir='new/path', param='value')
+          
+    In this case attr2 will build as attr1 copy with redefined
+    meta_basedir and default keyword argument param.
     """
     return prototype.__copy__(*args, **kwargs)
