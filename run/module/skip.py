d191b926bc7cbd0d2c226da636fab0faa463c53b
roll <roll@respect31.com>
2014-06-15 10:52:39 +0400

    added skip docs

--- run/module/skip.py
+++ run/module/skip.py
@@ -1,3 +1,16 @@
 def skip(attribute):
+    """Make attribute to be skipped.
+    
+    Usage example::
+    
+      class Module(Module):
+    
+          @skip
+          def method(self):
+              pass
+              
+    In this case method will not be converted to run's attribute and
+    will stay as regular python method.
+    """
     setattr(attribute, '__isskippedattribute__', True)
     return attribute
