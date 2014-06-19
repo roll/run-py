bac24fd7ebb1e27c096c47104a2bf92f39a7661b
roll <roll@respect31.com>
2014-06-19 09:11:27 +0400

    min

--- run/module/attribute.py
+++ run/module/attribute.py
@@ -1,6 +1,6 @@
 def attribute(module, name, *, category=None, getvalue=True):
-    """Return attribute by given name.
+    """Return module's attribute by given name.
      
-    .. seealso:: It's a shortcut to :func:`run.module.Module.__getattribute__`
+    .. seealso:: It's a shortcut to :attr:`run.module.Module.__getattribute__`
     """
     return module.__getattribute__(name, category=category, getvalue=getvalue)
