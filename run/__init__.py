ddc516c32ff63ae78fc10189b948c79a4e5cf9a7
roll <roll@respect31.com>
2014-07-16 12:54:37 +0400

    improved run formatting

--- run/__init__.py
+++ run/__init__.py
@@ -1,14 +1,14 @@
 from .attribute import build, fork, value
 from .dependency import depend, require, trigger
-from .module import (Module, attribute, skip, 
+from .module import (Module, attribute, skip,
                      AutoModule, FindModule, NullModule, SubprocessModule)
 from .settings import settings
 from .task import (task, module,
-                   DerivedTask, DescriptorTask, FindTask, FunctionTask, 
-                   InputTask, MethodTask, NullTask, RenderTask, 
+                   DerivedTask, DescriptorTask, FindTask, FunctionTask,
+                   InputTask, MethodTask, NullTask, RenderTask,
                    SubprocessTask, ValueTask)
 from .var import (var,
-                  DerivedVar, DescriptorVar, FindVar, FunctionVar, 
-                  InputVar, MethodVar, NullVar, RenderVar, 
+                  DerivedVar, DescriptorVar, FindVar, FunctionVar,
+                  InputVar, MethodVar, NullVar, RenderVar,
                   SubprocessVar, ValueVar)
 from .version import version
--- run/version.py
+++ run/version.py
@@ -1,11 +1,11 @@
 from box.packtools import Version
 
 class Version(Version):
-    
+
     major = 0
     minor = 18
     micro = 0
     level = 'final'
-    
-    
+
+
 version = Version()
