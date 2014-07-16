f646cfc4f6aeaaa39bd7d20618f5edaec760a586
roll <roll@respect31.com>
2014-07-16 10:38:15 +0400

    implemented value test

---
+++ tests/component/attribute/test_value.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest.mock import Mock
+from run.attribute.value import value
+
+class value_Test(unittest.TestCase):
+    
+    #Public
+    
+    def test(self):
+        attribute = Mock(__get__=Mock(return_value='attribute_value'))
+        attribute_value = value(attribute)
+        self.assertEqual(attribute_value, 'attribute_value')
+        attribute.__get__.assert_called_with(attribute.meta_module)
