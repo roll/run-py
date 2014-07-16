52a6d88ae78d3a5fc47af67d4e75be9986c8ddd8
roll <roll@respect31.com>
2014-07-16 09:51:40 +0400

    added metaclass module is none test

--- tests/component/attribute/test_metaclass.py
+++ tests/component/attribute/test_metaclass.py
@@ -10,31 +10,39 @@ class AttributeMetaclassTest(unittest.TestCase):
     def setUp(self):
         self.args = ('arg1',)
         self.kwargs = {'kwarg1': 'kwarg1'}
-        self.MockPrototype = Mock(return_value=Mock(__build__=Mock()))
-        self.MockMetaclass = self._make_mock_metaclass(self.MockPrototype)
-        self.MockClass = self._make_mock_class(self.MockMetaclass)
+        self.Prototype = Mock(return_value=Mock(__build__=Mock()))
+        self.Metaclass = self._make_mock_metaclass(self.Prototype)
+        self.Class = self._make_mock_class(self.Metaclass)
 
     def test(self):
-        self.assertTrue(issubclass(self.MockMetaclass, ABCMeta))
+        self.assertTrue(issubclass(self.Metaclass, ABCMeta))
 
     def test___call__(self):
-        instance = self.MockClass(*self.args, **self.kwargs)
+        instance = self.Class(*self.args, **self.kwargs)
         self.assertIsInstance(instance, Mock)
-        self.MockPrototype.assert_called_with(
-            self.MockClass, None, *self.args, **self.kwargs)
-        self.assertFalse(self.MockPrototype.return_value.called)
+        self.Prototype.assert_called_with(
+            self.Class, None, *self.args, **self.kwargs)
+        self.assertFalse(self.Prototype.return_value.called)
     
     def test___call___with_module(self):
-        instance = self.MockClass(meta_module='module')
+        instance = self.Class(meta_module='module')
         self.assertIsInstance(instance, Mock)
-        self.MockPrototype.assert_called_with(self.MockClass, None)
-        self.MockPrototype.return_value.__build__.assert_called_with('module')
+        self.Prototype.assert_called_with(self.Class, None)
+        self.Prototype.return_value.__build__.assert_called_with('module')
+        
+    def test___call___with_module_is_none(self):
+        instance = self.Class(meta_module=None)
+        self.assertIsInstance(instance, Mock)
+        self.Prototype.assert_called_with(self.Class, None)
+        self.Metaclass._null_module_class.assert_called_with()
+        self.Prototype.return_value.__build__.assert_called_with('null_module')        
         
     #Protected
     
     def _make_mock_metaclass(self, mock_prototype_class):
         class MockMetaclass(AttributeMetaclass):
             #Protected
+            _null_module_class = Mock(return_value='null_module')
             _prototype_class = mock_prototype_class
         return MockMetaclass
     
