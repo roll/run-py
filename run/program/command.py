0e24633c67115cc149fb0583cdd3ab74dfa955cb
roll <roll@respect31.com>
2014-07-16 12:50:38 +0400

    improved program formatting

--- run/program/command.py
+++ run/program/command.py
@@ -5,16 +5,16 @@ from box.functools import cachedproperty
 from ..settings import settings
 
 class Command(Command):
-    
-    #Public
-    
+
+    # Public
+
     default_attribute = settings.default_attribute
-    
+
     @property
     def attribute(self):
-        attribute = self._namespace.attribute 
+        attribute = self._namespace.attribute
         if self.list:
-            attribute = 'list'               
+            attribute = 'list'
         elif self.info:
             attribute = 'info'
         elif self.meta:
@@ -22,38 +22,38 @@ class Command(Command):
         elif not attribute:
             attribute = self.default_attribute
         return attribute
-    
+
     @property
     def arguments(self):
         attribute = self._namespace.attribute
         arguments = self._namespace.arguments
         if (self.list or self.info or self.meta) and attribute:
-            arguments = [attribute]+arguments
+            arguments = [attribute] + arguments
         return arguments
 
     @property
     def args(self):
         return self._parsed_arguments[0]
-    
-    @property    
+
+    @property
     def kwargs(self):
         return self._parsed_arguments[1]
-    
-    #Protected
-    
+
+    # Protected
+
     @cachedproperty
     def _parsed_arguments(self):
         args = []
         kwargs = {}
         for element in next(csv.reader([''.join(self.arguments)])):
-            parts = [self._parse_literal(item.strip()) for item in 
+            parts = [self._parse_literal(item.strip()) for item in
                      next(csv.reader([element], delimiter='='))]
             if len(parts) == 1:
                 args.append(parts[0])
             elif len(parts) == 2:
                 kwargs[parts[0]] = parts[1]
         return (args, kwargs)
-    
+
     def _parse_literal(self, literal):
         try:
             value = ast.literal_eval(literal)
--- run/program/program.py
+++ run/program/program.py
@@ -6,30 +6,30 @@ from ..settings import settings
 from .command import Command
 
 class Program(Program):
-         
-    #Protected
-    
-    _command_class = Command    
+
+    # Protected
+
+    _command_class = Command
     _machine_class = Machine
     _settings = settings
-    
+
     def _execute(self):
         self._machine.process(
             self._command.attribute,
-            *self._command.args, 
+            *self._command.args,
             **self._command.kwargs)
-            
-    @cachedproperty   
+
+    @cachedproperty
     def _machine(self):
         machine = self._machine_class(
             names=self._command.names,
             tags=self._command.tags,
-            file=self._command.file,            
-            basedir=self._command.basedir, 
+            file=self._command.file,
+            basedir=self._command.basedir,
             recursively=self._command.recursively,
             skip=self._command.skip,
             plain=self._command.plain)
         return machine
-    
-        
+
+
 program = Program(sys.argv)
