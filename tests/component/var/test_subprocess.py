dfa0b5581fd7bd7a4c8dd50985bf0c10c011260a
roll <roll@respect31.com>
2014-07-17 14:07:03 +0400

    added SubprocessVar test

---
+++ tests/component/var/test_subprocess.py
@@ -0,0 +1,10 @@
+import unittest
+from run.var.subprocess import SubprocessVar, Var, SubprocessTask
+
+class SubprocessVarTest(unittest.TestCase):
+
+    # Public
+
+    def test(self):
+        self.assertTrue(issubclass(SubprocessVar, Var))
+        self.assertTrue(issubclass(SubprocessVar, SubprocessTask))
