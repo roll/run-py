d529a121a4b4248fd9f689f52f0d57e840ec0ddb
roll <roll@respect31.com>
2014-07-17 13:56:37 +0400

    added InputVar test

---
+++ tests/component/var/test_input.py
@@ -0,0 +1,10 @@
+import unittest
+from run.var.input import InputVar, Var, InputTask
+
+class InputVarTest(unittest.TestCase):
+
+    # Public
+
+    def test(self):
+        self.assertTrue(issubclass(InputVar, Var))
+        self.assertTrue(issubclass(InputVar, InputTask))
