42ff69e47e6c34c4b552c2d91efb30625bd5d31b
roll <roll@respect31.com>
2014-07-17 13:53:28 +0400

    added FindVar test

---
+++ tests/component/var/test_find_var.py
@@ -0,0 +1,10 @@
+import unittest
+from run.var.find import FindVar, Var, FindTask
+
+class FindVarTest(unittest.TestCase):
+
+    # Public
+
+    def test(self):
+        self.assertTrue(issubclass(FindVar, Var))
+        self.assertTrue(issubclass(FindVar, FindTask))
