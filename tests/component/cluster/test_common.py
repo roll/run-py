d84da9791709f4a594582fc85e235eb14a0aafbf
roll <roll@respect31.com>
2014-07-18 12:37:57 +0400

    implemented CommonConstraint test

--- tests/component/cluster/test_common.py
+++ tests/component/cluster/test_common.py
@@ -1,9 +1,33 @@
 import unittest
+import inspect
+from unittest.mock import Mock
 from run.cluster.common import CommonConstraint
 
 class CommonConstraintTest(unittest.TestCase):
 
     # Public
 
-    def test(self):
-        pass
+    def setUp(self):
+        self.constraint = CommonConstraint(Mock)
+        self.emitter = Mock()
+        self.emitter.object = Mock
+        self.emitter.module = inspect.getmodule(self.emitter.object)
+
+    def test___call__(self):
+        self.constraint(self.emitter)
+        self.assertFalse(self.emitter.skip.called)
+
+    def test___call___skip_object_is_imported(self):
+        self.emitter.module = 'other_module'
+        self.constraint(self.emitter)
+        self.assertTrue(self.emitter.skip.called)
+
+    def test___call___skip_object_is_not_type(self):
+        self.emitter.object = Mock()
+        self.constraint(self.emitter)
+        self.assertTrue(self.emitter.skip.called)
+
+    def test___call___skip_object_is_not_subclass(self):
+        self.constraint = CommonConstraint(Exception)
+        self.constraint(self.emitter)
+        self.assertTrue(self.emitter.skip.called)
