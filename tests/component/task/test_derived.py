a4096769082895d53f89bd2520c40a32b7f0295e
roll <roll@respect31.com>
2014-07-18 10:09:57 +0400

    completed DerivedTask test

--- tests/component/task/test_derived.py
+++ tests/component/task/test_derived.py
@@ -1,30 +1,32 @@
 import unittest
-from unittest.mock import Mock
-from run.task.derived import DerivedTask
+from unittest.mock import patch
+from run.task import derived
 
 class DerivedTaskTest(unittest.TestCase):
 
     # Public
 
     def setUp(self):
-        self.MockTask = self._make_mock_task_class()
-        self.task = self.MockTask('task', meta_module=None)
-
-    def test___call__(self):
+        self.addCleanup(patch.stopall)
         self.args = ('arg1',)
         self.kwargs = {'kwarg1': 'kwarg1'}
-        self.assertEqual(self.task(*self.args, **self.kwargs), 'value')
+        self.attribute = patch.object(derived, 'attribute').start()
+        self.task = derived.DerivedTask('task', meta_module=None)
+
+    def test___call__(self):
+        self.assertEqual(
+            self.task(*self.args, **self.kwargs),
+            self.attribute.return_value.return_value)
+        # Check attribute call
+        self.attribute.assert_called_with(
+            self.task.meta_module, 'task',
+            category=derived.Task, getvalue=True)
+        # Check attribute's return value (task) call
+        self.attribute.return_value.assert_called_with(
+            *self.args, **self.kwargs)
 
     def test_meta_docstring(self):
         self.assertTrue(self.task.meta_docstring)
 
     def test_meta_signature(self):
         self.assertTrue(self.task.meta_signature)
-
-    # Protected
-
-    def _make_mock_task_class(self):
-        class MockTask(DerivedTask):
-            # Protected
-            _task = Mock(return_value='value')
-        return MockTask
