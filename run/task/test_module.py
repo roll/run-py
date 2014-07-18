28626b963ba0d580bb2e43a12c3bb0b17d136b0d
roll <roll@respect31.com>
2014-07-18 11:31:05 +0400

    implemented tasks's module test

--- run/task/test_module.py
+++ run/task/test_module.py
@@ -1,9 +1,22 @@
 import unittest
+from unittest.mock import Mock
 from run.task.module import module
 
 class module_Test(unittest.TestCase):
 
     # Public
 
-    def test(self):
-        pass
+    def setUp(self):
+        self.args = ('arg1',)
+        self.kwargs = {'kwarg1': 'kwarg1'}
+        self.module = module()
+
+    def test_expand(self):
+        mock_module = Mock()
+        mock_attr2 = getattr(mock_module, 'attr1.attr2')
+        self.module.attr1.attr2(*self.args, **self.kwargs)
+        self.assertEqual(
+            self.module.expand(mock_module),
+            mock_attr2.return_value)
+        # Check call
+        mock_attr2.assert_called_with(*self.args, **self.kwargs)
