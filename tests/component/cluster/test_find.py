4d7d56fbeed99f27790b5a787ed377058208844b
roll <roll@respect31.com>
2014-07-18 12:44:16 +0400

    added test_find test stub

---
+++ tests/component/cluster/test_find.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest.mock import Mock
+from run.cluster.find import find
+
+class find_Test(unittest.TestCase):
+
+    # Public
+
+    def setUp(self):
+        pass
+
+    def test___call__(self):
+        pass
