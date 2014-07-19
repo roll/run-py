064a9e1169e4fee75f24cf05e297ba675ab0407a
roll <roll@respect31.com>
2014-07-19 10:00:04 +0400

    implememted MetaConstraint tests

--- tests/component/cluster/test_meta.py
+++ tests/component/cluster/test_meta.py
@@ -16,14 +16,26 @@ class MetaConstraintTest(unittest.TestCase):
         self.constraint(self.emitter)
         self.assertFalse(self.emitter.skip.called)
 
+    def test___call___skip_name_is_descriptor(self):
+        self.emitter.object.meta_name = property(lambda self: self)
+        self.emitter.object.meta_tags = ['tag1', 'tag3']
+        self.constraint(self.emitter)
+        self.assertTrue(self.emitter.skip.called)
+
     def test___call___skip_name_does_not_match(self):
         self.emitter.object.meta_name = 'name3'
         self.emitter.object.meta_tags = ['tag1', 'tag3']
         self.constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
+        self.assertTrue(self.emitter.skip.called)
 
-    def test___call___skip_tags_does_not_match(self):
+    def test___call___skip_tags_is_descriptor(self):
+        self.emitter.object.meta_name = 'name1'
+        self.emitter.object.meta_tags = property(lambda self: self)
+        self.constraint(self.emitter)
+        self.assertTrue(self.emitter.skip.called)
+
+    def test___call___skip_tags_do_not_match(self):
         self.emitter.object.meta_name = 'name1'
         self.emitter.object.meta_tags = ['tag3']
         self.constraint(self.emitter)
-        self.assertTrue(self.emitter.skip.called)
