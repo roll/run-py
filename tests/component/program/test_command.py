f55ae738a584ef323acc13ee060839fc1e6cf503
roll <roll@respect31.com>
2014-07-19 12:13:17 +0400

    added test_command stub

---
+++ tests/component/program/test_command.py
@@ -0,0 +1,12 @@
+import unittest
+from run.program.command import Command
+
+class CommandTest(unittest.TestCase):
+
+    # Public
+
+    def setUp(self):
+        pass
+
+    def test(self):
+        pass
