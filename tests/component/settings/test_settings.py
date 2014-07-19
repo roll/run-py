7630e91df9670b4e4a641d61a6c9ad0a77019da0
roll <roll@respect31.com>
2014-07-19 12:14:24 +0400

    added test_settings stub

---
+++ tests/component/settings/test_settings.py
@@ -0,0 +1,12 @@
+import unittest
+from run.settings.settings import Settings
+
+class SettingsTest(unittest.TestCase):
+
+    # Public
+
+    def setUp(self):
+        pass
+
+    def test(self):
+        pass
