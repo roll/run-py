b563a7c6efac5efc67320bbf376c610fd81bda2f
roll <roll@respect31.com>
2014-07-19 12:10:08 +0400

    added test_dependency stub

---
+++ tests/component/dependency/test_dependency.py
@@ -0,0 +1,12 @@
+import unittest
+from run.dependency.dependency import Dependency
+
+class DependencyTest(unittest.TestCase):
+
+    # Public
+
+    def setUp(self):
+        pass
+
+    def test(self):
+        pass
