0c43f3c424e9b1e71f8cc7439e745074de5ff5ee
roll <roll@respect31.com>
2014-07-19 11:14:46 +0400

    implemented firts version of find test

--- tests/component/cluster/test_find.py
+++ tests/component/cluster/test_find.py
@@ -7,7 +7,37 @@ class find_Test(unittest.TestCase):
     # Public
 
     def setUp(self):
-        pass
+        self.find = self._make_mock_find()
 
-    def test___call__(self):
-        pass
+    def test(self):
+        list(self.find(
+            names='names',
+            tags='tags',
+            file='file',
+            exclude='exclude',
+            basedir='basedir',
+            recursively='recursively'))
+        self.find._find_files.assert_called_with(
+            filepath=None,
+            notfilepath=None,
+            filename='file',
+            notfilename='exclude',
+            basedir='basedir',
+            maxdepth=None)
+
+    # Protected
+
+    def _make_mock_find(self):
+        class mock_find(find):
+            # Public
+            default_basedir = 'default_basedir'
+            default_exclude = 'default_exclude'
+            default_file = 'default_file'
+            default_names = 'default_names'
+            default_recursively = 'default_recursively'
+            default_tags = 'default_tags'
+            # Protected
+            _find_files = Mock(return_value=['file1', 'file2'])
+            _loader_class = Mock(return_value=Mock(
+                load_module=Mock(return_value=unittest.mock)))
+        return mock_find
