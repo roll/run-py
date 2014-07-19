e8993d047899148a88b38729785c0f37c943ef4a
roll <roll@respect31.com>
2014-07-19 09:55:50 +0400

    added MetaConstraint tests

--- tests/component/cluster/test_meta.py
+++ tests/component/cluster/test_meta.py
@@ -7,8 +7,23 @@ class MetaConstraintTest(unittest.TestCase):
     # Public
 
     def setUp(self):
-        self.constraint = MetaConstraint(['name1, name2'], ['tag1', 'tag2'])
+        self.constraint = MetaConstraint(['name1', 'name2'], ['tag1', 'tag2'])
         self.emitter = Mock()
 
     def test___call___not_skip(self):
-        pass
+        self.emitter.object.meta_name = 'name1'
+        self.emitter.object.meta_tags = ['tag1', 'tag3']
+        self.constraint(self.emitter)
+        self.assertFalse(self.emitter.skip.called)
+
+    def test___call___skip_name_does_not_match(self):
+        self.emitter.object.meta_name = 'name3'
+        self.emitter.object.meta_tags = ['tag1', 'tag3']
+        self.constraint(self.emitter)
+        self.assertTrue(self.emitter.skip.called)
+
+    def test___call___skip_tags_does_not_match(self):
+        self.emitter.object.meta_name = 'name1'
+        self.emitter.object.meta_tags = ['tag3']
+        self.constraint(self.emitter)
+        self.assertTrue(self.emitter.skip.called)
