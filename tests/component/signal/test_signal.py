15edc663fdedd6f385c9c349fc00b5d4d15bf7ff
roll <roll@respect31.com>
2014-07-19 12:16:01 +0400

    added test_signal

---
+++ tests/component/signal/test_signal.py
@@ -0,0 +1,9 @@
+import unittest
+from run.signal.signal import Signal
+
+class SignalTest(unittest.TestCase):
+
+    # Public
+
+    def test(self):
+        self.assertTrue(issubclass(Signal, object))
