4a428c93f813f28a32fefd1921d17e3510128f58
roll <roll@respect31.com>
2014-07-19 12:09:20 +0400

    added test_resolver stub

---
+++ tests/component/dependency/test_resolver.py
@@ -0,0 +1,12 @@
+import unittest
+from run.dependency.resolver import Resolver
+
+class ResolverTest(unittest.TestCase):
+
+    # Public
+
+    def setUp(self):
+        pass
+
+    def test(self):
+        pass
