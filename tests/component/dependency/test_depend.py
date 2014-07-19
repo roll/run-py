67eaa1ddc0b14c67914a1da8ce9381b97d05a9be
roll <roll@respect31.com>
2014-07-19 12:02:00 +0400

    implemented test_depend

--- tests/component/dependency/test_depend.py
+++ tests/component/dependency/test_depend.py
@@ -1,9 +1,19 @@
 import unittest
+from unittest.mock import Mock, patch
 from run.dependency.depend import depend
 
 class depend_Test(unittest.TestCase):
 
     # Public
 
-    def test(self):
-        pass
+    def setUp(self):
+        self.addCleanup(patch.stopall)
+        self.MethodTask = patch.object(depend, '_method_task_class').start()
+
+    def test___call__(self):
+        result = depend('dependency')('method')
+        self.assertEqual(result, self.MethodTask.return_value)
+        # MethodTask call check
+        self.MethodTask.assert_called_with('method')
+        # MethodTask's return value (prototype) call check
+        self.MethodTask.return_value.depend.assert_called_with('dependency')
