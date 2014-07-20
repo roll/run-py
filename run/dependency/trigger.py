aff6882b6219178636469a1a28c6465729c63168
roll <roll@respect31.com>
2014-07-20 13:16:22 +0400

    min

--- run/dependency/trigger.py
+++ run/dependency/trigger.py
@@ -21,7 +21,6 @@ class trigger(Dependency):
         self._on_fail = kwargs.pop('on_fail', False)
         super().__init__(task, *args, **kwargs)
 
-
     def resolve(self, failed=None):
         if failed != None:
             if (self._on_success and not failed or
