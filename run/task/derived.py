acba0f1afbd5b0024688e36440c9ab8141c8f0b1
roll <roll@respect31.com>
2014-07-20 20:38:33 +0400

    renamed _task to _task_instance in DerivedTask

--- run/task/derived.py
+++ run/task/derived.py
@@ -6,27 +6,27 @@ class DerivedTask(Task):
     # Public
 
     def __init__(self, task, *args, **kwargs):
-        self._task_name = task
+        self._task = task
         super().__init__(*args, **kwargs)
 
     @property
     def meta_docstring(self):
         return self._meta_params.get('docstring',
             'Derived from task "{task_qualname}".\n{task_docstring}'.
-            format(task_qualname=self._task.meta_qualname,
-                   task_docstring=self._task.meta_docstring))
+            format(task_qualname=self._task_instance.meta_qualname,
+                   task_docstring=self._task_instance.meta_docstring))
 
     @property
     def meta_signature(self):
         return self._meta_params.get('signature',
-            self._task.meta_signature)
+            self._task_instance.meta_signature)
 
     def invoke(self, *args, **kwargs):
-        return self._task(*args, **kwargs)
+        return self._task_instance(*args, **kwargs)
 
     # Protected
 
     @property
-    def _task(self):
-        return attribute(self.meta_module, self._task_name,
+    def _task_instance(self):
+        return attribute(self.meta_module, self._task,
             category=Task, getvalue=True)
