33081ebfb1e376b80e71aafc04a7f1242da0c9d2
roll <roll@respect31.com>
2014-07-21 09:44:45 +0400

    min

--- tests/component/dependency/test_require.py
+++ tests/component/dependency/test_require.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 from run.dependency.require import require
 
 class require_Test(unittest.TestCase):
@@ -7,22 +7,23 @@ class require_Test(unittest.TestCase):
     # Public
 
     def setUp(self):
+        self.addCleanup(patch.stopall)
+        self.invoke = patch.object(require, 'invoke').start()
         self.require = require('task')
-        self.require.invoke = Mock()
 
     def test_resolve(self):
         self.require.resolve()
         self.require.resolve()
         # Check invoke call
-        self.assertEqual(self.require.invoke.call_count, 1)
+        self.assertEqual(self.invoke.call_count, 1)
 
     def test_resolve_not_enabled(self):
         self.require.disable()
         self.require.resolve()
         # Check invoke call
-        self.assertEqual(self.require.invoke.call_count, 0)
+        self.assertEqual(self.invoke.call_count, 0)
 
-    def test_resolve_failed(self):
-        self.require.resolve(failed=True)
+    def test_resolve_failed_not_none(self):
+        self.require.resolve(failed='failed')
         # Check invoke call
-        self.assertEqual(self.require.invoke.call_count, 0)
+        self.assertEqual(self.invoke.call_count, 0)
