7531f8645a185900f2d246d75ad82e88e02f66f8
roll <roll@respect31.com>
2014-07-23 10:55:49 +0400

    refactored AttributeTest

--- tests/component/attribute/test_attribute.py
+++ tests/component/attribute/test_attribute.py
@@ -7,6 +7,8 @@ class AttributeTest(unittest.TestCase):
     # Public
 
     def setUp(self):
+        self.Module = self._make_mock_module_class()
+        self.module = self.Module()
         self.Attribute = self._make_mock_attribute_class()
         self.attribute = self.Attribute(meta_module=None)
 
@@ -29,32 +31,71 @@ class AttributeTest(unittest.TestCase):
         self.assertEqual(self.attribute.meta_dispatcher, 'dispatcher')
 
     def test_meta_docstring(self):
-        self.assertEqual(self.attribute.meta_docstring, 'docstring')
+        self.assertEqual(self.attribute.meta_docstring,
+                         self.Attribute.__doc__)
+
+    def test_meta_docstring_initter(self):
+        self.attribute = self.Attribute(
+            meta_module=None, meta_docstring='initter_docstring')
+        self.assertEqual(self.attribute.meta_docstring, 'initter_docstring')
 
     def test_meta_docstring_setter(self):
-        self.attribute.meta_docstring = 'docstring'
-        self.assertEqual(self.attribute.meta_docstring, 'docstring')
+        self.attribute.meta_docstring = 'setter_docstring'
+        self.assertEqual(self.attribute.meta_docstring, 'setter_docstring')
 
     def test_meta_main_module(self):
         # NullModule
-        self.assertNotEqual(self.attribute.meta_main_module, None)
         self.assertFalse(self.attribute.meta_main_module)
+        self.assertNotEqual(self.attribute.meta_main_module, None)
 
     def test_meta_module(self):
-        # Null module
-        self.assertNotEqual(self.attribute.meta_module, None)
+        # NullModule
         self.assertFalse(self.attribute.meta_module)
+        self.assertNotEqual(self.attribute.meta_module, None)
+
+    def test_meta_module_with_module(self):
+        self.attribute = self.Attribute(meta_module=self.module)
+        self.assertEqual(self.attribute.meta_module, self.module)
 
     def test_meta_name(self):
         self.assertEqual(self.attribute.meta_name, '')
 
+    def test_meta_name_with_module(self):
+        self.attribute = self.Attribute(meta_module=self.module)
+        self.module.meta_attributes = {'attribute': self.attribute}
+        self.assertEqual(self.attribute.meta_name, 'attribute')
+
     def test_meta_qualname(self):
         self.assertEqual(self.attribute.meta_qualname, '')
+
+    def test_meta_qualname_with_module(self):
+        self.attribute = self.Attribute(meta_module=self.module)
+        self.module.meta_attributes = {'attribute': self.attribute}
+        self.assertEqual(self.attribute.meta_qualname, 'module.attribute')
+
+    def test_meta_qualname_with_module_is_main(self):
+        self.attribute = self.Attribute(meta_module=self.module)
+        self.module.meta_attributes = {'attribute': self.attribute}
+        self.module.meta_is_main_module = True
+        self.assertEqual(self.attribute.meta_qualname, '[module] attribute')
 
     def test_meta_type(self):
         self.assertEqual(self.attribute.meta_type, 'MockAttribute')
 
     # Protected
+
+    def _make_mock_module_class(self):
+        class MockModule:
+            # Public
+            meta_attributes = {}
+            meta_basedir = 'basedir'
+            meta_dispatcher = 'dispatcher'
+            meta_is_main_module = False
+            # Mocking NullModule
+            meta_main_module = False
+            meta_name = 'module'
+            meta_qualname = 'module'
+        return MockModule
 
     def _make_mock_attribute_class(self):
         class MockAttribute(Attribute):
@@ -63,69 +104,3 @@ class AttributeTest(unittest.TestCase):
             __get__ = Mock()
             __set__ = Mock()
         return MockAttribute
-
-
-class AttributeTest_with_module(AttributeTest):
-
-    # Public
-
-    def setUp(self):
-        self.Attribute = self._make_mock_attribute_class()
-        self.Module = self._make_mock_module_class()
-        self.module = self.Module()
-        self.attribute = self.Attribute(meta_module=self.module)
-        self.module.meta_attributes = {'attribute': self.attribute}
-
-    def test_meta_module(self):
-        self.assertEqual(self.attribute.meta_module, self.module)
-
-    def test_meta_name(self):
-        self.assertEqual(self.attribute.meta_name, 'attribute')
-
-    def test_meta_qualname(self):
-        self.assertEqual(self.attribute.meta_qualname, 'module.attribute')
-
-    # Protected
-
-    def _make_mock_module_class(self, is_main=False):
-        class MockModule:
-            # Public
-            meta_attributes = {}
-            meta_basedir = 'basedir'
-            meta_dispatcher = 'dispatcher'
-            meta_is_main_module = is_main
-            # Instead of NullModule
-            meta_main_module = False
-            meta_name = 'module'
-            meta_qualname = 'module'
-        return MockModule
-
-
-class AttributeTest_with_module_is_main(AttributeTest_with_module):
-
-    # Public
-
-    def setUp(self):
-        self.Attribute = self._make_mock_attribute_class()
-        self.MainModule = self._make_mock_module_class(is_main=True)
-        self.module = self.MainModule()
-        self.attribute = self.Attribute(meta_module=self.module)
-        self.module.meta_attributes = {'attribute': self.attribute}
-
-    def test_meta_qualname(self):
-        self.assertEqual(self.attribute.meta_qualname, '[module] attribute')
-
-
-class AttributeTest_with_docstring(AttributeTest):
-
-    # Public
-
-    def setUp(self):
-        self.Attribute = self._make_mock_attribute_class()
-        self.docstring = 'new_docstring'
-        self.attribute = self.Attribute(
-            meta_docstring=self.docstring,
-            meta_module=None)
-
-    def test_meta_docstring(self):
-        self.assertEqual(self.attribute.meta_docstring, self.docstring)
