255858701d262a91bbd7a0a87b152c0eb8e2725c
roll <roll@respect31.com>
2014-07-26 12:27:57 +0400

    improved fork docs

--- run/attribute/fork.py
+++ run/attribute/fork.py
@@ -1,12 +1,23 @@
 def fork(prototype, *args, **kwargs):
     """Fork attribute prototype with optional args, kwargs altering.
 
+    Parameters
+    ----------
+    prototype: :class:`.AttributePrototype`
+        Attribute prototype to fork.
+    *args: tuple
+        Positional arguments to add to prototype's default.
+    **kwargs: dict
+        Keyword arguments to add to prototype's default.
+
+    Examples
+    --------
     Usage example::
 
-      class Module(Module):
+        class Module(Module):
 
-          attr1 = SomeTask()
-          attr2 = fork(attr1, meta_basedir='new/path', param='value')
+            attr1 = SomeTask()
+            attr2 = fork(attr1, meta_basedir='new/path', param='value')
 
     In this case attr2 will build as attr1 copy with redefined
     meta_basedir and default keyword argument param.
