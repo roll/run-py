4692a2b9be4314a94e3de6d745cbf91a170f4ee2
roll <roll@respect31.com>
2014-07-26 12:06:40 +0400

    added doc to build

--- run/attribute/build.py
+++ run/attribute/build.py
@@ -1,4 +1,16 @@
 def build(prototype, module):
     """Build prototype to module's attribute.
+
+    Parameters
+    ----------
+    prototype: :class:`.AttributePrototype`
+        Attribute prototype.
+    module: :class:`.Module`
+        Module to build prototype as attribute of module.
+
+    Returns
+    -------
+    :class:`.Attribute`
+        Builded attribute.
     """
     return prototype.__build__(module)
