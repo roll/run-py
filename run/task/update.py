7abddfa415ecdf6c13fb4934f41b32adffc305bd
roll <roll@respect31.com>
2014-07-28 08:35:05 +0400

    added update to Task

---
+++ run/task/update.py
@@ -0,0 +1,12 @@
+class Update:
+
+    # Public
+
+    def __init__(self, name, *args, **kwargs):
+        self._name = name
+        self._args = args
+        self._kwargs = kwargs
+
+    def apply(self, attribute):
+        method = getattr(attribute, self._name)
+        method(*self._args, **self._kwargs)
