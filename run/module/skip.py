c4864e71bcafcf95e0ff7f79ccf6bbf6c1e61be1
roll <roll@respect31.com>
2014-07-28 14:19:53 +0400

    min

--- run/module/skip.py
+++ run/module/skip.py
@@ -1,7 +1,7 @@
 from box.functools import Decorator
 
 class skip(Decorator):
-    """Make attribute to be skipped.
+    """Make task to be skipped.
 
     Usage example::
 
@@ -11,12 +11,12 @@ class skip(Decorator):
           def method(self):
               pass
 
-    In this case method will not be converted to run's attribute and
+    In this case method will not be converted to run's task and
     will stay as regular python method.
     """
 
     attribute_name = '_run_module_skip'
 
-    def __call__(self, attribute):
-        setattr(attribute, self.attribute_name, True)
-        return attribute
+    def __call__(self, task):
+        setattr(task, self.attribute_name, True)
+        return task
