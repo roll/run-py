500d90b905d5d9408ff75a291c9e9cbfa17c4995
roll <roll@respect31.com>
2014-07-29 12:05:09 +0400

    refactored CommandTest

--- tests/integration/program/test_command.py
+++ tests/integration/program/test_command.py
@@ -1,79 +1,39 @@
 import unittest
-from box.functools import cachedproperty
+from functools import partial
 from run.program.command import Command
 from run.settings import settings
 
-# TODO: remove unittest inheritance?
 class CommandTest(unittest.TestCase):
 
     # Public
 
     def setUp(self):
-        self.argv = ['run']
+        self.pCommand = partial(Command, config=settings.argparse)
 
-    @cachedproperty
-    def command(self):
-        return Command(self.argv, config=settings.argparse)
-
-    def test_task(self):
-        self.assertEqual(self.command.task,
-                         self.command.default_task)
-
-    def test_args(self):
+    def test(self):
+        self.command = self.pCommand(['run'])
+        self.assertEqual(self.command.task, self.command.default_task)
         self.assertEqual(self.command.args, [])
-
-    def test_kwargs(self):
         self.assertEqual(self.command.kwargs, {})
 
-
-class CommandTest_with_task_and_arguments(CommandTest):
-
-    # Public
-
-    def setUp(self):
-        self.argv = ['run', 'task', 'arg1,True,kwarg1=1,', 'kwarg2=1.5']
-
-    def test_task(self):
+    def test_with_task_and_arguments(self):
+        self.command = self.pCommand(
+            ['run', 'task', 'arg1,True,kwarg1=1,', 'kwarg2=1.5'])
         self.assertEqual(self.command.task, 'task')
-
-    def test_args(self):
         self.assertEqual(self.command.args, ['arg1', True])
-
-    def test_kwargs(self):
         self.assertEqual(self.command.kwargs, {'kwarg1': 1, 'kwarg2': 1.5})
 
-
-class CommandTest_with_list_flag(CommandTest):
-
-    # Public
-
-    def setUp(self):
-        self.argv = ['run', 'task', '-l']
-
-    def test_task(self):
+    def test_with_with_list_flag(self):
+        self.command = self.pCommand(['run', 'task', '-l'])
         self.assertEqual(self.command.task, 'list')
-
-    def test_args(self):
         self.assertEqual(self.command.args, ['task'])
 
-
-class CommandTest_with_info_flag(CommandTest_with_list_flag):
-
-    # Public
-
-    def setUp(self):
-        self.argv = ['run', 'task', '-i']
-
-    def test_task(self):
+    def test_with_with_info_flag(self):
+        self.command = self.pCommand(['run', 'task', '-i'])
         self.assertEqual(self.command.task, 'info')
+        self.assertEqual(self.command.args, ['task'])
 
-
-class CommandTest_with_meta_flag(CommandTest_with_list_flag):
-
-    # Public
-
-    def setUp(self):
-        self.argv = ['run', 'task', '-m']
-
-    def test_task(self):
+    def test_with_with_meta_flag(self):
+        self.command = self.pCommand(['run', 'task', '-m'])
         self.assertEqual(self.command.task, 'meta')
+        self.assertEqual(self.command.args, ['task'])
