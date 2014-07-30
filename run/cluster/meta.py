a1e741400c42d412e4a50d3b41a9e373a6b8d8f5
roll <roll@respect31.com>
2014-07-30 11:47:06 +0400

    removed from MetaConstraint is_descriptor complexity

--- run/cluster/meta.py
+++ run/cluster/meta.py
@@ -1,5 +1,4 @@
 import inspect
-import logging
 
 class MetaConstraint:
 
@@ -11,25 +10,12 @@ class MetaConstraint:
 
     def __call__(self, emitter):
         if self._names:
-            if self._is_descriptor(emitter.object, 'meta_name'):
+            if inspect.isdatadescriptor(emitter.object.meta_name):
                 emitter.skip()
             elif emitter.object.meta_name not in self._names:
                 emitter.skip()
         if self._tags:
-            if self._is_descriptor(emitter.object, 'meta_tags'):
+            if inspect.isdatadescriptor(emitter.object.meta_tags):
                 emitter.skip()
             elif set(emitter.object.meta_tags).isdisjoint(self._tags):
                 emitter.skip()
-
-    # Protected
-
-    def _is_descriptor(self, obj, name):
-        is_descriptor = False
-        if inspect.isdatadescriptor(getattr(obj, name)):
-            is_descriptor = True
-            if name in vars(obj):
-                logging.getLogger(__name__).warning(
-                    'Module class {obj} skipped because "{name}" '
-                    'is not a static attribute (required for filter)'.
-                    format(obj=obj, name=name))
-        return is_descriptor
