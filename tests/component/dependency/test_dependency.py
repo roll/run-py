c9b7d38c7c3fe36871f333a320d2ce9890dc5efa
roll <roll@respect31.com>
2014-08-03 14:16:58 +0400

    min

--- tests/component/dependency/test_dependency.py
+++ tests/component/dependency/test_dependency.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import MagicMock, Mock, patch
+from unittest.mock import Mock, patch
 from run.dependency.dependency import Dependency
 
 class DependencyTest(unittest.TestCase):
