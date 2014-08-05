3069cd366f0f2128badf5dd9f69646f8ff98d7fa
roll <roll@respect31.com>
2014-08-05 18:44:25 +0400

    removed ModulePrototype._class usage from _initiate_task, _build_task

--- run/module/prototype.py
+++ run/module/prototype.py
@@ -12,18 +12,21 @@ class ModulePrototype(TaskPrototype):
 
     _task_prototype_class = TaskPrototype
 
-    # TODO: use self._class or type(task)?
+#     def _create_task(self):
+#         class_copy = type(task).__copy__()
+#         task = class_copy.__create__(self)
+#         return task
+
     def _initiate_task(self, task, module):
-        for name in dir(self._class):
-            attr = getattr(self._class, name)
+        for name in dir(type(task)):
+            attr = getattr(type(task), name)
             if isinstance(attr, self._task_prototype_class):
                 nested_task = build(attr, task)
-                setattr(self._class, name, nested_task)
+                setattr(type(task), name, nested_task)
         return super()._initiate_task(task, module)
 
-    # TODO: use self._class or type(task)?
     def _update_task(self, task):
         for name, nested_task in task.meta_tasks.items():
             nested_task = nested_task.__update__()
-            setattr(self._class, name, nested_task)
+            setattr(type(task), name, nested_task)
         return super()._update_task(task)
