f28484b3d5e2a24a23566ff42b9def367af4cf80
roll <roll@respect31.com>
2014-08-10 10:32:44 +0400

    fixed test_constraint

--- tests/component/finder/test_constraint.py
+++ tests/component/finder/test_constraint.py
@@ -1,7 +1,7 @@
 import unittest
 import inspect
 from unittest.mock import Mock
-from run.module.constraint import Constraint
+from run.finder.constraint import Constraint
 
 
 class ConstraintTest(unittest.TestCase):
@@ -13,10 +13,10 @@ class ConstraintTest(unittest.TestCase):
             names=['name1', 'name2'],
             tags=['tag1', 'tag2'])
         self.emitter = Mock()
-        self.emitter.object = Mock
-        self.emitter.object.meta_name = 'name1'
-        self.emitter.object.meta_tags = ['tag1', 'tag3']
-        self.emitter.module = inspect.getmodule(self.emitter.object)
+        self.emitter.objself = Mock
+        self.emitter.objself.meta_name = 'name1'
+        self.emitter.objself.meta_tags = ['tag1', 'tag3']
+        self.emitter.module = inspect.getmodule(self.emitter.objself)
 
     def test___call__not_skip(self):
         self.constraint(self.emitter)
@@ -28,7 +28,7 @@ class ConstraintTest(unittest.TestCase):
         self.assertTrue(self.emitter.skip.called)
 
     def test___call___skip_object_is_not_type(self):
-        self.emitter.object = Mock()
+        self.emitter.objself = Mock()
         self.constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
 
@@ -38,24 +38,24 @@ class ConstraintTest(unittest.TestCase):
         self.assertTrue(self.emitter.skip.called)
 
     def test___call___skip_name_is_descriptor(self):
-        self.emitter.object.meta_name = property(lambda self: self)
-        self.emitter.object.meta_tags = ['tag1', 'tag3']
+        self.emitter.objself.meta_name = property(lambda self: self)
+        self.emitter.objself.meta_tags = ['tag1', 'tag3']
         self.constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
 
     def test___call___skip_name_does_not_match(self):
-        self.emitter.object.meta_name = 'name3'
-        self.emitter.object.meta_tags = ['tag1', 'tag3']
+        self.emitter.objself.meta_name = 'name3'
+        self.emitter.objself.meta_tags = ['tag1', 'tag3']
         self.constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
 
     def test___call___skip_tags_is_descriptor(self):
-        self.emitter.object.meta_name = 'name1'
-        self.emitter.object.meta_tags = property(lambda self: self)
+        self.emitter.objself.meta_name = 'name1'
+        self.emitter.objself.meta_tags = property(lambda self: self)
         self.constraint(self.emitter)
         self.assertTrue(self.emitter.skip.called)
 
     def test___call___skip_tags_do_not_match(self):
-        self.emitter.object.meta_name = 'name1'
-        self.emitter.object.meta_tags = ['tag3']
+        self.emitter.objself.meta_name = 'name1'
+        self.emitter.objself.meta_tags = ['tag3']
         self.constraint(self.emitter)
