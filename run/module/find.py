c20ed196d8993be8f7030849ad92173f7c00e53a
roll <roll@respect31.com>
2014-08-10 10:54:57 +0400

    fixed FindModule

--- run/module/find.py
+++ run/module/find.py
@@ -1,6 +1,7 @@
+import os
 import inspect
-from .module import Module
 from ..finder import find
+from .module import Module
 
 
 class FindModule(Module):
@@ -9,13 +10,16 @@ class FindModule(Module):
 
     @classmethod
     def __meta_create__(cls, *args, meta_module, meta_updates, **kwargs):
+        basedir = kwargs.pop('basedir', None)
+        notfilepath = os.path.relpath(
+            inspect.getfile(type(meta_module)), start=basedir)
         Module = cls._find(
             names=kwargs.pop('names', None),
             tags=kwargs.pop('tags', None),
             file=kwargs.pop('file', None),
-            basedir=kwargs.pop('basedir', None),
+            basedir=basedir,
             recursively=kwargs.pop('recursively', None),
-            filters=[{'notfilepath': inspect.getfile(type(meta_module))}],
+            filters=[{'notfilepath': notfilepath}],
             getfirst=True)
         module = Module(
             *args,
