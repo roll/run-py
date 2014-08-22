3502ef0d7ab37bb4b283af6dc661676454b5ce53
roll <roll@respect31.com>
2014-08-22 12:08:03 +0400

    implemented ClusterTask

--- run/task/cluster.py
+++ run/task/cluster.py
@@ -1,5 +1,19 @@
-class ClusterTask:
+from .task import Task
+
+
+class ClusterTask(Task):
 
     # Public
 
-    pass
+    def __init__(self, tasks, *args, **kwargs):
+        self._tasks = tasks
+        super().__init__(*args, **kwargs)
+
+    def meta_invoke(self, *args, **kwargs):
+        for task in self._tasks:
+            task(*args, **kwargs)
+
+    @property
+    def meta_docstring(self):
+        return self._meta_params.get(
+            'docstring', 'Invoke "{self._tasks}" tasks.'.format(self=self))
