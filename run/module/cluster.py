6cab65b5175f58cd0537dbe1f643015aa048d10d
roll <roll@respect31.com>
2014-08-22 12:25:37 +0400

    implemented ClusterModule draft

--- run/module/cluster.py
+++ run/module/cluster.py
@@ -1,7 +1,28 @@
+import os
+import inspect
+from ..find import find
 from .module import Module
+
 
 class ClusterModule(Module):
 
     # Public
 
-    pass
+    def __init__(self, *args, **kwargs):
+        basedir = kwargs.pop('basedir', None)
+        notfilepath = os.path.relpath(
+            inspect.getfile(type(self.meta_module)), start=basedir)
+        Modules = self._find(
+            target=self._Module,
+            key=kwargs.pop('key', None),
+            tags=kwargs.pop('tags', None),
+            file=kwargs.pop('file', None),
+            basedir=basedir,
+            recursively=kwargs.pop('recursively', None),
+            filters=[{'notfilepath': notfilepath}])
+        return Modules
+
+    # Protected
+
+    _find = find
+    _Module = Module
