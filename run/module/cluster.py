19d7961763ed882d06e556a89556cb29bb7ec59c
roll <roll@respect31.com>
2014-08-22 13:20:14 +0400

    improved ClusterModule

--- run/module/cluster.py
+++ run/module/cluster.py
@@ -1,5 +1,6 @@
 import os
 import inspect
+from box.functools import cachedproperty
 from ..find import find
 from .module import Module
 
@@ -8,21 +9,48 @@ class ClusterModule(Module):
 
     # Public
 
-    def __init__(self, *args, **kwargs):
-        basedir = kwargs.pop('basedir', None)
-        notfilepath = os.path.relpath(
-            inspect.getfile(type(self.meta_module)), start=basedir)
-        Modules = self._find(
-            target=self._Module,
-            key=kwargs.pop('key', None),
-            tags=kwargs.pop('tags', None),
-            file=kwargs.pop('file', None),
-            basedir=basedir,
-            recursively=kwargs.pop('recursively', None),
-            filters=[{'notfilepath': notfilepath}])
-        return Modules
+    def __init__(self, *args,
+                 key=None, tags=None,
+                 file=None, exclude=None, basedir=None, recursively=None,
+                 **kwargs):
+        self._key = key
+        self._tags = tags
+        self._file = file
+        self._exclude = exclude
+        self._basedir = basedir
+        self._recursively = recursively
+        super().__init__(*args, **kwargs)
 
     # Protected
 
     _find = find
     _Module = Module
+
+    @cachedproperty
+    def _modules(self):
+        modules = []
+        for Module in self._Modules:
+            module = Module(
+                meta_dispatcher=self.meta_dispatcher,
+                meta_plain=self.meta_plain,
+                meta_module=None)
+            modules.append(module)
+        return modules
+
+    @cachedproperty
+    def _Modules(self):
+        Modules = self._find(
+            target=self._Module,
+            key=self._key,
+            tags=self._tags,
+            file=self._file,
+            basedir=self._basedir,
+            recursively=self._recursively,
+            filters=[{'notfilepath': self._notfilepath}])
+        return Modules
+
+    @property
+    def _notfilepath(self):
+        notfilepath = os.path.relpath(
+            inspect.getfile(type(self.meta_module)), start=self._basedir)
+        return notfilepath
