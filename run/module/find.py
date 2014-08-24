d06007a19b18b712bffbc4d49b31a9de3aec2f71
roll <roll@respect31.com>
2014-08-24 12:11:48 +0400

    added args, kwargs to FindModule

--- run/module/find.py
+++ run/module/find.py
@@ -9,9 +9,10 @@ class FindModule(Module):
     # Public
 
     @classmethod
-    def __meta_create__(cls, *, meta_module, meta_updates,
+    def __meta_create__(cls, *args, meta_module, meta_updates,
                         key=None, tags=None,
-                        file=None, exclude=None, basedir=None, recursively=None):
+                        file=None, exclude=None, basedir=None, recursively=None,
+                        **kwargs):
         notfilepath = os.path.relpath(
             inspect.getfile(type(meta_module)), start=basedir)
         Module = cls._find(
@@ -24,8 +25,10 @@ class FindModule(Module):
             filters=[{'notfilepath': notfilepath}],
             getfirst=True)
         module = Module(
+            *args,
             meta_module=meta_module,
-            meta_updates=meta_updates)
+            meta_updates=meta_updates,
+            **kwargs)
         return module
 
     # Protected
