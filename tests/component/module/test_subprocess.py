2ec9f901137164e68810c6bb8a87b5e3f9c7367c
roll <roll@respect31.com>
2014-08-26 08:23:42 +0400

    added test_without_mapping to Subprocess test

--- tests/component/module/test_subprocess.py
+++ tests/component/module/test_subprocess.py
@@ -1,4 +1,5 @@
 import unittest
+from functools import partial
 from unittest.mock import patch, call
 from run.module import subprocess
 
@@ -11,12 +12,17 @@ class SubprocessModuleTest(unittest.TestCase):
         self.addCleanup(patch.stopall)
         self.Task = patch.object(subprocess, 'SubprocessTask').start()
         self.Module = self._make_mock_module_class()
-        self.module = self.Module({'command1': 'command1'},
-            prefix='prefix_', separator='separator_', meta_module=None)
+        self.pModule = partial(
+            self.Module,
+            prefix='prefix_',
+            separator='separator_',
+            meta_module=None)
+        self.module = self.pModule({'command1': 'command1'})
 
     def test(self):
         self.assertEqual(self.module.command0, self.Task.return_value)
         self.assertEqual(self.module.command1, self.Task.return_value)
+        # Check Task calls
         self.Task.assert_has_calls(
             [call(prefix='prefix_separator_command0',
                   separator='separator_',
@@ -25,6 +31,15 @@ class SubprocessModuleTest(unittest.TestCase):
                   separator='separator_',
                   meta_module=self.module)],
             any_order=True)
+
+    def test_without_mapping(self):
+        self.module = self.pModule()
+        self.assertEqual(self.module.command0, self.Task.return_value)
+        # Check Task calls
+        self.Task.assert_has_calls(
+            [call(prefix='prefix_separator_command0',
+                  separator='separator_',
+                  meta_module=self.module)])
 
     def test_meta_docstring(self):
         self.assertTrue(self.module.meta_docstring)
