5d78b37506b576d829bc5615a28dadec0ce356db
roll <roll@respect31.com>
2014-08-26 09:38:39 +0400

    implemneted convert_Test

--- tests/component/converter/test_convert.py
+++ tests/component/converter/test_convert.py
@@ -1,9 +1,27 @@
 import unittest
+from unittest.mock import Mock
 from run.converter.convert import convert
 
 class convert_Test(unittest.TestCase):
 
     # Public
 
+    def setUp(self):
+        self.convert = self._make_mock_convert()
+
     def test(self):
-        pass
+        self.assertEqual(self.convert('object'), 'value')
+
+    def test_cant_convert(self):
+        self.convert._converters.pop()
+        self.assertRaises(TypeError, self.convert, 'object')
+
+    # Protected
+
+    def _make_mock_convert(self):
+        class mock_convert(convert):
+            # Protected
+            _converters = [
+                Mock(side_effect=TypeError()),
+                Mock(return_value='value')]
+        return mock_convert
