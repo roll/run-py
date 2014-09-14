e85ac347aea80ad3c1eef6400ac68441f6ee31fc
roll <roll@respect31.com>
2014-09-14 13:02:45 +0400

    made question in Dialog optional

--- run/library/dialog.py
+++ run/library/dialog.py
@@ -10,12 +10,9 @@ class DialogTask(FunctionTask):
 
     def __init__(self, *args, **kwargs):
         self.__dialog = Dialog()
+        function = self.__dialog.question
         if kwargs.get('message', False):
             function = self.__dialog.message
-        elif kwargs.get('question', False):
-            function = self.__dialog.question
-        else:
-            raise ValueError('Unsupported mode')
         super().__init__(function, *args, **kwargs)
 
 
