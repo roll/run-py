28da01fd56edf067ccbf5508097a81f46a05a82d
roll <roll@respect31.com>
2014-09-14 13:33:42 +0400

    rebased on dialog 0.3

--- run/library/dialog.py
+++ run/library/dialog.py
@@ -8,11 +8,16 @@ class DialogTask(FunctionTask):
 
     # Public
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, listen=False, **kwargs):
         self.__dialog = Dialog()
-        function = self.__dialog.question
-        if kwargs.get('message', False):
+        if listen:
+            function = self.__dialog.listen
+        elif kwargs.get('message', False):
             function = self.__dialog.message
+        elif kwargs.get('question', False):
+            function = self.__dialog.question
+        else:
+            raise ValueError('Unsupported mode')
         super().__init__(function, *args, **kwargs)
 
 
