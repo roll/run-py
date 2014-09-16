e571ccc1adda4b25f925cf62f99e733d7761c257
roll <roll@respect31.com>
2014-09-16 10:02:28 +0400

    fixed Constraint p/p

--- run/find/constraint.py
+++ run/find/constraint.py
@@ -7,32 +7,32 @@ class Constraint(Constraint):
     # Public
 
     def __init__(self, target, *, key=None, tags=None):
-        self._target = target
-        self._key = key
-        self._tags = tags
+        self.__target = target
+        self.__key = key
+        self.__tags = tags
 
     def __call__(self, emitter):
         if inspect.getmodule(emitter.objself) != emitter.module:
             emitter.skip()
         elif not isinstance(emitter.objself, type):
             emitter.skip()
-        elif not issubclass(emitter.objself, self._target):
+        elif not issubclass(emitter.objself, self.__target):
             emitter.skip()
-        elif not self._match_key(emitter.objself.meta_key):
+        elif not self.__match_key(emitter.objself.meta_key):
             emitter.skip()
-        elif not self._match_tags(emitter.objself.meta_tags):
+        elif not self.__match_tags(emitter.objself.meta_tags):
             emitter.skip()
 
-    # Protected
+    # Private
 
-    def _match_key(self, key):
-        if self._key is not None:
-            if key != self._key:
+    def __match_key(self, key):
+        if self.__key is not None:
+            if key != self.__key:
                 return False
         return True
 
-    def _match_tags(self, tags):
-        if self._tags is not None:
-            if set(tags).isdisjoint(self._tags):
+    def __match_tags(self, tags):
+        if self.__tags is not None:
+            if set(tags).isdisjoint(self.__tags):
                 return False
         return True
