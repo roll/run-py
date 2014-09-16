dc2143638d86661d62827bfabf7332f69879ff82
roll <roll@respect31.com>
2014-09-16 10:11:39 +0400

    fixed ClusterTask p/p

--- run/library/cluster/task.py
+++ run/library/cluster/task.py
@@ -6,12 +6,12 @@ class ClusterTask(Task):
     # Public
 
     def __init__(self, tasks, *args, **kwargs):
-        self._tasks = tasks
+        self.__tasks = tasks
         super().__init__(*args, **kwargs)
 
     def meta_invoke(self, *args, **kwargs):
         results = []
-        for task in self._tasks:
+        for task in self.__tasks:
             result = task(*args, **kwargs)
             results.append(result)
         return results
@@ -19,4 +19,4 @@ class ClusterTask(Task):
     @property
     def meta_docstring(self):
         return self._meta_params.get(
-            'docstring', 'Invoke "{self._tasks}" tasks.'.format(self=self))
+            'docstring', 'Invoke "{tasks}" tasks.'.format(tasks=self.__tasks))
