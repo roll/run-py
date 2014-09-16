351261b62a223be27719d1a86a013da9cd67d7cc
roll <roll@respect31.com>
2014-09-16 11:49:00 +0400

    fixed Handler p/p

--- run/signal/handler.py
+++ run/signal/handler.py
@@ -15,9 +15,9 @@ class CallbackHandler(Handler):
     def __init__(self, callback, signals=None):
         if signals is None:
             signals = []
-        self._callback = callback
-        self._signals = signals
+        self.__callback = callback
+        self.__signals = signals
 
     def handle(self, signal):
-        if isinstance(signal, tuple(self._signals)):
-            self._callback(signal)
+        if isinstance(signal, tuple(self.__signals)):
+            self.__callback(signal)
