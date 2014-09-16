2fa17fc2b9982cf087d6f1bf0204fdbc1b05e6ae
roll <roll@respect31.com>
2014-09-16 10:00:43 +0400

    fixed trigger p/p

--- run/dependency/trigger.py
+++ run/dependency/trigger.py
@@ -22,13 +22,13 @@ class trigger(Dependency):
     # Public
 
     def __init__(self, predecessor_name, *args, **kwargs):
-        self._on_success = kwargs.pop('on_success', True)
-        self._on_fail = kwargs.pop('on_fail', False)
+        self.__on_success = kwargs.pop('on_success', True)
+        self.__on_fail = kwargs.pop('on_fail', False)
         super().__init__(predecessor_name, *args, **kwargs)
 
     def resolve(self, failed=None):
         if self.enabled:
             if failed is not None:
-                if (self._on_success and not failed or
-                    self._on_fail and failed):
+                if (self.__on_success and not failed or
+                    self.__on_fail and failed):
                     self.invoke()
