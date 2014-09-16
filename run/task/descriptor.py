12573f161dc8c8e6d624474adf773ff84005081a
roll <roll@respect31.com>
2014-09-16 11:52:27 +0400

    fixed DescriptorTask p/p

--- run/task/descriptor.py
+++ run/task/descriptor.py
@@ -8,14 +8,14 @@ class DescriptorTask(Task):
 
     # TODO: why args, kwargs?
     def __init__(self, descriptor, *args, **kwargs):
-        self._descriptor = descriptor
+        self.__descriptor = descriptor
         super().__init__(*args, **kwargs)
 
     def meta_invoke(self):
-        return self._descriptor.__get__(
+        return self.__descriptor.__get__(
             self.meta_module, type(self.meta_module))
 
     @property
     def meta_docstring(self):
         return self._meta_params.get(
-            'docstring', str(inspect.getdoc(self._descriptor)).strip())
+            'docstring', str(inspect.getdoc(self.__descriptor)).strip())
