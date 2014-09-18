96c6f74dccfd56b0d56bc36c4ffcafe329514d6d
roll <roll@respect31.com>
2014-09-18 14:23:36 +0400

    min

--- run/machine/machine.py
+++ run/machine/machine.py
@@ -10,6 +10,7 @@ from ..task import TaskSignal
 from .stack import Stack
 
 
+# TODO: info/list/meta here from Module?
 # TODO: move all plain/format logic here?
 # TODO: Support for tasks without module in runfile?
 class Machine:
