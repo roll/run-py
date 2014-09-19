d664aebd8f906b416dec1e0fb1eb2df75b1a41b7
roll <roll@respect31.com>
2014-09-19 19:00:13 +0400

    implemented first version of DialogModule

--- run/library/dialog/module.py
+++ run/library/dialog/module.py
@@ -1,9 +1,29 @@
 from ...module import Module
-from .task import DialogTask
+from .var import DialogVar
 
 
+# TODO: improve work
 class DialogModule(Module):
 
     # Public
 
-    pass
+    def __init__(self, question='Please input "{name}": ', *args, **kwargs):
+        self.__question = question
+        self.__args = args
+        self.__kwargs = kwargs
+
+    def __getattr__(self, name):
+        var = DialogVar(
+            *self.__args,
+            question=self.__question,
+            name=name,
+            meta_module=self,
+            **self.__kwargs)
+        setattr(type(self), name, var)
+        result = getattr(self, name)
+        return result
+
+    @property
+    def meta_docstring(self):
+        return self.meta_get_parameter(
+            'docstring', inherit=False, default='DialogModule')
