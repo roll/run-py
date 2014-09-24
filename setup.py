9986d5f31eb32fd7756cf538354b4840f8406002
roll <roll@respect31.com>
2014-09-24 10:44:12 +0400

    updated version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     classifiers=[],       
     description='Run is a program to run tasks from files.',
     data_files=data_files,
-    download_url='https://github.com/respect31/run/tarball/0.35.0',
+    download_url='https://github.com/respect31/run/tarball/0.36.0',
     entry_points={'console_scripts': ['run = run:program']},
     license='MIT License',
     maintainer='roll',
@@ -200,4 +200,4 @@ THE SOFTWARE.
 ''',     
     tests_require=['nose', 'coverage'],
     test_suite='nose.collector',
-    version='0.35.0')
+    version='0.36.0')
