2aacfb195dab9581b4d469779ac3646b3a47b2c4
roll <roll@respect31.com>
2014-09-24 10:34:45 +0400

    fixed protected/private on ClusterModule

--- run/library/cluster/module.py
+++ run/library/cluster/module.py
@@ -6,7 +6,6 @@ from ..find import find_modules
 from .task import ClusterTask
 
 
-# TODO: fix private/protected after cachedproperty fix
 class ClusterModule(Module):
 
     # Public
@@ -17,47 +16,47 @@ class ClusterModule(Module):
         self.__key = key
         self.__tags = tags
         self.__basedir = basedir
-        for task_name, task_instances in self._tasks.items():
+        for task_name, task_instances in self.__tasks.items():
             if not hasattr(type(self), task_name):
                 task = ClusterTask(task_instances, meta_module=self)
                 setattr(type(self), task_name, task)
 
-    # Protected
+    # Private
 
     @property
-    def _tasks(self):
+    def __tasks(self):
         tasks = {}
         keys = set()
-        for module in self._modules:
+        for module in self.__modules:
             keys.update(module.meta_tasks.keys())
         for key in keys:
             tasks[key] = []
-            for module in self._modules:
+            for module in self.__modules:
                 if key in module.meta_tasks:
                     task = module.meta_tasks[key]
                     tasks[key].append(task)
         return tasks
 
     @cachedproperty
-    def _modules(self):
+    def __modules(self):
         modules = []
-        for Module in self._Modules:
+        for Module in self.__Modules:
             module = Module(meta_module=self)
             modules.append(module)
         return modules
 
     @cachedproperty
-    def _Modules(self):
+    def __Modules(self):
         Modules = find_modules(
             filename=self.__filename,
             key=self.__key,
             tags=self.__tags,
             basedir=self.__basedir,
-            filters=[{'notfilepath': self._notfilepath}])
+            filters=[{'notfilepath': self.__notfilepath}])
         return Modules
 
     @property
-    def _notfilepath(self):
+    def __notfilepath(self):
         notfilepath = os.path.relpath(
             inspect.getfile(type(self.meta_module)), start=self.__basedir)
         return notfilepath
