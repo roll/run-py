b509ab74b119a369ca2845fff2b37dca197271d4
roll <roll@respect31.com>
2014-10-07 14:14:43 +0400

    added target to RenderTask

---
+++ docs/metadata.py
@@ -0,0 +1,4 @@
+# TO MAKE CHANGES USE "meta" DIRECTORY (see packgram docs).
+
+name = 'run'
+version = '0.37.0'
--- run/library/render/task.py
+++ run/library/render/task.py
@@ -1,3 +1,4 @@
+import os
 import render
 from ...task import FunctionTask
 
@@ -14,3 +15,19 @@ class RenderTask(FunctionTask):
                 'Unsupported mode "{mode}".'.format(mode=mode))
         kwargs.setdefault('context', self.meta_module)
         super().__init__(function, *args, **kwargs)
+
+    # TODO: use pipe system instead of target?
+    def meta_invoke(self, *args, target=None, **kwargs):
+        result = super().meta_invoke(*args, **kwargs)
+        if target is not None:
+            return self.__save(result, target)
+        return result
+
+    # Private
+
+    def __save(self, result, target):
+        dirname = os.path.dirname(target)
+        if dirname:
+            os.makedirs(dirname, exist_ok=True)
+        with open(target, 'w') as file:
+            file.write(result)
