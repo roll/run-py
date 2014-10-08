fd89a36cb5360d6777661727776b0d30d8ae16b7
roll <roll@respect31.com>
2014-10-08 09:21:10 +0400

    removed target from RenderTask

--- run/library/render/task.py
+++ run/library/render/task.py
@@ -1,9 +1,7 @@
-import os
 import render
 from ...task import FunctionTask
 
 
-# TODO: use inheritance and self as context?
 class RenderTask(FunctionTask):
 
     # Public
@@ -16,15 +14,3 @@ class RenderTask(FunctionTask):
                 'Unsupported mode "{mode}".'.format(mode=mode))
         kwargs.setdefault('context', self.meta_module)
         super().__init__(function, *args, **kwargs)
-
-    # TODO: rename target to savepath?
-    # TODO: use pipe system instead of target?
-    def meta_invoke(self, *args, target=None, **kwargs):
-        result = super().meta_invoke(*args, **kwargs)
-        if target is None:
-            return result
-        dirname = os.path.dirname(target)
-        if dirname:
-            os.makedirs(dirname, exist_ok=True)
-        with open(target, 'w') as file:
-            file.write(result)
