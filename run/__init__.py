3d02782859424470c8d998b50e7132c9efb59c7a
roll <roll@respect31.com>
2014-11-07 15:30:50 +0300

    removed Dispatcher from API

--- run/__init__.py
+++ run/__init__.py
@@ -3,6 +3,6 @@ from .metadata import version
 from .module import Module, module
 from .program import program
 from .settings import settings
-from .signal import Dispatcher, Signal
+from .signal import Signal
 from .task import Task, task, depend, require, trigger, skip
 from .var import Var, var
