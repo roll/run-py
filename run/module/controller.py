2f02a6473f9d84811011121a5eeac575397469da
roll <roll@respect31.com>
2014-11-08 10:02:11 +0300

    added controller stub to module

---
+++ run/module/controller.py
@@ -0,0 +1,26 @@
+import logging
+
+
+class Controller:
+
+    # Public
+
+    def on_task_signal(self, signal):
+        # Stack operations
+        if self.__compact:
+            # TODO: stack here is not tread-safe?
+            self.__stack.push(signal.task)
+            formatted__stack = self.__stack.format()
+            self.__stack.pop()
+        else:
+            if signal.event == 'called':
+                self.__stack.push(signal.task)
+            formatted__stack = self.__stack.format()
+            if signal.event in ['successed', 'failed']:
+                self.__stack.pop()
+        # Logging operations
+        formatted_signal = signal.format()
+        if formatted_signal:
+            message = formatted_signal + formatted__stack
+            logger = logging.getLogger('task')
+            logger.info(message)
