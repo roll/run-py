960327cf354432f6c943e6d3bf16bb75efa2b871
roll <roll@respect31.com>
2014-11-09 11:42:35 +0300

    min

--- run/module/controller.py
+++ run/module/controller.py
@@ -16,17 +16,17 @@ class Controller:
         if self.__compact:
             # TODO: stack here is not tread-safe?
             self.__stack.push(signal.task)
-            formatted__stack = self.__stack.format()
+            formatted_stack = self.__stack.format()
             self.__stack.pop()
         else:
             if signal.event == 'called':
                 self.__stack.push(signal.task)
-            formatted__stack = self.__stack.format()
+            formatted_stack = self.__stack.format()
             if signal.event in ['successed', 'failed']:
                 self.__stack.pop()
         # Logging operations
         formatted_signal = signal.format()
         if formatted_signal:
-            message = formatted_signal + formatted__stack
+            message = formatted_signal + formatted_stack
             logger = logging.getLogger('task')
             logger.info(message)
