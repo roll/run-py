b7d5cdd2b47c7701ae4e96bc9756823310abf589
roll <roll@respect31.com>
2014-11-10 12:38:50 +0300

    implemented BriefLogger

--- run/loggers/brief.py
+++ run/loggers/brief.py
@@ -1,9 +1,27 @@
+import logging
+from ..helpers import sformat
+from ..task import CallTaskEvent
+from ..settings import settings
+
+
 class BriefLogger:
 
     # Public
 
     def __call__(self, event):
-        pass
+        logger = logging.getLogger('task')
+        if isinstance(event, CallTaskEvent):
+            if event.state in [event.DONE, event.FAIL]:
+                prefix = '[+] '
+                style = 'successed'
+                if event.state == event.FAIL:
+                    prefix = '[-] '
+                    style = 'failed'
+                message = ''
+                message += prefix
+                message += event.task.meta_qualname
+                message = sformat(message, style, settings.styles)
+                logger.info(message)
 
     def __repr__(self):
         return '<BriefLogger>'
