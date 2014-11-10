acf140e4a2e4f00278e5d34b87cc526ca36b8269
roll <roll@respect31.com>
2014-11-10 10:19:40 +0300

    added tree logger

---
+++ run/listeners/__init__.py
@@ -0,0 +1,1 @@
+from .tree_logger import TreeLogger
---
+++ run/listeners/tree_logger.py
@@ -0,0 +1,41 @@
+import logging
+from ..helpers import sformat
+from ..task import CallTaskSignal, DoneTaskSignal, FailTaskSignal
+from ..settings import settings
+
+
+class TreeLogger:
+
+    # Public
+
+    def __init__(self):
+        self.__compact = settings.compact
+        self.__plain = settings.plain
+        self.__stack = []
+
+    def __call__(self, signal):
+        logger = logging.getLogger('task')
+        if isinstance(signal, CallTaskSignal):
+            message = ''
+            message += (len(self.__stack) - 1) * '│  '
+            if self.__stack:
+                message += '├──'
+            message += signal.task.meta_qualname or 'main'
+            logger.info(message)
+            self.__stack.append(signal.task)
+        elif isinstance(signal, DoneTaskSignal):
+            self.__stack.pop()
+            message = ''
+            message += len(self.__stack) * '│  '
+            message += '└──'
+            message += '[10 ms]'
+            message = sformat(message, 'successed', settings.styles)
+            logger.info(message)
+        elif isinstance(signal, FailTaskSignal):
+            self.__stack.pop()
+            message = ''
+            message += len(self.__stack) * '│  '
+            message += '└──'
+            message += '[fail]'
+            message = sformat(message, 'failed', settings.styles)
+            logger.info(message)
