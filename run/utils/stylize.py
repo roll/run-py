6a51d7d33908c0f824cea6a909b446afdda32e58
roll <roll@respect31.com>
2014-11-11 12:13:46 +0300

    implemented stylize

--- run/utils/stylize.py
+++ run/utils/stylize.py
@@ -1,2 +1,17 @@
-def stylize():
-    pass
+import sugarbowl
+from ..settings import settings
+
+
+def stylize(self, string, *, styles, modes={}, layers={}, colors={}, **style):
+    """Format string with style.
+    """
+    if not settings.plain:
+        estyle = {}
+        for item in styles:
+            item = settings.styles.get(item, None)
+            if item is not None:
+                estyle.update(item)
+        estyle.update(style)
+        string = sugarbowl.stylize(
+            string, modes=modes, layers=layers, colors=colors, **estyle)
+    return string
