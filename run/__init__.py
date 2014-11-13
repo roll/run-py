0f1b644292b5a3b820e79c5f990e11d738510f0e
roll <roll@respect31.com>
2014-11-13 12:34:47 +0300

    added more events to API

--- run/__init__.py
+++ run/__init__.py
@@ -3,6 +3,7 @@ from .loggers import BriefLogger, LinearLogger, TreeLogger
 from .module import Module, module
 from .program import program
 from .settings import settings
-from .task import Task, Event, task, depend, require, trigger, skip
+from .task import (Task, Event, TaskEvent, CallTaskEvent,
+                   task, depend, require, trigger, skip)
 from .utils import stylize
 from .var import Var, var
