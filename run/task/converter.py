7d381ca80b699e40fd4f8df410e8d9f04535ed53
roll <roll@respect31.com>
2014-11-13 08:49:55 +0300

    fixed property converting

--- run/task/converter.py
+++ run/task/converter.py
@@ -54,6 +54,7 @@ class task(Function):
     def protocol(self, *args, **kwargs):
         try:
             if (inspect.isfunction(args[0]) or
+                inspect.isdatadescriptor(args[0]) or
                 isinstance(args[0], (Task, Prototype))):
                 return Function.FUNCTION
         except IndexError:
